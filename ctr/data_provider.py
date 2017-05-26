c126c52040d29254ab5726fc7a71cdc502725207
Superjom <superjom@gmail.com>
2017-05-26 09:09:33 +0800

    add cross feature into model input

--- ctr/data_provider.py
+++ ctr/data_provider.py
@@ -47,7 +47,7 @@ feature_dims = {}
 categorial_features = ('C1 banner_pos site_category app_category ' +
                        'device_type device_conn_type').split()
 
-id_features = 'id site_id app_id device_id'.split()
+id_features = 'id site_id app_id device_id _device_id_cross_site_id'.split()
 
 
 def get_all_field_names(mode=0):
@@ -98,12 +98,14 @@ class CategoryFeatureGenerator(object):
 
 
 class IDfeatureGenerator(object):
-    def __init__(self, max_dim):
+    def __init__(self, max_dim, cross_fea0=None, cross_fea1=None):
         '''
         @max_dim: int
             Size of the id elements' space
         '''
         self.max_dim = max_dim
+        self.cross_fea0 = cross_fea0
+        self.cross_fea1 = cross_fea1
 
     def gen(self, key):
         '''
@@ -134,19 +136,27 @@ class ContinuousFeatureGenerator(object):
         return (val - self.min) / self.len_part
 
 
+# init all feature generators
 fields = {}
 for key in categorial_features:
     fields[key] = CategoryFeatureGenerator()
 for key in id_features:
-    fields[key] = IDfeatureGenerator(10000)
+    # for cross features
+    if 'cross' in key:
+        feas = key[1:].split('_cross_')
+        fields[key] = IDfeatureGenerator(10000000, *feas)
+    # for normal ID features
+    else:
+        fields[key] = IDfeatureGenerator(10000)
 
+# used as feed_dict in PaddlePaddle
 field_index = dict(
     (key, id) for id, key in enumerate(['dnn_input', 'lr_input', 'click']))
 
 
 def detect_dataset(path, topn, id_fea_space=10000):
     '''
-    Parse the first `topn` records to collect information of this dataset.
+    Parse the first `topn` records to collect meta information of this dataset.
 
     NOTE the records should be randomly shuffled first.
     '''
@@ -164,23 +174,23 @@ def detect_dataset(path, topn, id_fea_space=10000):
     for key, item in fields.items():
         feature_dims[key] = item.size()
 
-    for key in id_features:
-        feature_dims[key] = id_fea_space
+    #for key in id_features:
+        #feature_dims[key] = id_fea_space
 
     feature_dims['hour'] = 24
     feature_dims['click'] = 1
 
     feature_dims['dnn_input'] = np.sum(
-        feature_dims[key] for key in categorial_features + ['hour']) + 10
+        feature_dims[key] for key in categorial_features + ['hour']) + 1
     feature_dims['lr_input'] = np.sum(feature_dims[key]
-                                      for key in id_features) + 10
+                                      for key in id_features) + 1
 
     return feature_dims
 
 
 def concat_sparse_vectors(inputs, dims):
     '''
-    concaterate sparse vectors into one
+    Concaterate more than one sparse vectors into one.
 
     @inputs: list
         list of sparse vector
@@ -198,6 +208,9 @@ def concat_sparse_vectors(inputs, dims):
 
 
 class AvazuDataset(object):
+    '''
+    Load AVAZU dataset as train set.
+    '''
     TRAIN_MODE = 0
     TEST_MODE = 1
 
@@ -239,7 +252,12 @@ class AvazuDataset(object):
 
                 record = []
                 for key in id_features:
-                    record.append(fields[key].gen(row[key]))
+                    if 'cross' not in key:
+                        record.append(fields[key].gen(row[key]))
+                    else:
+                        fea0 = fields[key].cross_fea0
+                        fea1 = fields[key].cross_fea1
+                        record.append(fields[key].gen_cross_fea(row[fea0], row[fea1]))
 
                 sparse_input = concat_sparse_vectors(record, id_dims)
 
