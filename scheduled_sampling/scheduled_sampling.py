4a181015387d3756af8570de96cff4958e77d908
Hu Weiwei <wwhu@foxmail.com>
2017-06-17 11:28:16 +0800

    remove V2 postfix

--- scheduled_sampling/scheduled_sampling.py
+++ scheduled_sampling/scheduled_sampling.py
@@ -168,8 +168,8 @@ def seqToseq_net(source_dict_dim, target_dict_dim, is_generating=False):
         return out
 
     decoder_group_name = "decoder_group"
-    group_input1 = paddle.layer.StaticInputV2(input=encoded_vector, is_seq=True)
-    group_input2 = paddle.layer.StaticInputV2(input=encoded_proj, is_seq=True)
+    group_input1 = paddle.layer.StaticInput(input=encoded_vector, is_seq=True)
+    group_input2 = paddle.layer.StaticInput(input=encoded_proj, is_seq=True)
     group_inputs = [group_input1, group_input2]
 
     if not is_generating:
@@ -198,7 +198,7 @@ def seqToseq_net(source_dict_dim, target_dict_dim, is_generating=False):
 
         return cost
     else:
-        trg_embedding = paddle.layer.GeneratedInputV2(
+        trg_embedding = paddle.layer.GeneratedInput(
             size=target_dict_dim,
             embedding_name='_target_language_embedding',
             embedding_size=word_vector_dim)
