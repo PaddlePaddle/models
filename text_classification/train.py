30c701ca797ee289fa70866c1b5dfa0dce623d76
olenet <olenet@126.com>
2017-11-28 17:05:24 +0800

    fix typos : porivided -> provided

--- text_classification/train.py
+++ text_classification/train.py
@@ -39,7 +39,7 @@ def train(topology,
     use_default_data = (train_data_dir is None)
 
     if use_default_data:
-        logger.info(("No training data are porivided, "
+        logger.info(("No training data are providedd, "
                      "use paddle.dataset.imdb to train the model."))
         logger.info("please wait to build the word dictionary ...")
 
