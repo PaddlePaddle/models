83613458f28993ad5a7beeaedde8999d8b13adb6
robot <2466956298@qq.com>
2018-06-06 19:16:25 +0800

    DQN Atari based on RLLab

---
+++ fluid/DeepQNetwork/utils.py
@@ -0,0 +1,25 @@
+#-*- coding: utf-8 -*-
+#File: utils.py
+#Author: yobobobo(zhouboacmer@qq.com)
+import tensorflow as tf
+
+class Summary(object):
+    """Logging in tensorboard without tensorflow ops."""
+
+    def __init__(self, log_dir):
+        """Creates a summary writer logging to log_dir."""
+        self.writer = tf.summary.FileWriter(log_dir)
+
+    def log_scalar(self, tag, value, step):
+        """Log a scalar variable.
+        Parameter
+        ----------
+        tag : basestring
+            Name of the scalar
+        value
+        step : int
+            training iteration
+        """
+        summary = tf.Summary(value=[tf.Summary.Value(tag=tag,
+                                                     simple_value=value)])
+        self.writer.add_summary(summary, step)
