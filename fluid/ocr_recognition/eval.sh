3605054376bedf63e6acb75ff901c6d6888f33eb
root <root@yq01-idl-gpu-jpaas-mgr00.yq01.baidu.com>
2018-06-21 03:15:34 +0000

    Remove eval.sh

--- fluid/ocr_recognition/eval.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-python eval.py \
---input_images_dir="/root/.cache/paddle/dataset/ctc_data/data/test_images/" \
---input_images_list="/root/.cache/paddle/dataset/ctc_data/data/test.list" \
---use_gpu=True \
---model_path="/home/work/workspace/models/fluid/ocr_recognition/models/model_60000"
-
