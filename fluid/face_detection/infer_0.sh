05abd53679867c05a556aea969f586388eda3bd7
Dang <dangqingqing@baidu.com>
2018-06-27 16:33:24 +0000

    infer scripts.

---
+++ fluid/face_detection/infer_0.sh
@@ -0,0 +1,1 @@
+env CUDA_VISIBLE_DEVICES=0, python -u infer.py --model_dir=output_pyramidbox_0626_anchor_sampling_fix/29.bak/ --slice_num=4 --slice_index=0 >log_pyramidbox_infer_anchor_sampling_pass29_0 2>&1 &
---
+++ fluid/face_detection/infer_2.sh
@@ -0,0 +1,1 @@
+env CUDA_VISIBLE_DEVICES=2, python -u infer.py --model_dir=output_pyramidbox_0626_anchor_sampling_fix/29.bak/ --slice_num=4 --slice_index=2 >log_pyramidbox_infer_anchor_sampling_pass29_2 2>&1 &
