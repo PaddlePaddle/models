75558ff209b4d9c1b30bb7aab0e9065ecdff1037
guochaorong <32069604+guochaorong@users.noreply.github.com>
2018-07-27 00:42:49 +0800

    Update .run.sh

--- fluid/object_detection/.run.sh
+++ fluid/object_detection/.run.sh
@@ -9,3 +9,7 @@ if [ ! -d "/root/.cache/paddle/dataset/pascalvoc" ];then
     bash ./.move.sh
 fi
 FLAGS_benchmark=true  python train.py --batch_size=64 --num_passes=2 --for_model_ce=True --data_dir=/root/.cache/paddle/dataset/pascalvoc/ 
+
+cudaid=${object_detection_multi_cudaid:=0,1,2,3} # use 0-th card as default
+export CUDA_VISIBLE_DEVICES=$cudaid
+FLAGS_benchmark=true  python train.py --batch_size=64 --num_passes=2 --gpu_card_num=4 --data_dir=/root/.cache/paddle/dataset/pascalvoc/ 
