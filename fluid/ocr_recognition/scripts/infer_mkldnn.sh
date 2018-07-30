d7633471430d54a872413a03e3a15295f3527b91
Michal Gallus <michal.gallus@intel.com>
2018-07-30 09:56:26 +0200

    Make crnn-ctc scripts more portable

--- fluid/ocr_recognition/scripts/infer_mkldnn.sh
+++ fluid/ocr_recognition/scripts/infer_mkldnn.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-FLAGS_use_mkldnn=1 OMP_NUM_THREADS=56 KMP_AFFINITY=granularity=fine,compact,1,0 python ../infer.py \
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export FLAGS_use_mkldnn=1
+
+ht=`lscpu |grep "per core"|awk -F':' '{print $2}'|xargs`
+if [ $ht -eq 1 ]; then # HT is OFF
+    if [ -z "$KMP_AFFINITY" ]; then
+        export KMP_AFFINITY="granularity=fine,compact,0,0"
+    fi
+    if [ -z "$OMP_DYNAMIC" ]; then
+        export OMP_DYNAMIC="FALSE"
+    fi
+else # HT is ON
+    if [ -z "$KMP_AFFINITY" ]; then
+        export KMP_AFFINITY="granularity=fine,compact,1,0"
+    fi
+fi
+
+python ../infer.py \
     --model_path mkldnn_model/model_00001 \
     --input_images_list ~/.cache/paddle/dataset/ctc_data/data/test.list \
     --input_images_dir ~/.cache/paddle/dataset/ctc_data/data/test_images \
