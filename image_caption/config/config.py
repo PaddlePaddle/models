df8077504c884d6ff3ffac44b1038d5e280af663
Zhiyuan Hu <zhiyuan.hu.bj@gmail.com>
2018-08-01 02:24:36 +0800

    Add files via upload

---
+++ image_caption/config/config.py
@@ -0,0 +1,12 @@
+dict_size = 4528
+source_dict_dim = target_dict_dim = dict_size
+hidden_dim = 64
+word_dim = 50
+batch_size = 128
+max_length = 8
+topk_size = 50
+beam_size = 2
+is_sparse = True
+shape = [3,32,32]
+EPOCH_NUM = 5000
+model_save_dir = "machine_translation.inference.model"
