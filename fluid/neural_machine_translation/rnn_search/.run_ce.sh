68e7332780327e862facc710a0ab7909a77e98fb
Qingsheng Li <liqingsheng@baidu.com>
2018-08-08 10:08:27 +0800

    Fix script (#1108)
    Added CE py file

--- fluid/neural_machine_translation/rnn_search/.run_ce.sh
+++ fluid/neural_machine_translation/rnn_search/.run_ce.sh
@@ -2,4 +2,4 @@
 ####This file is only used for continuous evaluation.
 
 model_file='train.py'
-python $model_file --pass_num 1 --learning_rate 0.001 --save_interval 10 --enable_ce
+python $model_file --pass_num 1 --learning_rate 0.001 --save_interval 10 --enable_ce | python _ce.py
