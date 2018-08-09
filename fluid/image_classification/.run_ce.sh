af65da08b570a24672989ea94a887592059fb1ed
baiyfbupt <baiyfbupt@gmail.com>
2018-08-09 15:27:49 +0800

    update batch_size

--- fluid/image_classification/.run_ce.sh
+++ fluid/image_classification/.run_ce.sh
@@ -3,8 +3,8 @@
 # This file is only used for continuous evaluation.
 cudaid=${object_detection_cudaid:=0}
 export CUDA_VISIBLE_DEVICES=$cudaid
-python train.py --batch_size=256 --num_passes=10 --total_images=6149 --enable_ce=True | python _ce.py
+python train.py --batch_size=64 --num_passes=10 --total_images=6149 --enable_ce=True | python _ce.py
 
 cudaid=${object_detection_cudaid:=0, 1, 2, 3}
 export CUDA_VISIBLE_DEVICES=$cudaid
-python train.py --batch_size=256 --num_passes=10 --total_images=6149 --enable_ce=True | python _ce.py
+python train.py --batch_size=64 --num_passes=10 --total_images=6149 --enable_ce=True | python _ce.py
