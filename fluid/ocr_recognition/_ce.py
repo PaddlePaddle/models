09700a282d14f83732289571631f40b5cf11c6ca
wanghaoshuang <wanghaoshuang@baidu.com>
2018-08-09 15:18:13 +0000

    remove skipping.

--- fluid/ocr_recognition/_ce.py
+++ fluid/ocr_recognition/_ce.py
@@ -7,10 +7,10 @@ from kpi import CostKpi, DurationKpi, AccKpi
 
 # NOTE kpi.py should shared in models in some way!!!!
 
-train_cost_kpi = CostKpi('train_cost', 0.02, actived=True)
-test_acc_kpi = AccKpi('test_acc', 0.005, actived=True)
-train_duration_kpi = DurationKpi('train_duration', 0.06, actived=True)
-train_acc_kpi = AccKpi('train_acc', 0.005, actived=True)
+train_cost_kpi = CostKpi('train_cost', 0.02, 0, actived=True)
+test_acc_kpi = AccKpi('test_acc', 0.005, 0, actived=True)
+train_duration_kpi = DurationKpi('train_duration', 0.06, 0, actived=True)
+train_acc_kpi = AccKpi('train_acc', 0.005, 0, actived=True)
 
 tracking_kpis = [
     train_acc_kpi,
