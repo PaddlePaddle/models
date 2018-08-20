d66573930cbc4b1958125d6398d9525ab74656ea
Bai Yifan <bai.yf@qq.com>
2018-08-20 15:52:25 +0800

    update ce (#1173)

--- fluid/image_classification/.run_ce.sh
+++ fluid/image_classification/.run_ce.sh
@@ -5,6 +5,6 @@ cudaid=${object_detection_cudaid:=0}
 export CUDA_VISIBLE_DEVICES=$cudaid
 python train.py --batch_size=64 --num_epochs=10 --total_images=6149 --enable_ce=True | python _ce.py
 
-cudaid=${object_detection_cudaid:=0, 1, 2, 3}
+cudaid=${object_detection_cudaid_m:=0, 1, 2, 3}
 export CUDA_VISIBLE_DEVICES=$cudaid
 python train.py --batch_size=64 --num_epochs=10 --total_images=6149 --enable_ce=True | python _ce.py
