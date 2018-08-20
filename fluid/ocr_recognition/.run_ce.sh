41ce9f29409492ee9ad2954517cced175fe2efbf
wanghaoshuang <wanghaoshuang@baidu.com>
2018-08-20 06:36:14 +0000

    fix run_ce.sh

--- fluid/ocr_recognition/.run_ce.sh
+++ fluid/ocr_recognition/.run_ce.sh
@@ -1,5 +1,4 @@
 export ce_mode=1
-rm *factor.txt
-python ctc_train.py --batch_size=32 --total_step=30000 --eval_period=30000 --log_period=30000 --use_gpu=True 1> ./tmp.log
+python ctc_train.py --batch_size=32 --total_step=1 --eval_period=1 --log_period=1 --use_gpu=True 1> ./tmp.log
 cat tmp.log | python _ce.py
 rm tmp.log
