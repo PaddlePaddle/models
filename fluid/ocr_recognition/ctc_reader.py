2cdf723fe130b8a71155df8a6e821efebc8e93e4
wanghaoshuang <wanghaoshuang@baidu.com>
2018-08-20 06:35:21 +0000

    Remove v2 api.

--- fluid/ocr_recognition/ctc_reader.py
+++ fluid/ocr_recognition/ctc_reader.py
@@ -4,8 +4,8 @@ import tarfile
 import numpy as np
 from PIL import Image
 from os import path
-from paddle.v2.image import load_image
-import paddle.v2 as paddle
+from paddle.dataset.image import load_image
+import paddle
 
 NUM_CLASSES = 95
 DATA_SHAPE = [1, 48, 512]
