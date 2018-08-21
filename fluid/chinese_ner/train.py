1fbae957d62d73b0b82f514915db52278805ae4f
bingyanghuang <bingyang.huang@intel.com>
2018-08-21 16:02:19 +0800

    fix chinese_ner inference appearing linear_chain_crf_grad op

--- fluid/chinese_ner/train.py
+++ fluid/chinese_ner/train.py
@@ -266,11 +266,12 @@ def main(args):
     with fluid.program_guard(main, startup):
         avg_cost, feature_out, word, mention, target = ner_net(
             args.word_dict_len, args.label_dict_len)
-        sgd_optimizer = fluid.optimizer.SGD(learning_rate=1e-3)
-        sgd_optimizer.minimize(avg_cost)
 
         crf_decode = fluid.layers.crf_decoding(
             input=feature_out, param_attr=fluid.ParamAttr(name='crfw'))
+
+        sgd_optimizer = fluid.optimizer.SGD(learning_rate=1e-3)
+        sgd_optimizer.minimize(avg_cost)
 
         (precision, recall, f1_score, num_infer_chunks, num_label_chunks,
          num_correct_chunks) = fluid.layers.chunk_eval(
@@ -348,8 +349,8 @@ def main(args):
                   + str(f1))
             save_dirname = os.path.join(args.model_save_dir,
                                         "params_pass_%d" % pass_id)
-            fluid.io.save_inference_model(
-                save_dirname, ['word', 'mention', 'target'], [crf_decode], exe)
+            fluid.io.save_inference_model(save_dirname, ['word', 'mention'],
+                                          [crf_decode], exe)
 
 
 if __name__ == "__main__":
