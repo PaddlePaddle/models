14841f9e1e9b4fd474437a99bc96d8591c7d006d
Dang Qingqing <dangqingqing@baidu.com>
2018-09-14 02:41:49 +0000

    Refine code.

--- fluid/faster_rcnn/train.py
+++ fluid/faster_rcnn/train.py
@@ -4,14 +4,15 @@ import numpy as np
 import argparse
 import functools
 import shutil
+import cPickle
+from utility import add_arguments, print_arguments
+
 import paddle
 import paddle.fluid as fluid
 import reader
-import paddle.fluid.layers.learning_rate_scheduler as lr_scheduler
-from paddle.fluid.layers import control_flow
 from fasterrcnn_model import FasterRcnn, RPNloss
-from utility import add_arguments, print_arguments
-import cPickle
+from learning_rate import exponential_with_warmup_decay
+
 
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
@@ -43,48 +44,6 @@ add_arg('debug',            bool,   False,   "Debug mode")
 def load(file_name):
     v = cPickle.load(open(file_name))
     return v.astype('float32')
-
-def exponential_with_warmup_decay(learning_rate, boundaries, values, warmup_iter, warmup_factor):
-    global_step = lr_scheduler._decay_step_counter()
-
-    lr = fluid.layers.create_global_var(
-        shape=[1],
-        value=0.0,
-        dtype='float32',
-        persistable=True,
-        name="learning_rate")
-
-    warmup_iter_var = fluid.layers.fill_constant(
-        shape=[1],
-        dtype='float32',
-        value=float(warmup_iter),
-        force_cpu=True)
-
-    with control_flow.Switch() as switch:
-        with switch.case(global_step < warmup_iter_var):
-            alpha = global_step / warmup_iter_var
-            factor = warmup_factor * (1 - alpha) + alpha
-            decayed_lr = learning_rate * factor
-            fluid.layers.assign(decayed_lr, lr)
-
-        for i in range(len(boundaries)):
-            boundary_val = fluid.layers.fill_constant(
-                shape=[1],
-                dtype='float32',
-                value=float(boundaries[i]),
-                force_cpu=True)
-            value_var = fluid.layers.fill_constant(
-                shape=[1], dtype='float32', value=float(values[i]))
-            with switch.case(global_step < boundary_val):
-                fluid.layers.assign(value_var, lr)
-
-        last_value_var = fluid.layers.fill_constant(
-            shape=[1], dtype='float32', value=float(values[len(values) - 1]))
-        with switch.default():
-            fluid.layers.assign(last_value_var, lr)
-
-    return lr
-
 
 def train(args):
     num_passes = args.num_passes
