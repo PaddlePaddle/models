6bef9c6f68b541b4fbf6aef8ab93cc2f1e2e786b
baiyfbupt <baiyfbupt@gmail.com>
2018-09-18 16:00:26 +0800

    code fix

--- fluid/face_detection/reader.py
+++ fluid/face_detection/reader.py
@@ -294,7 +294,7 @@ def train(settings,
                         generator_output = enqueuer.queue.get()
                         break
                     else:
-                        time.sleep(0.01)
+                        time.sleep(0.02)
                 yield generator_output
                 generator_output = None
         finally:
