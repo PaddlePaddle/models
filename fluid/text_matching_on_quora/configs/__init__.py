d92a8053705921de048a255e542307a76e6e8e3a
barrierye <barriery@qq.com>
2018-10-12 10:55:13 +0800

    add PWIM code, but not finished

--- fluid/text_matching_on_quora/configs/__init__.py
+++ fluid/text_matching_on_quora/configs/__init__.py
@@ -1,3 +1,4 @@
 
 from cdssm import cdssm_base
 from dec_att import decatt_glove
+from pwim import pwim_base
--- fluid/text_matching_on_quora/models/__init__.py
+++ fluid/text_matching_on_quora/models/__init__.py
@@ -1,3 +1,3 @@
 from cdssm import cdssmNet
 from dec_att import DecAttNet
-
+from pwim import pwimNet
