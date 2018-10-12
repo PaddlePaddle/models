9c67d84628804117fc07f59aaac50506d7eb58d7
barrierye <barriery@qq.com>
2018-10-12 10:56:32 +0800

    add PWIM code, but not finished

---
+++ fluid/text_matching_on_quora/configs/pwim.py
@@ -0,0 +1,25 @@
+
+import basic_config
+
+def pwim_base():
+    """
+    set configs
+    """
+    config = basic_config.config()
+    config.batch_size = 128
+    config.learning_rate = 0.001
+    config.save_dirname = "pwim_model"
+    config.use_pretrained_word_embedding = False
+    config.dict_dim = 40000 # approx_vocab_size
+    
+    # net config
+    config.emb_dim = 300
+    config.kernel_size = 5
+    config.kernel_count = 300
+    config.fc_dim = 128
+    config.mlp_hid_dim = [128, 128]
+    config.droprate_conv = 0.1
+    config.droprate_fc = 0.1
+    config.class_dim = 2
+
+    return config 
