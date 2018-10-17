21b407332bddffb804cab533e44f31aa340d3d1a
mapingshuo <mps2012@yeah.net>
2018-10-17 16:12:16 +0800

    change infer-sent config in text_matching_on_quora model

--- fluid/text_matching_on_quora/configs/infer_sent.py
+++ fluid/text_matching_on_quora/configs/infer_sent.py
@@ -10,7 +10,7 @@ def infer_sent_v1():
     config.lr_decay = 0.99
     config.optimizer_type = 'sgd'
     config.save_dirname = "model_dir"
-    config.use_pretrained_word_embedding = False
+    config.use_pretrained_word_embedding = True
     config.dict_dim = 40000 # approx_vocab_size
     config.class_dim = 2
 
