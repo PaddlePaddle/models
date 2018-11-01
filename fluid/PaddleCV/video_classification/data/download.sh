efece26215161b01adb89f95159e27d7dae07f22
Xin Pan <panxin.grad@gmail.com>
2018-11-01 16:01:19 +0800

    Clean models that are not in our integration system.

--- fluid/PaddleCV/video_classification/data/download.sh
+++
@@ -1,9 +0,0 @@
-# Download the dataset
-echo "Downloading..."
-wget http://crcv.ucf.edu/data/UCF101/UCF101.rar
-wget http://crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
-
-# Extract the data.
-echo "Extracting..."
-unrar x UCF101.rar
-unzip UCF101TrainTestSplits-RecognitionTask.zip
