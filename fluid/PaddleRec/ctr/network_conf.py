768112b47fdec31c6ae32529ea46a43c1a74c661
tangwei12 <tangwei12@baidu.com>
2018-11-28 17:39:05 +0800

    update feeder to pyreader

--- fluid/PaddleRec/ctr/network_conf.py
+++ fluid/PaddleRec/ctr/network_conf.py
@@ -3,14 +3,26 @@ import math
 
 dense_feature_dim = 13
 
+
 def ctr_dnn_model(embedding_size, sparse_feature_dim):
     dense_input = fluid.layers.data(
         name="dense_input", shape=[dense_feature_dim], dtype='float32')
+
     sparse_input_ids = [
         fluid.layers.data(
             name="C" + str(i), shape=[1], lod_level=1, dtype='int64')
         for i in range(1, 27)
     ]
+
+    label = fluid.layers.data(name='label', shape=[1], dtype='int64')
+
+    datas = [dense_input] + sparse_input_ids + [label]
+
+    py_reader = fluid.layers.create_py_reader_by_data(capacity=64,
+                                                      feed_list=datas,
+                                                      name='py_reader',
+                                                      use_double_buffer=True)
+    words = fluid.layers.read_file(py_reader)
 
     def embedding_layer(input):
         return fluid.layers.embedding(
@@ -22,8 +34,8 @@ def ctr_dnn_model(embedding_size, sparse_feature_dim):
             size=[sparse_feature_dim, embedding_size],
             param_attr=fluid.ParamAttr(name="SparseFeatFactors", initializer=fluid.initializer.Uniform()))
 
-    sparse_embed_seq = map(embedding_layer, sparse_input_ids)
-    concated = fluid.layers.concat(sparse_embed_seq + [dense_input], axis=1)
+    sparse_embed_seq = map(embedding_layer, words[1:-1])
+    concated = fluid.layers.concat(sparse_embed_seq + words[0:1], axis=1)
 
     fc1 = fluid.layers.fc(input=concated, size=400, act='relu',
         param_attr=fluid.ParamAttr(initializer=fluid.initializer.Normal(scale=1/math.sqrt(concated.shape[1]))))
@@ -34,13 +46,10 @@ def ctr_dnn_model(embedding_size, sparse_feature_dim):
     predict = fluid.layers.fc(input=fc3, size=2, act='softmax',
         param_attr=fluid.ParamAttr(initializer=fluid.initializer.Normal(scale=1/math.sqrt(fc3.shape[1]))))
 
-    label = fluid.layers.data(name='label', shape=[1], dtype='int64')
-
-    data_list = [dense_input] + sparse_input_ids + [label]
-
-    cost = fluid.layers.cross_entropy(input=predict, label=label)
+    cost = fluid.layers.cross_entropy(input=predict, label=words[-1:])
     avg_cost = fluid.layers.reduce_sum(cost)
-    accuracy = fluid.layers.accuracy(input=predict, label=label)
-    auc_var, batch_auc_var, auc_states = fluid.layers.auc(input=predict, label=label, num_thresholds=2**12, slide_steps=20)
+    accuracy = fluid.layers.accuracy(input=predict, label=words[-1:])
+    auc_var, batch_auc_var, auc_states = \
+        fluid.layers.auc(input=predict, label=words[-1:], num_thresholds=2**12, slide_steps=20)
 
-    return avg_cost, data_list, auc_var, batch_auc_var
+    return avg_cost, auc_var, batch_auc_var, py_reader
