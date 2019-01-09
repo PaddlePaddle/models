e222caa05a81ce1d73553926cc8aa4359ffbfaaa
zhengya01 <zhengya01@baidu.com>
2019-01-09 08:38:35 +0000

    add ce

--- fluid/PaddleCV/human_pose_estimation/.run_ce.sh
+++ fluid/PaddleCV/human_pose_estimation/.run_ce.sh
@@ -16,7 +16,7 @@ export CUDA_VISIBLE_DEVICES=$cudaid
 FLAGS_benchmark=true  python train.py --dataset=coco --num_epochs=2 --batch_num=10 --enable_ce | python _ce.py
 
 
-cudaid=${face_detection_8:=0,1,2,3,4,5,6,7} # use 0,1,2,3 card as default
+cudaid=${face_detection_8:=0,1,2,3,4,5,6,7} # use 0,1,2,3,4,5,6,7 card as default
 export CUDA_VISIBLE_DEVICES=$cudaid
 
 FLAGS_benchmark=true  python train.py --dataset=coco --num_epochs=2 --batch_num=10 --enable_ce | python _ce.py
