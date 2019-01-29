a98cb3f9b40a09bec7ae23c53e15e2f258560039
zhengya01 <zhengya01@baidu.com>
2019-01-29 12:03:01 +0000

    add ce for video_classification

---
+++ fluid/PaddleCV/video_classification/.run_ce.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+
+cudaid=${video_classification:=0} # use 0-th card as default
+export CUDA_VISIBLE_DEVICES=$cudaid
+
+export FLAGS_fraction_of_gpu_memory_to_use=0.5
+FLAGS_benchmark=true  python train.py --batch_size=16 --total_videos=9537 --class_dim=101 --num_epochs=1 --image_shape=3,224,224 --model_save_dir=output/ --with_mem_opt=True --lr_init=0.01 --num_layers=50 --seg_num=7 --enable_ce=True | python _ce.py
+export FLAGS_fraction_of_gpu_memory_to_use=0.92
+
+
+cudaid=${video_classification_4:=0,1,2,3} # use 0,1,2,3 card as default
+export CUDA_VISIBLE_DEVICES=$cudaid
+
+FLAGS_benchmark=true  python train.py --batch_size=16 --total_videos=9537 --class_dim=101 --num_epochs=1 --image_shape=3,224,224 --model_save_dir=output/ --with_mem_opt=True --lr_init=0.01 --num_layers=50 --seg_num=7 --enable_ce=True | python _ce.py
+
+
+cudaid=${video_classification_8:=0,1,2,3,4,5,6,7} # use 0,1,2,3,4,5,6,7 card as default
+export CUDA_VISIBLE_DEVICES=$cudaid
+
+FLAGS_benchmark=true  python train.py --batch_size=16 --total_videos=9537 --class_dim=101 --num_epochs=1 --image_shape=3,224,224 --model_save_dir=output/ --with_mem_opt=True --lr_init=0.01 --num_layers=50 --seg_num=7 --enable_ce=True | python _ce.py
--- fluid/PaddleCV/video_classification/train.py
+++ fluid/PaddleCV/video_classification/train.py
@@ -26,6 +26,9 @@ add_arg('model_save_dir',   str,    "output",       "Model save directory.")
 add_arg('pretrained_model', str,    None,           "Whether to use pretrained model.")
 add_arg('total_videos',     int,    9537,           "Training video number.")
 add_arg('lr_init',          float,  0.01,           "Set initial learning rate.")
+add_arg('enable_ce',        bool,   False,          "If set True, enable continuous evaluation job.")
+add_arg('num_devices',      int,    1,              "Number of devices for ce")
+add_arg('num_batches',      int,    200,            "Number of batch.")
 # yapf: enable
 
 
@@ -54,6 +57,11 @@ def train(args):
     avg_cost = fluid.layers.mean(x=cost)
     acc_top1 = fluid.layers.accuracy(input=out, label=label, k=1)
     acc_top5 = fluid.layers.accuracy(input=out, label=label, k=5)
+
+    if args.enable_ce:
+        SEED = 102
+        fluid.default_main_program().random_seed = SEED
+        fluid.default_startup_program().random_seed = SEED
 
     # for test
     inference_program = fluid.default_main_program().clone(for_test=True)
@@ -92,6 +100,9 @@ def train(args):
 
     # reader
     train_reader = paddle.batch(reader.train(seg_num), batch_size=batch_size, drop_last=True)
+    if args.enable_ce:
+        train_reader = paddle.batch(reader.train_ce(seg_num), batch_size=batch_size, drop_last=False)
+
     # test in single GPU
     test_reader = paddle.batch(reader.test(seg_num), batch_size=batch_size / 16)
     feeder = fluid.DataFeeder(place=place, feed_list=[image, label])
@@ -100,15 +111,20 @@ def train(args):
 
     fetch_list = [avg_cost.name, acc_top1.name, acc_top5.name]
 
+    total_time = 0
     # train
     for pass_id in range(num_epochs):
         train_info = [[], [], []]
         test_info = [[], [], []]
         for batch_id, data in enumerate(train_reader()):
+            if args.enable_ce:
+                if batch_id > args.num_batches:
+                    break
             t1 = time.time()
             loss, acc1, acc5 = train_exe.run(fetch_list, feed=feeder.feed(data))
             t2 = time.time()
             period = t2 - t1
+            total_time += period
             loss = np.mean(np.array(loss))
             acc1 = np.mean(np.array(acc1))
             acc5 = np.mean(np.array(acc5))
@@ -130,6 +146,8 @@ def train(args):
         # test
         cnt = 0
         for batch_id, data in enumerate(test_reader()):
+            if args.enable_ce and batch_id > 3:
+                break
             t1 = time.time()
             loss, acc1, acc5 = exe.run(inference_program,
                                        fetch_list=fetch_list,
@@ -168,6 +186,23 @@ def train(args):
         if not os.path.isdir(model_path):
             os.makedirs(model_path)
         fluid.io.save_persistables(exe, model_path)
+
+    if args.enable_ce:
+        gpu_num = get_cards(args)
+        epoch_idx = num_epochs
+        print("kpis\teach_pass_duration_card%s\t%s" %
+                (gpu_num, total_time / epoch_idx))
+        print("kpis\ttrain_loss_card%s\t%s" %
+                (gpu_num, train_loss))
+
+
+def get_cards(args):
+    if args.enable_ce:
+        cards = os.environ.get('CUDA_VISIBLE_DEVICES')
+        num = len(cards.split(","))
+        return num
+    else:
+        return args.num_devices
 
 
 def main():
