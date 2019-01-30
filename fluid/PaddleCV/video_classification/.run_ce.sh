f246c2e9c6d2ff2e5748250efbf162547307afad
zhengya01 <zhengya01@baidu.com>
2019-01-30 08:12:55 +0000

    add ce

--- fluid/PaddleCV/video_classification/.run_ce.sh
+++ fluid/PaddleCV/video_classification/.run_ce.sh
@@ -9,7 +9,7 @@ export CUDA_VISIBLE_DEVICES=$cudaid
 
 export FLAGS_fraction_of_gpu_memory_to_use=0.5
 FLAGS_benchmark=true  python train.py --batch_size=16 --total_videos=9537 --class_dim=101 --num_epochs=1 --image_shape=3,224,224 --model_save_dir=output/ --with_mem_opt=True --lr_init=0.01 --num_layers=50 --seg_num=7 --enable_ce=True | python _ce.py
-#export FLAGS_fraction_of_gpu_memory_to_use=0.92
+export FLAGS_fraction_of_gpu_memory_to_use=0.92
 
 
 cudaid=${video_classification_4:=0,1,2,3} # use 0,1,2,3 card as default
