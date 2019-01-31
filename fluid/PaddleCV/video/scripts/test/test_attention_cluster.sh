630c3224db717d24f479580bbb2a54d46d0b2b03
SunGaofeng <peakbee@gmail.com>
2019-01-31 14:13:49 +0000

    fix .sh params

--- fluid/PaddleCV/video/scripts/test/test_attention_cluster.sh
+++ fluid/PaddleCV/video/scripts/test/test_attention_cluster.sh
@@ -1,2 +1,2 @@
-python3 test.py --model-name="AttentionCluster" --config=./configs/attention_cluster.txt \
+python test.py --model-name="AttentionCluster" --config=./configs/attention_cluster.txt \
                 --log-interval=5 --weights=./checkpoints/AttentionCluster_epoch0
