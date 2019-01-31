8353a83de90e52b734bb5d0bc8d287c54bf942d6
SunGaofeng <peakbee@gmail.com>
2019-01-31 13:07:22 +0000

    modify epoch and batch size

--- fluid/PaddleCV/video/scripts/test/test_nextvlad.sh
+++ fluid/PaddleCV/video/scripts/test/test_nextvlad.sh
@@ -1,2 +1,2 @@
-python3 test.py --model-name="NEXTVLAD" --config=./configs/nextvlad.txt \
+python test.py --model-name="NEXTVLAD" --config=./configs/nextvlad.txt \
                 --log-interval=10 --weights=./checkpoints/NEXTVLAD_epoch0
--- fluid/PaddleCV/video/scripts/train/train_tsn.sh
+++ fluid/PaddleCV/video/scripts/train/train_tsn.sh
@@ -1,2 +1,2 @@
-python3 train.py --model-name="TSN" --config=./configs/tsn.txt --epoch-num=6 \
+python3 train.py --model-name="TSN" --config=./configs/tsn.txt --epoch-num=45 \
                 --valid-interval=1 --save-interval=1 --log-interval=10
