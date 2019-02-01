ef119e8ed2c44a7e2d4bbd9cf897e83ea48e59ff
wangguibao <wangguibao@baidu.com>
2019-02-01 17:47:06 +0800

    Add build_raw_data.py

--- fluid/PaddleNLP/text_classification/async_executor/data_generator/build_raw_data.py
+++ fluid/PaddleNLP/text_classification/async_executor/data_generator/build_raw_data.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Build lego raw data
+Build raw data
 """
 from __future__ import print_function
 import sys
