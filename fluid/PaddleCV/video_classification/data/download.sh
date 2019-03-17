8ff41f587ff6e936f3e63f677ab7109757009823
kangyuzhe666 <1399109998@qq.com>
2019-03-17 19:25:25 +0800

    fix  invalid dataset url
    fix  invalid dataset url

--- fluid/PaddleCV/video_classification/data/download.sh
+++ fluid/PaddleCV/video_classification/data/download.sh
@@ -1,5 +1,6 @@
 # Download the dataset
 echo "Downloading..."
+
 wget https://www.crcv.ucf.edu/datasets/human-actions/ucf101/UCF101.rar
 wget https://www.crcv.ucf.edu/wp-content/uploads/2019/03/UCF101TrainTestSplits-RecognitionTask.zip
 
