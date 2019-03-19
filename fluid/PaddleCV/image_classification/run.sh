0259f8466ad2377fd231539eb81a3d5f53bbcc6f
baojun-nervana <baojun.liu@intel.com>
2019-03-19 21:58:17 +0000

    update to use ngraph

--- fluid/PaddleCV/image_classification/run.sh
+++ fluid/PaddleCV/image_classification/run.sh
@@ -192,3 +192,18 @@ python train.py \
 #	--model_category=models_name \
 #       --model_save_dir=output/ \
 #	--l2_decay=3e-4
+
+#ResNet50 nGraph:
+#FLAGS_use_ngraph=true python train.py \
+#	--model=ResNet50 \
+#	--batch_size=256 \
+#	--total_images=1281167 \
+#	--class_dim=1000 \
+#	--image_shape=3,224,224 \
+#	--lr_strategy=none \
+#	--lr=0.001 \
+#	--num_epochs=120 \
+#       --with_mem_opt=False \
+#       --model_category=models_name \
+#       --model_save_dir=output/ \
+#	--use_gpu=False 
