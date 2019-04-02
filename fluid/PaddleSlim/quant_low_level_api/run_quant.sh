f96a344b5c7ecf80610c7467eae5c3e817abae65
Zhen Wang <wangzhen31@baidu.com>
2019-04-02 15:16:41 +0800

    add checkpoint function for pass.

--- fluid/PaddleSlim/quant_low_level_api/run_quant.sh
+++ fluid/PaddleSlim/quant_low_level_api/run_quant.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 #MobileNet v1:
 python quant.py \
        --model=MobileNet \
-       --pretrained_model=../data/pretrain/MobileNetV1_pretrained \
+       --pretrained_fp32_model=../data/pretrain/MobileNetV1_pretrained \
        --use_gpu=True \
        --data_dir=../data/ILSVRC2012 \
-       --batch_size=64 \
+       --batch_size=256 \
        --total_images=1281167 \
        --class_dim=1000 \
        --image_shape=3,224,224 \
        --model_save_dir=output/ \
        --lr_strategy=piecewise_decay \
-       --num_epochs=10 \
+       --num_epochs=20 \
        --lr=0.0001 \
        --act_quant_type=abs_max \
        --wt_quant_type=abs_max
@@ -23,16 +23,16 @@ python quant.py \
 #ResNet50:
 #python quant.py \
 #       --model=ResNet50 \
-#       --pretrained_model=../data/pretrain/ResNet50_pretrained \
+#       --pretrained_fp32_model=../data/pretrain/ResNet50_pretrained \
 #       --use_gpu=True \
 #       --data_dir=../data/ILSVRC2012 \
-#       --batch_size=32 \
+#       --batch_size=128 \
 #       --total_images=1281167 \
 #       --class_dim=1000 \
 #       --image_shape=3,224,224 \
 #       --model_save_dir=output/ \
 #       --lr_strategy=piecewise_decay \
-#       --num_epochs=10 \
+#       --num_epochs=20 \
 #       --lr=0.0001 \
 #       --act_quant_type=abs_max \
 #       --wt_quant_type=abs_max
