99d836204737e46b02c135f26ba9f296014ff87b
zhangshuai28 <zhangshuai28@baidu.com>
2019-04-10 21:13:18 +0800

    fix logger

--- PaddleNLP/paddle-nlp/similarity_net/download.sh
+++ PaddleNLP/paddle-nlp/similarity_net/download.sh
@@ -2,7 +2,7 @@
 model_files_path="./model_files"
 
 #get pretrained_bow_pairwise_model
-wget https://baidu-nlp.bj.bcebos.com/simnet_bow-pairwise-1.0.0.tar.gz
+wget --no-check-certificate https://baidu-nlp.bj.bcebos.com/simnet_bow-pairwise-1.0.0.tar.gz
 if [ ! -d $model_files_path ]; then
     mkdir $model_files_path
 fi
@@ -10,7 +10,7 @@ tar xzf simnet_bow-pairwise-1.0.0.tar.gz -C $model_files_path
 rm simnet_bow-pairwise-1.0.0.tar.gz
 
 #get data
-wget  https://baidu-nlp.bj.bcebos.com/simnet_dataset-1.0.0.tar.gz
+wget --no-check-certificate https://baidu-nlp.bj.bcebos.com/simnet_dataset-1.0.0.tar.gz
 tar xzf simnet_dataset-1.0.0.tar.gz
 rm simnet_dataset-1.0.0.tar.gz
 
