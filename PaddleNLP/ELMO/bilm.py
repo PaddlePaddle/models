aa87df53603699069226cb3ab0cc33b2c402e0e6
chenjiawen <chenjiawen@baidu.com>
2019-04-11 21:21:44 +0800

    修改README，加入数据示例

--- PaddleNLP/ELMO/bilm.py
+++ PaddleNLP/ELMO/bilm.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This file is used to finetune
+# This file is used to finetune.
 
 from __future__ import absolute_import
 from __future__ import division
