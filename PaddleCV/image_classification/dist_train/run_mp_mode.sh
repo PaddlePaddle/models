3ce67970c3c19baba5858694288b74effcbc0f15
gongweibao <gongweibao@baidu.com>
2019-04-11 12:49:30 +0000

    add

--- PaddleCV/image_classification/dist_train/run_mp_mode.sh
+++ PaddleCV/image_classification/dist_train/run_mp_mode.sh
@@ -15,5 +15,5 @@ PADDLE_TRAINING_ROLE="TRAINER" \
 PADDLE_CURRENT_ENDPOINT="127.0.0.1:716${i}" \
 PADDLE_TRAINER_ID="${i}" \
 FLAGS_selected_gpus="${i}" \
-python dist_train.py --model $MODEL --update_method nccl2 --batch_size 32 --fp16 0 --data_dir ../data --scale_loss 8 &> logs/tr$i.log &
+python dist_train.py --model $MODEL --update_method nccl2 --batch_size 32 --fp16 1  --scale_loss 8 &> logs/tr$i.log &
 done
