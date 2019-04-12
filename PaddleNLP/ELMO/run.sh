a1797f01108f578e988cc8ff2516d61360249a41
xuezhong <xuezhongq@gmail.com>
2019-04-12 16:01:40 +0800

    Merge pull request #2037 from JesseyXujin/elmo_1
    Elmo 1

--- PaddleNLP/ELMO/run.sh
+++ PaddleNLP/ELMO/run.sh
@@ -1,8 +1,8 @@
 export CUDA_VISIBLE_DEVICES=0 
 python  train.py \
---train_path='baike/train/sentence_file_*'  \
---test_path='baike/dev/sentence_file_*'  \
---vocab_path baike/vocabulary_min5k.txt \
+--train_path='data/train/sentence_file_*'  \
+--test_path='data/dev/sentence_file_*'  \
+--vocab_path data/vocabulary_min5k.txt \
 --learning_rate 0.2 \
 --use_gpu True \
 --local True $@
