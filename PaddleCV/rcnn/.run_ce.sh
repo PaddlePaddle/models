bb594699d36bed70bf7a84416065d9c624d7678c
kolinwei <331911734@qq.com>
2019-04-21 20:46:44 +0800

    Update .run_ce.sh

--- PaddleCV/rcnn/.run_ce.sh
+++ PaddleCV/rcnn/.run_ce.sh
@@ -13,5 +13,5 @@ FLAGS_benchmark=true  python train.py --model_save_dir=output/ --data_dir=datase
 cudaid=${face_detection_m:=0,1,2,3} # use 0,1,2,3 card as default
 export CUDA_VISIBLE_DEVICES=$cudaid
 
-FLAGS_benchmark=true  python train.py --model_save_dir=output/ --data_dir=dataset/coco/ --max_iter=500 --enable_ce --pretrained_model=./imagenet_resnet50_fusebn --learning_rate-0.005 | python _ce.py
+FLAGS_benchmark=true  python train.py --model_save_dir=output/ --data_dir=dataset/coco/ --max_iter=500 --enable_ce --pretrained_model=./imagenet_resnet50_fusebn --learning_rate=0.005 | python _ce.py
 
