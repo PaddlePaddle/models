78693ca6b19ee96d1a8a518843b08e8079df3f8a
junjun315 <lujun315023@baidu.com>
2019-04-22 14:19:34 +0000

    update dygraph models:mnist, move resnet, test=develop

--- dygraph/mnist/mnist_dygraph.py
+++ dygraph/mnist/mnist_dygraph.py
@@ -1,20 +1,30 @@
+# Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import print_function
 
-import contextlib
 import numpy as np
-import six
-
+from PIL import Image
+import os
 import paddle
 import paddle.fluid as fluid
-from paddle.fluid.optimizer import SGDOptimizer
+from paddle.fluid.optimizer import AdamOptimizer
 from paddle.fluid.dygraph.nn import Conv2D, Pool2D, FC
 from paddle.fluid.dygraph.base import to_variable
 
 
 class SimpleImgConvPool(fluid.dygraph.Layer):
-    """
-    Conv Pool Layer
-    """
     def __init__(self,
                  name_scope,
                  num_channels,
@@ -64,9 +74,6 @@ class SimpleImgConvPool(fluid.dygraph.Layer):
 
 
 class MNIST(fluid.dygraph.Layer):
-    """
-    MNIST model
-    """
     def __init__(self, name_scope):
         super(MNIST, self).__init__(name_scope)
 
@@ -86,49 +93,110 @@ class MNIST(fluid.dygraph.Layer):
                               loc=0.0, scale=scale)),
                       act="softmax")
 
-    def forward(self, inputs):
+    def forward(self, inputs, label=None):
         x = self._simple_img_conv_pool_1(inputs)
         x = self._simple_img_conv_pool_2(x)
         x = self._fc(x)
-        return x
+        if label is not None:
+            acc = fluid.layers.accuracy(input=x, label=label)
+            return x, acc
+        else:
+            return x
+
+
+def test_train(reader, model, batch_size):
+    acc_set = []
+    avg_loss_set = []
+    for batch_id, data in enumerate(reader()):
+        dy_x_data = np.array(
+            [x[0].reshape(1, 28, 28)
+             for x in data]).astype('float32')
+        y_data = np.array(
+            [x[1] for x in data]).astype('int64').reshape(batch_size, 1)
+
+        img = to_variable(dy_x_data)
+        label = to_variable(y_data)
+        label.stop_gradient = True
+        prediction, acc = model(img, label)
+        loss = fluid.layers.cross_entropy(input=prediction, label=label)
+        avg_loss = fluid.layers.mean(loss)
+        acc_set.append(float(acc.numpy()))
+        avg_loss_set.append(float(avg_loss.numpy()))
+
+        # get test acc and loss
+    acc_val_mean = np.array(acc_set).mean()
+    avg_loss_val_mean = np.array(avg_loss_set).mean()
+
+    return avg_loss_val_mean, acc_val_mean
 
 
 def train_mnist():
-    seed = 90
-    epoch_num = 10
-
+    epoch_num = 5
+    BATCH_SIZE = 64
     with fluid.dygraph.guard():
-        fluid.default_startup_program().random_seed = seed
-        fluid.default_main_program().random_seed = seed
 
         mnist = MNIST("mnist")
-        sgd = SGDOptimizer(learning_rate=1e-3)
+        adam = AdamOptimizer(learning_rate=0.001)
         train_reader = paddle.batch(
-            paddle.dataset.mnist.train(), batch_size=128, drop_last=True)
-
+            paddle.dataset.mnist.train(), batch_size=BATCH_SIZE, drop_last=True)
+        test_reader = paddle.batch(
+            paddle.dataset.mnist.test(), batch_size=BATCH_SIZE, drop_last=True)
         for epoch in range(epoch_num):
             for batch_id, data in enumerate(train_reader()):
                 dy_x_data = np.array(
                     [x[0].reshape(1, 28, 28)
                      for x in data]).astype('float32')
                 y_data = np.array(
-                    [x[1] for x in data]).astype('int64').reshape(128, 1)
+                    [x[1] for x in data]).astype('int64').reshape(BATCH_SIZE, 1)
 
                 img = to_variable(dy_x_data)
                 label = to_variable(y_data)
                 label.stop_gradient = True
 
-                cost = mnist(img)
+                cost, acc = mnist(img, label)
+
                 loss = fluid.layers.cross_entropy(cost, label)
                 avg_loss = fluid.layers.mean(loss)
                 avg_loss.backward()
-                sgd.minimize(avg_loss)
+                adam.minimize(avg_loss)
+                # save checkpoint
                 mnist.clear_gradients()
+                if batch_id % 100 == 0:
+                    print("Loss at epoch {} step {}: {:}".format(epoch, batch_id, avg_loss.numpy()))
 
-                dy_out = avg_loss.numpy()
-                print("batch id %d, loss %f" % (batch_id, dy_out))
+            mnist.eval()
+            test_cost, test_acc = test_train(test_reader, mnist, BATCH_SIZE)
+            mnist.train()
+            print("Loss at epoch {} , Test avg_loss is: {}, acc is: {}".format(epoch, test_cost, test_acc))
+
+        fluid.dygraph.save_persistables(mnist.state_dict(), "save_dir")
+        print("checkpoint saved")
+
+    with fluid.dygraph.guard():
+
+        mnist_infer = MNIST("mnist")
+        # load checkpoint
+        mnist_infer.load_dict(
+            fluid.dygraph.load_persistables("save_dir"))
+        print("checkpoint loaded")
+
+        # start evaluate mode
+        mnist_infer.eval()
+
+        def load_image(file):
+            im = Image.open(file).convert('L')
+            im = im.resize((28, 28), Image.ANTIALIAS)
+            im = np.array(im).reshape(1, 1, 28, 28).astype(np.float32)
+            im = im / 255.0 * 2.0 - 1.0
+            return im
+
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        tensor_img = load_image(cur_dir + '/image/infer_3.png')
+
+        results = mnist_infer(to_variable(tensor_img))
+        lab = np.argsort(results.numpy())
+        print("Inference result of image/infer_3.png is: %d" % lab[0][-1])
 
 
 if __name__ == '__main__':
     train_mnist()
-
