334d8a17a1ef2da6ebcd0b50871f40516fac4d40
dengkaipeng <dengkaipeng@baidu.com>
2019-04-23 13:42:30 +0000

    add config and optimizer.
    Change-Id: I66e24851adf5cac0db1707ae12e912059b23653f

---
+++ ppdet/core/config.py
@@ -0,0 +1,94 @@
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import yaml
+from edict import AttrDict
+
+
+__all__ = ['load_cfg', 'merge_cfg']
+
+
+def load_cfg(cfg_file, fmt='yaml'):
+    """
+    Load config from given config filename, configs will be loaded as
+    AttrDict. Currently supported format: ['yaml']
+    """
+    assert os.path.exists(cfg_file), \
+        "Config file {} not exist.".format(cfg_file)
+    assert fmt in ['yaml'], \
+        "Format {} not supported.".format(fmt)
+
+    with open(cfg_file) as f:
+        try:
+            cfg = yaml.load(f)
+        except Exception, e:
+            print(e)
+            return None
+
+    return AttrDict(cfg)
+
+def _merge_cfg_a_to_b(a, b):
+    if a is None or b is None:
+        return
+
+    for k, v in a.items():
+        try:
+            v = eval(v)
+        except:
+            pass
+
+        if b.has_key(k):
+            if type(v) == type(b[k]):
+                if isinstance(v, dict):
+                    _merge_cfg_a_to_b(v, b[k])
+                else:
+                    b[k] = v
+            elif type(v) in [list, tuple] and \
+                    type(b[k]) in [list, tuple]:
+                        b[k] = tuple(v)
+            else:
+                TypeError("Type mismatch for key {}".format(k))
+        else:
+            b[k] = v
+
+def merge_cfg(cfg_from, cfg_to):
+    """
+    Merge config cfg_from to cfg_to, cfg_to has a higher priority.
+    """
+
+    assert isinstance(cfg_from, dict), \
+            "cfg_from should be a dict"
+    assert isinstance(cfg_to, dict), \
+            "cfg_to should be a dict"
+
+    _merge_cfg_a_to_b(cfg_from, cfg_to)
+
+
+if __name__ == "__main__":
+    def test_merge_cfg():
+        cfga = {"a": {"aa": 1, "ac": 4}, "b": 2, "c": 3}
+        cfgb = {"a": {"aa": 2, "ab": 3}, "b": 1}
+        merge_cfg(cfga, cfgb)
+        assert cfgb['a']['aa'] == 1
+        assert cfgb['a']['ac'] == 4
+        assert cfgb['b'] == 2
+        assert cfgb['c'] == 3
+
+    test_merge_cfg()
---
+++ ppdet/core/optimizer.py
@@ -0,0 +1,85 @@
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import paddle.fluid.optimizer as optimizer
+import paddle.fluid.layers as layers
+import paddle.fluid.regularizer as regularizer
+
+__all__ = ['OptimizerBuilder']
+
+
+class OptimizerBuilder():
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self._build_optimizer()
+
+    def get_optimizer(self):
+        return self.optimizer
+
+    def get_bn_regularizer(self):
+        return self.bn_regularizer
+    
+    def _build_optimizer(self):
+        self._build_regularizer()
+        self._build_lr_decay()
+
+        opt_params = dict()
+        for k, v in self.cfg.items():
+            if k == 'TYPE':
+                opt_func = getattr(optimizer, v)
+            elif not isinstance(v, dict):
+                opt_params.update({k.lower(): v})
+
+        self.optimizer = opt_func(regularization=self.regularizer,
+                                  learning_rate=self.lr_decay,
+                                  **opt_params)
+
+    def _build_regularizer(self):
+        reg_cfg = self.cfg.WEIGHT_DECAY
+        reg_func = getattr(regularizer, reg_cfg.TYPE+"Decay")
+        self.regularizer = reg_func(reg_cfg.FACTOR)
+        self.bn_regularizer = reg_func(reg_cfg.BN_FACTOR)
+
+    def _build_lr_decay(self):
+        policy, params = self._parse_lr_decay_cfg()
+        decay_func = getattr(layers, policy)
+        self.lr_decay = decay_func(**params)
+    
+    def _parse_lr_decay_cfg(self):
+        lr_cfg = self.cfg.LR_DECAY
+        policy = lr_cfg.POLICY
+        # parse decay params
+        params = dict()
+        for k, v in lr_cfg.items():
+            if k != 'POLICY':
+                params.update({k.lower(): v})
+        return policy, params
+
+if __name__ == "__main__":
+    from config import load_cfg
+    
+    cfg = load_cfg('./config/faster-rcnn_ResNet-50.yml')
+    ob = OptimizerBuilder(cfg.OPTIMIZER)
+    assert ob.get_optimizer() is not None
+    assert ob.get_bn_regularizer() is not None
+
+    cfg = load_cfg('./config/yolov3_darknet53_syncbn.yml')
+    ob = OptimizerBuilder(cfg.OPTIMIZER)
+    assert ob.get_optimizer() is not None
+    assert ob.get_bn_regularizer() is not None
