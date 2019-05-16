e536579f92d36816f283134f920262c0612b46f3
Hongyu Liu <43953930+phlrain@users.noreply.github.com>
2019-05-16 18:31:02 +0800

    Update reader.py (#2262)

--- PaddleNLP/language_model/reader.py
+++ PaddleNLP/language_model/reader.py
@@ -1,4 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#   Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserve.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+
 """Utilities for parsing PTB text files."""
 from __future__ import absolute_import
 from __future__ import division
