61591558d19033d25ac5154ffc2b979d50b43dbb
Kaipeng Deng <dengkaipeng@baidu.com>
2019-05-16 20:56:00 +0800

    fix video train param name epoch. (#2266)
    * fix video train param name epoch.
    
    * fix for script.

--- PaddleCV/video/scripts/train/train_stnet.sh
+++ PaddleCV/video/scripts/train/train_stnet.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-python train.py --model_name="STNET" --config=./configs/stnet.txt --epoch_num=60 \
+python train.py --model_name="STNET" --config=./configs/stnet.txt --epoch=60 \
                 --valid_interval=1 --log_interval=10
--- PaddleCV/video/scripts/train/train_tsm.sh
+++ PaddleCV/video/scripts/train/train_tsm.sh
@@ -5,5 +5,5 @@ export FLAGS_fast_eager_deletion_mode=1
 export FLAGS_eager_delete_tensor_gb=0.0
 export FLAGS_fraction_of_gpu_memory_to_use=0.98
 
-python train.py --model_name="TSM" --config=./configs/tsm.txt --epoch_num=65 \
+python train.py --model_name="TSM" --config=./configs/tsm.txt --epoch=65 \
                 --valid_interval=1 --log_interval=10
