48e066a94ea72bdeb2418e6c66aa362322bb77f5
SunAhong <sunyanfang01@baidu.com>
2019-05-22 21:33:04 +0800

    modify download.sh

--- PaddleCV/yolov3/weights/download.sh
+++ PaddleCV/yolov3/weights/download.sh
@@ -3,8 +3,8 @@ cd "$DIR"
 
 # Download the pretrain weights.
 echo "Downloading..."
-wget https://paddlemodels.bj.bcebos.com/yolo/darknet53.tar.gz
-wget https://paddlemodels.bj.bcebos.com/yolo/yolov3.tar.gz
+wget https://paddlemodels.bj.bcebos.com/yolo/darknet53.tar.gz --no-check-certificate
+wget https://paddlemodels.bj.bcebos.com/yolo/yolov3.tar.gz --no-check-certificate
 echo "Extracting..."
 tar -xf darknet53.tar.gz
 tar -xf yolov3.tar.gz
