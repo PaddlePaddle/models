3790807c3f77c0c8e9d2121850cb5cd9441cbd7b
xyzhou-puck <zhouxiangyang@yq01-sys-hic-p40-box-a12-0071.yq01.baidu.com>
2019-05-23 13:52:36 +0800

    test=develop

--- dygraph/reinforcement_learning/test_reinforce_load.py
+++ dygraph/reinforcement_learning/test_reinforce_load.py
@@ -160,31 +160,21 @@ with fluid.dygraph.guard():
         return returns
 
     running_reward = 10
-    for i_episode in count(1):
-        state, ep_reward = env.reset(), 0
-        for t in range(1, 10000):  # Don't infinite loop while learning
-            state = np.array(state).astype("float32")
-            action = select_action(state)
-            state, reward, done, _ = env.step(action)
+    state, ep_reward = env.reset(), 0
+    policy.load_dict(fluid.dygraph.load_persistables(args.save_dir))
 
-            if args.render:
-                env.render()
+    for t in range(1, 10000):  # Don't infinite loop while learning
+        state = np.array(state).astype("float32")
+        action = select_action(state)
+        state, reward, done, _ = env.step(action)
 
-            policy.rewards.append(reward)
-            ep_reward += reward
+        if args.render:
+            env.render()
 
-            if done:
-                break
+        policy.rewards.append(reward)
+        ep_reward += reward
 
-        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
-        returns = finish_episode()
-        if i_episode % args.log_interval == 0:
-            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.
-                  format(i_episode, ep_reward, running_reward))
-
-        if running_reward > env.spec.reward_threshold:
-            print("Solved! Running reward is now {} and "
-                  "the last episode runs to {} time steps!".format(
-                      running_reward, t))
-            fluid.dygraph.save_persistables(policy.state_dict(), args.save_dir)
+        if done:
             break
+
+    print('Test reward: {:.2f}'.format(ep_reward))
