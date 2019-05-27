5d01622162e7b7bca8f283ff677ce8b762843417
chalsliu <chles.liu@gmail.com>
2019-05-27 20:07:31 +0800

    Remove environment variable LD_LIBRARY_PATH

--- PaddleSlim/.run_ce.sh
+++ PaddleSlim/.run_ce.sh
@@ -4,14 +4,16 @@
 export FLAGS_eager_delete_tensor_gb=0.0
 export CUDA_VISIBLE_DEVICES=3
 
-export LD_LIBRARY_PATH=/home/work/cuda-9.2/lib64:/home/users/dangqingqing/nccl2/nccl_2.1.15-1+cuda9.0_x86_64/lib:/home/work/cudnn/cudnn_v7.1/cuda/lib64:/home/liuxudong/.jumbo/opt/gcc53/lib64:$LD_LIBRARY_PATH
-
 if [ ! -d 'pretrain' ]; then
     ln -s ${PRETRAINED_MODEL_PATH} ./pretrain
 fi
 
 if [ ! -d 'data' ]; then
     ln -s ${ILSVRC2012_DATA_PATH} ./data
+fi
+
+if [ -d 'checkpoints' ]; then
+    rm -rf checkpoints
 fi
 
 sed -i "s/epoch: 200/epoch: 5/g" configs/filter_pruning_uniform.yaml
