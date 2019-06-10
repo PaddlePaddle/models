badd725c143d18905b6095abda47eea3cee3a22d
sunyanfang01 <sunyanfang01@baidu.com>
2019-06-10 11:05:00 +0800

    fix the ssd v1
    Change-Id: Idc707282952dc2ae1d370c2a72ae16b53b3d184c

--- ppdet/models/tests/test_ssd_head.py
+++ ppdet/models/tests/test_ssd_head.py
@@ -29,11 +29,21 @@ def init_ssd_input(cfg):
         name='featuremap1', shape=[512, 19, 19], dtype='float32', lod_level=0)
     fm2 = fluid.layers.data(
         name='featuremap2', shape=[1024, 10, 10], dtype='float32', lod_level=0)
+    fm3 = fluid.layers.data(
+        name='featuremap3', shape=[512, 10, 10], dtype='float32', lod_level=0)
+    fm4 = fluid.layers.data(
+        name='featuremap4', shape=[256, 5, 5], dtype='float32', lod_level=0)
+    fm5 = fluid.layers.data(
+        name='featuremap5', shape=[256, 3, 3], dtype='float32', lod_level=0)
+    fm6 = fluid.layers.data(
+        name='featuremap6', shape=[128, 2, 2], dtype='float32', lod_level=0)
     gt_label = fluid.layers.data(
         name='gt_label', shape=[1], dtype='int32', lod_level=1)
     gt_box = fluid.layers.data(
         name='gt_box', shape=[4], dtype='float32', lod_level=1)
-    return img, fm1, fm2, gt_label, gt_box
+    is_difficult = fluid.layers.data(
+        name='difficult', shape=[1], dtype='float32', lod_level=1)
+    return img, fm1, fm2, fm3, fm4, fm5, fm6, gt_label, gt_box, is_difficult
 
 
 def test_ssd_head(cfg_file):
@@ -46,11 +56,17 @@ def test_ssd_head(cfg_file):
         img = head_inputs[0]
         fm1 = head_inputs[1]
         fm2 = head_inputs[2]
-        gt_label = head_inputs[3]
-        gt_box = head_inputs[4]
-        nmsed_out = ob.get_prediction(img, fm1, fm2)
-        loss = ob.get_loss(img, fm1, fm2, gt_box, gt_label)
-        map_eval = ob.get_map(gt_box, gt_label)
+        fm3 = head_inputs[3]
+        fm4 = head_inputs[4]
+        fm5 = head_inputs[5]
+        fm6 = head_inputs[6]
+        fms = [fm1, fm2, fm3, fm4, fm5, fm6]
+        gt_label = head_inputs[7]
+        gt_box = head_inputs[8]
+        is_difficult = head_inputs[9]
+        nmsed_out = ob.get_prediction(img, fms)
+        loss = ob.get_loss(img, fms, gt_box, gt_label)
+        map_eval = ob.get_map(gt_box, gt_label, is_difficult)
 
         assert nmsed_out is not None
         assert loss is not None
