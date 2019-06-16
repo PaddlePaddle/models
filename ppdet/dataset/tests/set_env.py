cc96a64faae87248d3b8f19b23606761264b36ae
dengkaipeng <dengkaipeng@baidu.com>
2019-06-16 05:44:00 +0000

    refine log level
    Change-Id: Iec7d1317ad1a9d24a578686773fb7cc6436ada9a

--- ppdet/dataset/tests/set_env.py
+++ ppdet/dataset/tests/set_env.py
@@ -35,7 +35,7 @@ script = os.path.join(os.path.dirname(__file__), 'data/prepare_data.sh')
 if not os.path.exists(data_root):
     ret = os.system('bash %s %s' % (script, version))
     if ret != 0:
-        logging.info('not found file[%s], you should manually prepare '
+        logging.error('not found file[%s], you should manually prepare '
             'your data using "data/prepare_data.sh"' % (data_root))
         sys.exit(1)
 
