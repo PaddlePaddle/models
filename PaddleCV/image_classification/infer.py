7935b528bd973a0b2f86c90ac1f33c927427fed9
ruri <shipeng1108@163.com>
2019-06-18 13:58:14 +0800

    fix padding bug and add resize_short_size args (#2428)
    * fix padding bug and add resize_short_size args
    
    * fix typo
    
    * pass args:settings in eval and infer

--- PaddleCV/image_classification/infer.py
+++ PaddleCV/image_classification/infer.py
@@ -7,7 +7,7 @@ import time
 import sys
 import paddle
 import paddle.fluid as fluid
-import reader
+import reader_cv2 as reader
 import argparse
 import functools
 import models
@@ -25,6 +25,7 @@ add_arg('with_mem_opt',     bool, True,                 "Whether to use memory o
 add_arg('pretrained_model', str,  None,                 "Whether to use pretrained model.")
 add_arg('model',            str,  "SE_ResNeXt50_32x4d", "Set the network to use.")
 add_arg('save_inference',   bool, False,                 "Whether to save inference model or not")
+add_arg('resize_short_size', int, 256,                  "Set resize short size")
 # yapf: enable
 
 def infer(args):
@@ -78,7 +79,7 @@ def infer(args):
         print("model: ",model_name," is already saved")
         exit(0)
     test_batch_size = 1
-    test_reader = paddle.batch(reader.test(), batch_size=test_batch_size)
+    test_reader = paddle.batch(reader.test(settings=args), batch_size=test_batch_size)
     feeder = fluid.DataFeeder(place=place, feed_list=[image])
 
     TOPK = 1
