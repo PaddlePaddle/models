55138a40a219975bcbbeb6c1edef5284de721f72
ruri <shipeng1108@163.com>
2019-06-19 21:06:01 +0800

    Add license and follow PEP 8 (#2452)

--- PaddleCV/image_classification/utils/learning_rate.py
+++ PaddleCV/image_classification/utils/learning_rate.py
@@ -1,13 +1,28 @@
+#copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+import math
+
 import paddle
 import paddle.fluid as fluid
 import paddle.fluid.layers.ops as ops
 from paddle.fluid.initializer import init_on_cpu
 from paddle.fluid.layers.learning_rate_scheduler import _decay_step_counter
-import math
-
 
 def cosine_decay(learning_rate, step_each_epoch, epochs=120):
     """Applies cosine decay to the learning rate.
