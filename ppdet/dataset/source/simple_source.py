a507f119c21fd5bed0dcd301d1498e5447b739c5
zhangyang57 <zhangyang57@baidu.com>
2019-06-20 10:32:54 +0800

    CLEANUP: simple_source.py
    Change-Id: I93669b90a1d61a2f4d4a70fedc1521caa124a763

--- ppdet/dataset/source/simple_source.py
+++ ppdet/dataset/source/simple_source.py
@@ -12,43 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#function:
+# function:
 #    interface to load data from txt file.
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __future__ import unicode_literals
 
 import os
-import random
 import numpy as np
 import copy
 from ..dataset import Dataset
 
 
 class SimpleSource(Dataset):
-    """ a simple Dataset interface used to provide testing data which are image files stored in local files.
+    """
+    Load image files for testing purpose
+
+    Args:
+        test_file (str): list of image file names, relative to `image_dir`
+        image_dir (str): root dir for images
+        samples (int): number of samples to load, -1 means all
+        load_img (bool): should images be loaded
     """
 
-    def __init__(self, test_file, image_dir=None, samples=-1, load_img=True):
-        """ Init
-
-        Args:
-            test_file (str): file names for each image which is located in 'image_dir'
-            image_dir (str): root dir for images
-            samples (int): samples to load, -1 means all
-            load_img (bool): whether load data in this class
-        """
+    def __init__(self,
+                 test_file='',
+                 image_dir=None,
+                 samples=-1,
+                 load_img=True,
+                 **kwargs):
         super(SimpleSource, self).__init__()
         self._epoch = -1
-        assert os.path.isfile(
-            test_file), 'invalid file[%s] for SimpleSource' % (test_file)
+        assert test_file != '' and os.path.isfile(test_file), \
+            "test file not found: " + test_file
         self._fname = test_file
         self._image_dir = image_dir
-        if image_dir is not None:
-            assert os.path.isdir(image_dir), 'invalid image directory[%s]' % (
-                image_dir)
+        assert image_dir is not None and os.path.isdir(image_dir), \
+            "image directory not found: " + image_dir
         self._simple = None
         self._pos = -1
         self._drained = False
@@ -56,19 +57,13 @@ class SimpleSource(Dataset):
         self._load_img = load_img
         self._imid2path = {}
 
-    def __str__(self):
-        return 'SimpleSource(fname:%s,epoch:%d,size:%d,pos:%d)' \
-            % (self._fname, self._epoch, self.size(), self._pos)
-
     def next(self):
-        """ load next sample
-        """
         if self._epoch < 0:
             self.reset()
 
         if self._pos >= self.size():
             self._drained = True
-            raise StopIteration('%s no more data' % (str(self)))
+            raise StopIteration("no more data in " + str(self))
         else:
             sample = copy.deepcopy(self._simple[self._pos])
             if self._load_img:
@@ -80,24 +75,20 @@ class SimpleSource(Dataset):
             return sample
 
     def _load(self):
-        """ load data from file
-        """
-        assert os.path.isfile(self._fname) and self._fname.endswith(
-            '.txt'), 'invalid test file path'
+        assert os.path.isfile(self._fname) and self._fname.endswith('.txt'), \
+            "invalid test file path"
         ct = 0
         records = []
         with open(self._fname, 'r') as fr:
             while True:
                 line = fr.readline().strip()
-                if not line or (self._samples > 0 \
-                        and ct >= self._samples):
+                if not line or (self._samples > 0 and ct >= self._samples):
                     break
                 rec = {'im_id': np.array([ct]), 'im_file': line}
                 self._imid2path[ct] = line
                 ct += 1
                 records.append(rec)
-        assert len(records) > 0, 'not found any test image in %s' % (
-            self._fname)
+        assert len(records) > 0, "no image file found in " + self._fname
         return records
 
     def _load_image(self, where):
@@ -106,8 +97,6 @@ class SimpleSource(Dataset):
             return f.read()
 
     def reset(self):
-        """ implementation of Dataset.reset
-        """
         if self._simple is None:
             self._simple = self._load()
 
@@ -120,22 +109,15 @@ class SimpleSource(Dataset):
         self._drained = False
 
     def size(self):
-        """ implementation of Dataset.size
-        """
         return len(self._simple)
 
     def drained(self):
-        """ implementation of Dataset.drained
-        """
-        assert self._epoch >= 0, 'The first epoch has not begin!'
+        assert self._epoch >= 0, "the first epoch has not started yet"
         return self._pos >= self.size()
 
     def epoch_id(self):
-        """ return epoch id for latest sample
-        """
         return self._epoch
 
     def get_imid2path(self):
-        """ return image id to image path map
-        """
+        """return image id to image path map"""
         return self._imid2path
