0120915ceb1896953f085f7370f59f526794358e
zhangyang57 <zhangyang57@baidu.com>
2019-06-20 10:25:00 +0800

    Fix a bug in `configure analyze`
    Change-Id: I0089c0b8a0a8568348fdfe399387e6a710330023

--- tools/configure.py
+++ tools/configure.py
@@ -38,14 +38,14 @@ MISC_CONFIG = {
 
 def dump_value(value):
     # XXX this is hackish, but collections.abc is not available in python 2
-    if hasattr(value, '__dict__') or type(value) in (dict, tuple, list):
+    if hasattr(value, '__dict__') or isinstance(value, (dict, tuple, list)):
         value = yaml.dump(value, default_flow_style=True)
         value = value.replace('\n', '')
         value = value.replace('...', '')
         return "'{}'".format(value)
     else:
         # primitive types
-        return value
+        return str(value)
 
 
 def dump_config(module, minimal=False):
