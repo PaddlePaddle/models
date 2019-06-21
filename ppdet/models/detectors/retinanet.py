9adf9e97c6f9e56cf5f2e81c639a0158f55895e4
zhangyang57 <zhangyang57@baidu.com>
2019-06-21 12:40:18 +0800

    CLEANUP: retinanet.py

--- ppdet/models/detectors/retinanet.py
+++ ppdet/models/detectors/retinanet.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@ from __future__ import division
 from __future__ import print_function
 
 import paddle.fluid as fluid
-from collections import OrderedDict
 
 from ppdet.core.workspace import register
 
