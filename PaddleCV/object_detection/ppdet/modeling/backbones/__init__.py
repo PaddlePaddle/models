58ef369cfb531a188016f4baa15ffebeadbb5d99
Yang Zhang <yangzhang@live.com>
2019-06-25 09:46:39 +0800

    Add back import shortcuts (#2527)

--- PaddleCV/object_detection/ppdet/modeling/backbones/__init__.py
+++ PaddleCV/object_detection/ppdet/modeling/backbones/__init__.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,3 +18,10 @@ from . import darknet
 from . import mobilenet
 from . import senet
 from . import fpn
+
+from .resnet import *
+from .resnext import *
+from .darknet import *
+from .mobilenet import *
+from .senet import *
+from .fpn import *
