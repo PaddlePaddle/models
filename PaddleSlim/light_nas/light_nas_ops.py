b9c0163eb96213a724628155d578afb6bb2bfc9e
yuxiyu <yuxiyu@baidu.com>
2019-06-26 15:18:39 +0800

    fixed bugs

--- PaddleSlim/light_nas/light_nas_ops.py
+++ PaddleSlim/light_nas/light_nas_ops.py
@@ -14,8 +14,6 @@
 
 import paddle.fluid as fluid
 import paddle
-import sys
-sys.path.append('..')
 from light_nas_space import get_tokens_set, LightNASSpace
 from get_ops_from_program import get_ops_from_program, write_lookup_table
 
@@ -26,11 +24,12 @@ n = len(tokens)
 all_ops = []
 for idx in range(0, n):
     current_token = tokens[idx]
-    startup_program, main_program, _, _, _, _, _ = space.create_net(current_token)
+    startup_program, main_program, _, _, _, _, _ = space.create_net(
+        current_token)
     op_params = get_ops_from_program(main_program)
     all_ops = all_ops + op_params
     all_ops = list(set(all_ops))
-    if (idx+1)%10 == 0:
+    if (idx + 1) % 10000 == 0:
         write_lookup_table(all_ops, 'lightnas_ops_tmp.txt')
         print('current file number is: {}'.format(idx))
         print('current number of ops is:', len(all_ops))
--- PaddleSlim/light_nas/light_nas_space.py
+++ PaddleSlim/light_nas/light_nas_space.py
@@ -19,6 +19,7 @@ import sys
 sys.path.append('..')
 from models import LightNASNet
 import reader
+import copy
 
 total_images = 1281167
 lr = 0.1
@@ -66,16 +67,17 @@ def get_bottleneck_params_list(var):
         params_list[i * 7 + 13] = NAS_SE[var[i * 6 + 5]]
     return params_list
 
+
 def get_tokens_set():
     """Get all possible candidate tokens.
     Returns:
         list, tokens.
     """
     tokens = []
-    base = [0, 0, 1, 0, 0, 0] # fixed layer number, shortcut, and se
+    base = [0, 0, 1, 0, 0, 0]  # fixed layer number, shortcut, and se
 
     # all possible combinations for each bottleneck
-    tokens_for_bottlenecks = [[],[],[],[],[]]
+    tokens_for_bottlenecks = [[], [], [], [], []]
     for i in range(5):
         tmp = copy.deepcopy(base)
         for j in range(len(NAS_FILTERS_MULTIPLIER)):
@@ -93,9 +95,11 @@ def get_tokens_set():
             for tokens2 in tokens_for_bottlenecks[2]:
                 for tokens3 in tokens_for_bottlenecks[3]:
                     for tokens4 in tokens_for_bottlenecks[4]:
-                        tokens.append(tokens0+tokens1+tokens2+tokens3+tokens4)
+                        tokens.append(tokens0 + tokens1 + tokens2 + tokens3 +
+                                      tokens4)
 
     return tokens
+
 
 class LightNASSpace(SearchSpace):
     def __init__(self):
