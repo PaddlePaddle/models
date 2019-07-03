f13ef67b4fb03ecf82077cfd4d5067007c4e6def
zhengya01 <zhengya01@baidu.com>
2019-07-03 10:06:00 +0000

    add rnn_search

--- PaddleNLP/unarchived/neural_machine_translation/rnn_search/_ce.py
+++ PaddleNLP/unarchived/neural_machine_translation/rnn_search/_ce.py
@@ -9,10 +9,10 @@ from kpi import CostKpi, DurationKpi, AccKpi
 
 train_ppl_card1_kpi = CostKpi('train_ppl_card1', 0.02, 0, actived=True)
 train_duration_card1_kpi = DurationKpi(
-    'train_duration_card1', 0.08, 0, actived=True)
+    'train_duration_card1', 0.02, 0, actived=True)
 train_ppl_card4_kpi = CostKpi('train_ppl_card4', 0.02, 0, actived=True)
 train_duration_card4_kpi = DurationKpi(
-    'train_duration_card4', 0.06, 0, actived=True)
+    'train_duration_card4', 0.02, 0, actived=True)
 
 tracking_kpis = [
     train_ppl_card1_kpi,
