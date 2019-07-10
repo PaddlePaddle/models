9fb7dca1da3ec36dc5ceb279fea358f3e77f2cf1
yuanpengcheng01 <yuanpengcheng01@baidu.com>
2019-07-10 19:54:11 +0800

    add webvision2018

--- PaddleCV/Research/webvision2018/infer.py
+++ PaddleCV/Research/webvision2018/infer.py
@@ -38,7 +38,6 @@ add_arg = functools.partial(add_arguments, argparser=parser)
 add_arg('use_gpu',          bool, True,                 "Whether to use GPU or not.")
 add_arg('class_dim',        int,  5000,                 "Class number.")
 add_arg('image_shape',      str,  "3,224,224",          "Input image size")
-add_arg('with_mem_opt',     bool, True,                 "Whether to use memory optimization or not.")
 add_arg('pretrained_model', str,  None,                 "Whether to use pretrained model.")
 add_arg('model',            str,  "ResNeXt101_32x4d",   "Set the network to use.")
 add_arg('save_inference',   bool, False,                "Whether to save inference model or not")
@@ -53,7 +52,6 @@ def infer(args):
     model_name = args.model
     save_inference = args.save_inference
     pretrained_model = args.pretrained_model
-    with_memory_optimization = args.with_mem_opt
     image_shape = [int(m) for m in args.image_shape.split(",")]
     model_list = [m for m in dir(models) if "__" not in m]
     assert model_name in model_list, "{} is not in lists: {}".format(args.model,
@@ -67,14 +65,10 @@ def infer(args):
         out, _, _ = model.net(input=image, class_dim=class_dim)
     else:
         out = model.net(input=image, class_dim=class_dim)
-        #out = fluid.layers.softmax(out)
 
     test_program = fluid.default_main_program().clone(for_test=True)
 
     fetch_list = [out.name]
-    if with_memory_optimization and not save_inference:
-        fluid.memory_optimize(
-            fluid.default_main_program(), skip_opt_set=set(fetch_list))
 
     place = fluid.CUDAPlace(0) if args.use_gpu else fluid.CPUPlace()
     exe = fluid.Executor(place)
@@ -101,7 +95,7 @@ def infer(args):
         result = exe.run(test_program,
                          fetch_list=fetch_list,
                          feed=feeder.feed(data))
-        
+
         result = result[0][0]
         pred_label = np.argsort(result)[::-1][:TOPK]
         print("Test-{0}-score: {1}, class {2}"
