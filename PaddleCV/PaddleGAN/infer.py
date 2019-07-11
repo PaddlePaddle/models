c004d129f2b6df3cf4970085418fb9b3319bf75f
ceci3 <592712189@qq.com>
2019-07-11 06:07:20 +0000

    fix_bn=1

--- PaddleCV/PaddleGAN/infer.py
+++ PaddleCV/PaddleGAN/infer.py
@@ -27,7 +27,7 @@ import imageio
 import glob
 from util.config import add_arguments, print_arguments
 from data_reader import celeba_reader_creator
-from util.utility import check_attribute_conflict, check_gpu
+from util.utility import check_attribute_conflict, check_gpu, save_batch_image, save_single_image
 import copy
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -144,7 +144,11 @@ def infer(args):
             tensor_label_trg_ = fluid.LoDTensor()
             tensor_img.set(real_img, place)
             tensor_label_org.set(label_org, place)
-            real_img_temp = np.squeeze(real_img).transpose([0, 2, 3, 1])
+
+            if(len(label_org) > 1):
+                real_img_temp = save_batch_image(real_img) 
+            else:
+                real_img_temp = save_single_image(real_img)
             images = [real_img_temp]
             for i in range(args.c_dim):
                 label_trg_tmp = copy.deepcopy(label_trg)
@@ -165,10 +169,14 @@ def infer(args):
                     "label_trg_": tensor_label_trg_
                 },
                               fetch_list=[fake.name])
-                fake_temp = np.squeeze(out[0]).transpose([0, 2, 3, 1])
+                if(len(label_org) > 1):
+                    fake_temp = save_batch_image(out[0]) 
+                else:
+                    fake_temp = save_single_image(out[0])
                 images.append(fake_temp)
             images_concat = np.concatenate(images, 1)
-            images_concat = np.concatenate(images_concat, 1)
+            if (len(label_org) > 1):
+                images_concat = np.concatenate(images_concat, 1)
             imageio.imwrite(args.output + "/fake_img_" + name[0], (
                 (images_concat + 1) * 127.5).astype(np.uint8))
     elif args.model_net == 'StarGAN':
@@ -187,7 +195,11 @@ def infer(args):
             tensor_label_org = fluid.LoDTensor()
             tensor_img.set(real_img, place)
             tensor_label_org.set(label_org, place)
-            real_img_temp = np.squeeze(real_img).transpose([0, 2, 3, 1])
+
+            if(len(label_org) > 1):
+                real_img_temp = save_batch_image(real_img) 
+            else:
+                real_img_temp = save_single_image(real_img)
             images = [real_img_temp]
             for i in range(args.c_dim):
                 label_trg_tmp = copy.deepcopy(label_org)
@@ -201,10 +213,14 @@ def infer(args):
                     feed={"input": tensor_img,
                           "label_trg_": tensor_label_trg},
                     fetch_list=[fake.name])
-                fake_temp = np.squeeze(out[0]).transpose([0, 2, 3, 1])
+                if(len(label_org) > 1):
+                    fake_temp = save_batch_image(out[0]) 
+                else:
+                    fake_temp = save_single_image(out[0])
                 images.append(fake_temp)
             images_concat = np.concatenate(images, 1)
-            images_concat = np.concatenate(images_concat, 1)
+            if(len(label_org) > 1):
+                images_concat = np.concatenate(images_concat, 1)
             imageio.imwrite(args.output + "/fake_img_" + name[0], (
                 (images_concat + 1) * 127.5).astype(np.uint8))
 
--- PaddleCV/PaddleGAN/util/utility.py
+++ PaddleCV/PaddleGAN/util/utility.py
@@ -266,6 +266,14 @@ def check_attribute_conflict(label_batch, attr, attrs):
     return label_batch
 
 
+def save_single_image(img):
+    res_img = np.squeeze(img).transpose([1, 2, 0])
+    return res_img
+
+def save_batch_image(img):
+    res_img = np.squeeze(img).transpose([0, 2, 3, 1])
+    return res_img
+
 def check_gpu(use_gpu):
      """
      Log error and exit when set use_gpu=true in paddlepaddle
