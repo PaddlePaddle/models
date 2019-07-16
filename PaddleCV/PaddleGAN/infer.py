3085b43e2fa018bc9f0d608fcda3a80e35efe207
ceci3 <592712189@qq.com>
2019-07-16 09:11:46 +0000

    cherry-pick-1.5

--- PaddleCV/PaddleGAN/infer.py
+++ PaddleCV/PaddleGAN/infer.py
@@ -154,9 +154,10 @@ def infer(args):
                         label_trg_tmp, attr_names[i], attr_names)
                 label_trg_ = list(
                     map(lambda x: ((x * 2) - 1) * 0.5, label_trg_tmp))
-                for j in range(len(label_org)):
-                    label_trg_[j][i] = label_trg_[j][i] * 2.0
-                tensor_label_org_.set(label_org, place)
+                if args.model_net == 'AttGAN':
+                    for k in range(len(label_org)):
+                        label_trg_[k][i] = label_trg_[k][i] * 2.0
+                tensor_label_org_.set(label_org_, place)
                 tensor_label_trg.set(label_trg, place)
                 tensor_label_trg_.set(label_trg_, place)
                 out = exe.run(feed={
--- PaddleCV/PaddleGAN/util/utility.py
+++ PaddleCV/PaddleGAN/util/utility.py
@@ -114,8 +114,8 @@ def save_test_image(epoch,
             for i in range(cfg.c_dim):
                 label_trg_tmp = copy.deepcopy(label_org)
                 label_trg_tmp[0][i] = 1.0 - label_trg_tmp[0][i]
-                label_trg = check_attribute_conflict(
-                    label_trg_tmp, attr_names[i], attr_names)
+                label_trg = check_attribute_conflict(label_trg_tmp,
+                                                     attr_names[i], attr_names)
                 tensor_label_trg = fluid.LoDTensor()
                 tensor_label_trg.set(label_trg, place)
                 fake_temp, rec_temp = exe.run(
@@ -158,9 +158,10 @@ def save_test_image(epoch,
                 label_trg_ = list(
                     map(lambda x: ((x * 2) - 1) * 0.5, label_trg_tmp))
 
-                for j in range(len(label_org)):
-                    label_trg_[j][i] = label_trg_[j][i] * 2.0
-                tensor_label_org_.set(label_org, place)
+                if cfg.model_net == 'AttGAN':
+                    for k in range(len(label_org)):
+                        label_trg_[k][i] = label_trg_[k][i] * 2.0
+                tensor_label_org_.set(label_org_, place)
                 tensor_label_trg.set(label_trg, place)
                 tensor_label_trg_.set(label_trg_, place)
                 out = exe.run(test_program,
@@ -267,19 +268,19 @@ def check_attribute_conflict(label_batch, attr, attrs):
 
 
 def check_gpu(use_gpu):
-     """
+    """
      Log error and exit when set use_gpu=true in paddlepaddle
      cpu version.
      """
-     err = "Config use_gpu cannot be set as true while you are " \
-           "using paddlepaddle cpu version ! \nPlease try: \n" \
-           "\t1. Install paddlepaddle-gpu to run model on GPU \n" \
-           "\t2. Set use_gpu as false in config file to run " \
-           "model on CPU"
+    err = "Config use_gpu cannot be set as true while you are " \
+          "using paddlepaddle cpu version ! \nPlease try: \n" \
+          "\t1. Install paddlepaddle-gpu to run model on GPU \n" \
+          "\t2. Set use_gpu as false in config file to run " \
+          "model on CPU"
 
-     try:
-         if use_gpu and not fluid.is_compiled_with_cuda():
-             logger.error(err)
-             sys.exit(1)
-     except Exception as e:
-         pass
+    try:
+        if use_gpu and not fluid.is_compiled_with_cuda():
+            logger.error(err)
+            sys.exit(1)
+    except Exception as e:
+        pass
