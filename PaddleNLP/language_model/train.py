439e9535f59675103d0c08ea2be8709bd13e2269
phlrain <phliuhongyu@126.com>
2019-07-19 12:35:18 +0000

    fix ptb reader and remove useless language model; test=develop

--- PaddleNLP/language_model/train.py
+++ PaddleNLP/language_model/train.py
@@ -178,9 +178,9 @@ def main():
 
     data_path = args.data_path
     print("begin to load data")
-    raw_data = reader.ptb_raw_data(data_path)
+    ptb_data = reader.get_ptb_data(data_path)
     print("finished load data")
-    train_data, valid_data, test_data, _ = raw_data
+    train_data, valid_data, test_data = ptb_data
 
     def generate_init_data():
         init_hidden = np.zeros(
@@ -331,7 +331,7 @@ def main():
                 cost_train = np.array(fetch_outs[0])
                 lr = np.array(fetch_outs[1])
                 init_hidden = np.array(fetch_list[2])
-                init_cell = np.array( fetch_list[3] )
+                init_cell = np.array(fetch_list[3])
 
                 total_loss += cost_train
                 iters += config.num_steps
