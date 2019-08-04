87827626b659e5e86d4c9a59412d4d68a66c769a
dengkaipeng <dengkaipeng@baidu.com>
2019-08-04 12:26:42 +0000

    add datareader and pointnet_modules

---
+++ PaddleCV/PointNet++/data/indoor3d_reader.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import os.path as osp
+import numpy as np
+import h5py
+import random
+
+
+class Indoor3DReader(object):
+    def __init__(self, data_dir, test_area="Area_5"):
+        self.data_dir = data_dir
+        self.test_area = test_area
+        self.load_data()
+
+    def _read_data_file(self, fname):
+        assert osp.isfile(fname), \
+                "{} is not a file".format(fname)
+        with open(fname) as f:
+            return [line.strip() for line in f]
+
+    def _load_h5_file(self, fname):
+        assert osp.isfile(fname), \
+                "{} is not a file".format(fname)
+        f = h5py.File(fname)
+        return f['data'][:], f['label'][:]
+
+    def load_data(self):
+        # read all_files.txt
+        all_files_fname = osp.join(self.data_dir, 'all_files.txt')
+        all_files = self._read_data_file(all_files_fname)
+
+        # read room_filelist.txt
+        room_fname = osp.join(self.data_dir, 'room_filelist.txt')
+        room_filelist = self._read_data_file(room_fname)
+
+        points, labels = [], []
+        for f in all_files:
+            h5_fname = osp.join(self.data_dir, osp.split(f)[-1]) 
+            point, label = self._load_h5_file(h5_fname)
+            points.append(point)
+            labels.append(label)
+        points = np.concatenate(points, 0)
+        labels = np.concatenate(labels, 0)
+
+        train_idxs, test_idxs = [], []
+        for i, room in enumerate(room_filelist):
+            if self.test_area in room:
+                test_idxs.append(i)
+            else:
+                train_idxs.append(i)
+
+        self.data = {}
+        self.data['train'] = {}
+        self.data['train']['points'] = points[train_idxs, ...]
+        self.data['train']['labels'] = labels[train_idxs, ...]
+        self.data['test'] = {}
+        self.data['test']['points'] = points[test_idxs, ...]
+        self.data['test']['labels'] = labels[test_idxs, ...]
+
+    def get_reader(self, batch_size, num_points, mode='train', shuffle=True):
+        assert mode in ['train', 'test'], \
+                "mode can only be 'train' or 'test'"
+        data = self.data[mode]
+        points = data['points']
+        labels = data['labels']
+
+        if shuffle:
+            idxs = np.arange(len(points))
+            np.random.shuffle(idxs)
+            points = points[idxs]
+            labels = labels[idxs]
+
+        def reader():
+            batch_out = []
+            for point, label in zip(points, labels):
+                p = point.copy()
+                l = label.copy()
+                pt_idxs = np.arange(num_points)
+                np.random.shuffle(pt_idxs)
+                batch_out.append((p[pt_idxs], l[pt_idxs]))
+
+                if len(batch_out) == batch_size:
+                    yield batch_out
+                    batch_out = []
+
+        return reader
+
+if __name__ == "__main__":
+    ir = Indoor3DReader("dataset/Indoor3DSemSeg/indoor3d_sem_seg_hdf5_data")
+
+    train_reader = ir.get_reader(32, 4096)
+    for i, data in enumerate(train_reader()):
+        print('train', i, len(data))
+
+    test_reader = ir.get_reader(32, 4096, 'test')
+    for i, data in enumerate(test_reader()):
+        print('test', i, len(data))
---
+++ PaddleCV/PointNet++/models/pointnet2_modules.py
@@ -0,0 +1,236 @@
+#  Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+"""
+Contains model utility functions.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import six
+import numpy as np
+
+import paddle.fluid as fluid
+from paddle.fluid.param_attr import ParamAttr
+from paddle.fluid.initializer import Constant
+
+
+def query_and_group(xyz, new_xyz, radius, nsample, features=None, use_xyz=True):
+    """
+    Perform query_ball and group_points
+
+    Args:
+        xyz (Variable): xyz coordiantes features with shape [B, N, 3]
+        new_xyz (Variable): centriods features with shape [B, npoint, 3]
+        radius (float32): radius of ball
+        nsample (int32): maximum number of gather features
+        features (Variable): features with shape [B, N, C]
+        use_xyz (bool): whether use xyz coordiantes features
+
+    Returns:
+        out (Variable): features with shape [B, 3 + C, npoint, nsample]
+    """
+    idx = fluid.layers.query_ball(xyz, new_xyz, radius, nsample)
+    grouped_xyz = fluid.layers.group_points(xyz, idx)
+    expand_new_xyz = fluid.layers.unsqueeze(new_xyz, axes=[2])
+    expand_new_xyz = fluid.layers.expand(expand_new_xyz, [1, 1, grouped_xyz.shape[2], 1])
+    grouped_xyz -= expand_new_xyz 
+
+    if features is not None:
+        grouped_feaures = fluid.layers.group_points(features, idx)
+        return fluid.layers.concat([grouped_xyz, grouped_feaures], axis=-1) \
+                if use_xyz else grouped_feaures
+    else:
+        assert use_xyz, "use_xyz should be True when features is None"
+        return grouped_xyz
+
+
+def group_all(xyz, features=None, use_xyz=True):
+    """
+    Group all xyz and features when npoint is None
+    See query_and_group
+    """
+    grouped_xyz = fluid.layers.unsqueeze(xyz, axes=[2])
+    if features is not None:
+        grouped_feaures = fluid.layers.unsqueeze(features, axes=[2])
+        return fluid.layers.concat([grouped_xyz, grouped_feaures], axis=1) if use_xyz else grouped_feaures
+    else:
+        return grouped_xyz
+
+
+def conv_bn(input, out_channels, bn=False, act='relu', name=None):
+    param_attr = ParamAttr(name='{}_conv_weight'.format(name))
+    bias_attr = ParamAttr(initializer=fluid.initializer.Constant(0.0),
+                          name='{}_conv_bias'.format(name)) \
+                                  if bn else None
+    out = fluid.layers.conv2d(input,
+                              num_filters=out_channels,
+                              filter_size=1,
+                              stride=1,
+                              padding=0,
+                              dilation=1,
+                              param_attr=param_attr,
+                              bias_attr=bias_attr,
+                              act=act if not bn else None)
+    if bn:
+        bn_name = name + "_bn"
+        out = fluid.layers.batch_norm(out,
+                                      act=act,
+                                      param_attr=ParamAttr(name=bn_name + "_scale"),
+                                      bias_attr=ParamAttr(name=bn_name + "_offset"),
+                                      moving_mean_name=bn_name + '_mean',
+                                      moving_variance_name=bn_name + '_var')
+
+    return out
+
+
+def MLP(features, out_channels_list, bn=False, act='relu', name=None):
+    out = features
+    for i, out_channels in enumerate(out_channels_list):
+        out = conv_bn(out, out_channels, bn=bn, act=act, name=name + "_{}".format(i))
+    return out
+        
+
+def pointnet_sa_module_msg(xyz,
+                       npoint=None,
+                       radiuss=[],
+                       nsamples=[],
+                       mlps=[],
+                       features=None,
+                       bn=True,
+                       use_xyz=True,
+                       name=None):
+    """
+    PointNet MSG(Multi-Scale Group) Set Abstraction Module
+
+    Args:
+        xyz (Variable): xyz coordiantes features with shape [B, N, 3]
+        radiuss ([float32]): list of radius of ball
+        nsamples ([int32]): list of maximum number of gather features
+        mlps ([[int32]]): list of out_channels_list
+        features (Variable): features with shape [B, C, N]
+        bn (bool): whether perform batch norm after conv2d
+        use_xyz (bool): whether use xyz coordiantes features
+
+    Returns:
+        new_xyz (Variable): centriods features with shape [B, npoint, 3]
+        out (Variable): features with shape [B, \sum_i{mlps[i][-1]}, npoint]
+    """
+    assert len(radiuss) == len(nsamples) == len(mlps), \
+            "radii, nsamples, mlps length should be same"
+
+    farthest_idx = fluid.layers.farthest_point_sampling(xyz, npoint)
+    new_xyz = fluid.layers.gather_point(xyz, farthest_idx)
+
+    outs = []
+    if features is not None:
+        for i, (radius, nsample, mlp) in enumerate(zip(radiuss, nsamples, mlps)):
+            out = query_and_group(xyz, new_xyz, radius, nsample, features, use_xyz) if npoint is not None else group_all(xyz, features, use_xyz)
+            out = fluid.layers.transpose(out, perm=[0, 3, 1, 2])
+            # mlp[0] = mlp[0] + 3 if use_xyz else mlp[0]
+            out = MLP(out, mlp, bn=bn, name=name + '_mlp{}'.format(i)) # TODO(dengkaipeng): mlp[1:] ?
+            out = fluid.layers.pool2d(out, pool_size=[1, out.shape[3]], pool_type='max')
+            out = fluid.layers.squeeze(out, axes=[-1])
+            outs.append(out)
+
+    out = fluid.layers.concat(outs, axis=1) if len(outs) > 0 else None
+
+    return (new_xyz, out)
+
+
+def pointnet_sa_module_ssg(xyz,
+                       npoint=None,
+                       radius=None,
+                       nsample=None,
+                       mlp=[],
+                       features=None,
+                       bn=True,
+                       use_xyz=True,
+                       name=None):
+    """
+    PointNet SSG(Single-Scale Group) Set Abstraction Module
+    see pointnet_sa_module_msg
+    """
+    return pointnet_sa_module_msg(xyz, npoint, [radius], [nsample], [mlp], features, bn, use_xyz, name)
+
+
+def pointnet_fp_module(unknown, known, unknown_feats, known_feats, mlp, bn=True, name=None):
+    """
+    PointNet Feature Propagation Module
+
+    Args:
+        unknown (Variable): unknown xyz coordiantes features with shape [B, N, 3]
+        known (Variable): known xyz coordiantes features with shape [B, M, 3]
+        unknown_feats (Variable): unknown features with shape [B, N, C1] to be propagated to
+        known_feats (Variable): known features with shape [B, N, C2] to be propagated from
+        mlp ([int32]): out_channels_list
+        bn (bool): whether perform batch norm after conv2d
+
+    Returns:
+        new_features (Variable): new features with shape [B, mlp[-1], N]
+    """
+    if known is None:
+        interp_feats = fluid.layers.expand()
+    else:
+        dist, idx = fluid.layers.three_nn(unknown, known, eps=1e-8)
+        dist_recip = (dist / dist) / dist; # 1.0 / dist
+        norm = fluid.layers.reduce_sum(dist_recip, dim=-1, keep_dim=True)
+        interp_feats = fluid.layers.three_interp(known_feats, dist_recip / norm, idx)
+
+    new_features = interp_feats if unknown_feats is None else \
+                    fluid.layers.concat([interp_feats, unknown_feats], axis=-1)
+    new_features = fluid.layers.transpose(new_features, perm=[0, 2, 1])
+    new_features = fluid.layers.unsqueeze(new_features, axes=[-1])
+    new_features = MLP(new_features, mlp, bn=bn, name=name + '_mlp')
+    
+    return new_features
+
+
+if __name__ == "__main__":
+    # xyz = fluid.layers.data(name='xyz', shape=[9, 3], dtype='float32')
+    # xyz_feats = fluid.layers.data(name='xyz_feats', shape=[9, 6], dtype='float32')
+    # new_xyz, out = pointnet_sa_module_msg(xyz, 2, [5.0, 10.0], [6, 3], [[3], [6]], xyz_feats, name="test")
+    #
+    # place = fluid.CUDAPlace(0)
+    # exe = fluid.Executor(place)
+    # exe.run(fluid.default_startup_program())
+    #
+    # np.random.seed(2333)
+    # xyz_np = np.random.random((2, 9, 3)).astype('float32')
+    # xyz_feats_np = np.random.random((2, 9, 6)).astype('float32')
+    # print("xyz: ", xyz_np.shape, xyz_np)
+    # print("xyz_feats: ", xyz_feats_np.shape, xyz_feats_np)
+    # ret = exe.run(fetch_list=[new_xyz.name, out.name], feed={'xyz': xyz_np, 'xyz_feats': xyz_feats_np})
+    # print("new_xyz: ", ret[0].shape, ret[0])
+    # print("out: ", ret[1].shape, ret[1])
+
+    known = fluid.layers.data(name='known', shape=[6, 3], dtype='float32')
+    unknown = fluid.layers.data(name='unknown', shape=[9, 3], dtype='float32')
+    known_feats = fluid.layers.data(name='known_feats', shape=[6, 9], dtype='float32')
+    unknown_feats = fluid.layers.data(name='unknown_feats', shape=[9, 8], dtype='float32')
+    new_features = pointnet_fp_module(known, unknown, known_feats, unknown_feats, [3], name="test")
+
+    place = fluid.CUDAPlace(0)
+    exe = fluid.Executor(place)
+    exe.run(fluid.default_startup_program())
+
+    np.random.seed(2333)
+    known_np = np.random.random((2, 6, 3)).astype('float32')
+    unknown_np = np.random.random((2, 9, 3)).astype('float32')
+    known_feats_np = np.random.random((2, 6, 9)).astype('float32')
+    unknown_feats_np = np.random.random((2, 9, 8)).astype('float32')
+    ret = exe.run(fetch_list=[new_features.name], feed={'known': known_np, 'unknown': unknown_np, 'known_feats': known_feats_np, 'unknown_feats': unknown_feats_np})
+    print(ret)
