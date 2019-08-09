2cb892cdc8fa81b5340e3f57baaf901493f021d6
gongweibao <weibao.gong@gmail.com>
2019-08-09 07:37:25 +0000

    cleanup not used ngraph

--- PaddleNLP/language_representations_kit/BERT/train.py
+++ PaddleNLP/language_representations_kit/BERT/train.py
@@ -299,21 +299,7 @@ def train(args):
         generate_neg_sample=args.generate_neg_sample)
 
     
-    # use_ngraph is for CPU only, please refer to README_ngraph.md for details
-    use_ngraph = os.getenv('FLAGS_use_ngraph')
-    if not use_ngraph:
-        """
-        train_exe = fluid.ParallelExecutor(
-            use_cuda=args.use_cuda,
-            loss_name=total_loss.name,
-            exec_strategy=exec_strategy,
-            main_program=train_program,
-            num_trainers=fleet.worker_num,
-            trainer_id=fleet.worker_index)
-        """
-        train_exe =  exe
-    else:
-        train_exe = exe
+    train_exe = exe
 
     if args.validation_set_dir and args.validation_set_dir != "":
         predict = predict_wrapper(
@@ -339,26 +325,14 @@ def train(args):
             skip_steps = args.skip_steps * fleet.worker_num()
 
             if fleet.worker_index() != 0:
-                if use_ngraph:
-                    train_exe.run(fetch_list=[], program=train_program)
-                else:
-                    train_exe.run(fetch_list=[], program=train_program)
+                train_exe.run(fetch_list=[], program=train_program)
                 continue
 
             if steps % skip_steps != 0:
-                if use_ngraph:
-                    train_exe.run(fetch_list=[], program=train_program)
-                else:
-                    train_exe.run(fetch_list=[], program=train_program)
+                train_exe.run(fetch_list=[], program=train_program)
 
             else:
-                if use_ngraph:
-                    each_next_acc, each_mask_lm_cost, each_total_cost, np_lr = train_exe.run(
-                        fetch_list=[
-                            next_sent_acc.name, mask_lm_loss.name, total_loss.name,
-                            scheduled_lr.name], program=train_program)
-                else:
-                    each_next_acc, each_mask_lm_cost, each_total_cost, np_lr = train_exe.run(
+                each_next_acc, each_mask_lm_cost, each_total_cost, np_lr = train_exe.run(
                         fetch_list=[
                             next_sent_acc.name, mask_lm_loss.name, total_loss.name,
                             scheduled_lr.name], program=train_program)
@@ -367,7 +341,6 @@ def train(args):
                 lm_cost.extend(each_mask_lm_cost)
                 cost.extend(each_total_cost)
 
-                print("feed_queue size", train_pyreader.queue.size())
                 time_end = time.time()
                 used_time = time_end - time_begin
                 epoch, current_file_index, total_file, current_file = data_reader.get_progress(
