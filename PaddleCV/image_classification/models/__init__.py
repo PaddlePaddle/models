9f70efee0566a4829a4f7109c2ef4059af7b156f
shippingwang <shipeng1108@163.com>
2019-08-15 07:51:03 +0000

    add paddle copyright

--- PaddleCV/image_classification/models/__init__.py
+++ PaddleCV/image_classification/models/__init__.py
@@ -1,3 +1,16 @@
+#copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
 from .alexnet import AlexNet
 from .mobilenet import MobileNet
 from .mobilenet_v2 import MobileNetV2, MobileNetV2_x0_25, MobileNetV2_x0_5, MobileNetV2_x1_0, MobileNetV2_x1_5, MobileNetV2_x2_0, MobileNetV2_scale
