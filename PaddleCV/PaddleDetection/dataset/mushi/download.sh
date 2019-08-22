085ee1de597470a6a2eb95a2cbbf6c614a36cfd7
dengkaipeng <dengkaipeng@baidu.com>
2019-08-22 07:05:53 +0000

    rename mushi->insect, add yolov3_mobilenet_v1_insect

--- PaddleCV/PaddleDetection/dataset/mushi/download.sh
+++
@@ -1,9 +0,0 @@
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "$DIR"
-
-# Download the data.
-echo "Downloading..."
-wget https://paddlemodels.bj.bcebos.com/mushi.tar
-# Extract the data.
-echo "Extracting..."
-tar xf mushi.tar
