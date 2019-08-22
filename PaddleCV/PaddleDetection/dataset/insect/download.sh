e9620a49707cb7fcea935ca3c6db29461eaa8ca9
dengkaipeng <dengkaipeng@baidu.com>
2019-08-22 07:13:43 +0000

    fix insect download

--- PaddleCV/PaddleDetection/dataset/insect/download.sh
+++ PaddleCV/PaddleDetection/dataset/insect/download.sh
@@ -3,7 +3,7 @@ cd "$DIR"
 
 # Download the data.
 echo "Downloading..."
-wget https://paddlemodels.bj.bcebos.com/mushi.tar
+wget https://paddlemodels.bj.bcebos.com/insect.tar
 # Extract the data.
 echo "Extracting..."
-tar xf mushi.tar
+tar xf insect.tar
