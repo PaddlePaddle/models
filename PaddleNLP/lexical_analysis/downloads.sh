a6db61c0ac787df8548a67328db5415dc4f5d689
huangdingbang <huangdingbang@baidu.com>
2019-08-29 20:09:15 +0800

    add postfix '.pdckpt' to model

--- PaddleNLP/lexical_analysis/downloads.sh
+++ PaddleNLP/lexical_analysis/downloads.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # download baseline model file to ./model_baseline/
-if [ -d ./model_baseline/ ]
+if [ -d ./model_baseline.pdckpt/ ]
 then
     echo "./model_baseline/ directory already existed, ignore download"
 else
     wget --no-check-certificate https://baidu-nlp.bj.bcebos.com/lexical_analysis-1.0.0.tar.gz
     tar xvf lexical_analysis-1.0.0.tar.gz
+    mv model_baseline model_baseline.pdckpt
     /bin/rm lexical_analysis-1.0.0.tar.gz
 fi
 
