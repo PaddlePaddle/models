168cc98496a4db926a803a130e82a1e39d3d92e3
dengkaipeng <dengkaipeng@baidu.com>
2019-09-16 09:59:49 +0000

    fix logs

--- PaddleCV/PaddleDetection/ppdet/utils/download.py
+++ PaddleCV/PaddleDetection/ppdet/utils/download.py
@@ -81,7 +81,7 @@ def get_dataset_path(path, annotation, image_dir):
     if _dataset_exists(path, annotation, image_dir):
         return path
 
-    logger.info("Dataset {} not valid for reason above, try searching {} or "
+    logger.info("Dataset {} is not valid for reason above, try searching {} or "
                 "downloading dataset...".format(
                     osp.realpath(path), DATASET_HOME))
 
@@ -125,10 +125,9 @@ def get_dataset_path(path, annotation, image_dir):
             return data_dir
 
     # not match any dataset in DATASETS
-    raise ValueError("Dataset {} not valid and cannot parse dataset type "
-                     "'{}' for automaticly downloading, automaticly "
-                     "downloading only support 'voc' and 'coco' "
-                     "currently".format(path, osp.split(path)[-1]))
+    raise ValueError("Dataset {} is not valid and cannot parse dataset type "
+                     "'{}' for automaticly downloading, which only support "
+                     "'voc' and 'coco' currently".format(path, osp.split(path)[-1]))
 
 
 def get_path(url, root_dir, md5sum=None):
@@ -174,7 +173,7 @@ def _dataset_exists(path, annotation, image_dir):
     Check if user define dataset exists
     """
     if not osp.exists(path):
-        logger.info("Config dataset_dir {} not exits, "
+        logger.info("Config dataset_dir {} is not exits, "
                 "dataset config is not valid".format(path))
         return False
 
