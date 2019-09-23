1d094f7133ff4808efc310eaefa3ac407203f15c
dengkaipeng <dengkaipeng@baidu.com>
2019-09-23 13:52:25 +0000

    revert pointnet2_modules debug

--- PaddleCV/PointRCNN/models/pointnet2_modules.py
+++ PaddleCV/PointRCNN/models/pointnet2_modules.py
@@ -73,12 +73,8 @@ def group_all(xyz, features=None, use_xyz=True):
 
 
 def conv_bn(input, out_channels, bn=True, bn_momentum=0.05, act='relu', name=None):
-    param_attr = ParamAttr(name='{}_conv_weight'.format(name),
-                           # initializer=fluid.initializer.Constant(1.376)
-                           )
-    bias_attr = ParamAttr(
-            # initializer=fluid.initializer.Constant(6.213),
-                          name='{}_conv_bias'.format(name)) \
+    param_attr = ParamAttr(name='{}_conv_weight'.format(name),)
+    bias_attr = ParamAttr(name='{}_conv_bias'.format(name)) \
                                   if not bn else False
     out = fluid.layers.conv2d(input,
                               num_filters=out_channels,
@@ -94,18 +90,14 @@ def conv_bn(input, out_channels, bn=True, bn_momentum=0.05, act='relu', name=Non
         out = fluid.layers.batch_norm(out,
                                       act=act,
 				      momentum=bn_momentum,
-                                      param_attr=ParamAttr(
-                                          # initializer=fluid.initializer.Constant(2.673),
-                                          name=bn_name + "_scale"),
-                                      bias_attr=ParamAttr(
-                                          # initializer=fluid.initializer.Constant(1.467),
-                                          name=bn_name + "_offset"),
+                                      param_attr=ParamAttr(name=bn_name + "_scale"),
+                                      bias_attr=ParamAttr(name=bn_name + "_offset"),
                                       moving_mean_name=bn_name + '_mean',
                                       moving_variance_name=bn_name + '_var')
 
     return out
 
-def fc_bn(input,out_channels,bn=False,bn_momentum=0.1,act='relu',name=None):
+def fc_bn(input,out_channels,bn=False,bn_momentum=0.05,act='relu',name=None):
     param_attr = ParamAttr(name='{}_fc_weight'.format(name),
                            initializer=fluid.initializer.Constant(2.4))
     if not bn:
