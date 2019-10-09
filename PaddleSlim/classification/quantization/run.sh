dde82e36df62a96205fea9c237b364240103185d
slf12 <slf12thuss@163.com>
2019-10-09 11:30:46 +0000

    add resnet 34 test=develop

--- PaddleSlim/classification/quantization/run.sh
+++ PaddleSlim/classification/quantization/run.sh
@@ -4,7 +4,7 @@
 root_url="http://paddle-imagenet-models-name.bj.bcebos.com"
 MobileNetV1="MobileNetV1_pretrained.tar"
 MobileNetV2="MobileNetV2_pretrained.tar"
-ResNet50="ResNet50_pretrained.tar"
+ResNet34="ResNet34_pretrained.tar"
 pretrain_dir='../pretrain'
 
 if [ ! -d ${pretrain_dir} ]; then
@@ -23,9 +23,9 @@ if [ ! -f ${MobileNetV2} ]; then
     tar xf ${MobileNetV2}
 fi
 
-if [ ! -f ${ResNet50} ]; then
-    wget ${root_url}/${ResNet50}
-    tar xf ${ResNet50}
+if [ ! -f ${ResNet34} ]; then
+    wget ${root_url}/${ResNet34}
+    tar xf ${ResNet34}
 fi
 
 cd -
@@ -37,14 +37,14 @@ export FLAGS_eager_delete_tensor_gb=0.0
 export CUDA_VISIBLE_DEVICES=0
 
 ## for quantization for mobilenet_v1
-python -u compress.py \
-    --model "MobileNet" \
-    --use_gpu 1 \
-    --batch_size 32 \
-    --pretrained_model ../pretrain/MobileNetV1_pretrained \
-    --config_file "./configs/mobilenet_v1.yaml" \
-> mobilenet_v1.log 2>&1 &
-tailf mobilenet_v1.log
+#python -u compress.py \
+#    --model "MobileNet" \
+#    --use_gpu 1 \
+#    --batch_size 256 \
+#    --pretrained_model ../pretrain/MobileNetV1_pretrained \
+#    --config_file "./configs/mobilenet_v1.yaml" \ 
+#> mobilenet_v1.log 2>&1 &
+#tailf mobilenet_v1.log
 
 ## for quantization of mobilenet_v2
 #python -u compress.py \
@@ -56,12 +56,12 @@ tailf mobilenet_v1.log
 #    > mobilenet_v2.log 2>&1 &
 #tailf mobilenet_v2.log
 
-# for compression of resnet50
-#python -u compress.py \
-#    --model "ResNet50" \
-#    --use_gpu 1 \
-#    --batch_size 32 \
-#    --pretrained_model ../pretrain/ResNet50_pretrained \
-#    --config_file "./configs/resnet50.yaml" \
-#    > resnet50.log 2>&1 &
-#tailf resnet50.log
+# for compression of resnet34
+python -u compress.py \
+    --model "ResNet50" \
+    --use_gpu 1 \
+    --batch_size 32 \
+    --pretrained_model ../pretrain/ResNet50_pretrained \
+    --config_file "./configs/resnet34.yaml" \
+  > resnet34.log 2>&1 &
+tailf resnet34.log
