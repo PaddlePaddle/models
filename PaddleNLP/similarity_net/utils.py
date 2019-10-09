f9409c71b9fb8f54d30af7b1e67d056b9e960f60
Dilyar <444138472@qq.com>
2019-10-09 17:16:08 +0800

    Update utils.py

--- PaddleNLP/similarity_net/utils.py
+++ PaddleNLP/similarity_net/utils.py
@@ -1,3 +1,4 @@
+#encoding=utf-8
 #   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# --coding=utf-8
 """
 SimNet utilities.
 """
