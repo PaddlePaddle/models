a76ed7512d98893feeba2822eef863ef0c80ebca
wanghaoshuang <wanghaoshuang@baidu.com>
2019-10-11 13:09:14 +0800

    debug demo of pruning yolov3

--- PaddleCV/PaddleDetection/slim/prune/run.sh
+++ PaddleCV/PaddleDetection/slim/prune/run.sh
@@ -1,4 +1,7 @@
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=6,7
 python compress.py \
 -c yolov3_mobilenet_v1_voc.yml \
--s yolov3_mobilenet_v1_slim.yaml
+-s yolov3_mobilenet_v1_slim.yaml \
+> run.log 2>&1 &
+
+tailf run.log
