38acff7ce18cfabcebd6a275d06133f37190e5bf
still-wait <742925032@qq.com>
2019-10-12 05:35:00 +0000

    fix Format specification

--- PaddleCV/PaddleDetection/ppdet/utils/widerface_eval_utils.py
+++ PaddleCV/PaddleDetection/ppdet/utils/widerface_eval_utils.py
@@ -49,14 +49,15 @@ def cal_iou(rect1, rect2):
 def widerface_eval(
         eval_results,
         output_eval_dir,
-        output_fname='pred_result.txt', ):
+        confs_threshold=0.3,
+        output_fname='pred_result.txt'):
     """
     Calculate ap according to prediction result list `eval_results`
     """
 
     def is_same_face(face_gt, face_pred):
         iou = cal_iou(face_gt, face_pred)
-        return iou >= 0.3
+        return iou >= 0.5
 
     def eval_single_image(faces_gt, faces_pred):
         pred_is_true = [False] * len(faces_pred)
@@ -72,56 +73,59 @@ def widerface_eval(
             pred_is_true[i] = isFace
         return pred_is_true
 
+    logger.info("Please wait a moment, calculating ap ...")
     faces_num_gt = 0
     score_res_pair = {}
     for t in eval_results:
         bboxes = t['bbox'][0]
+        keep_index = np.where(bboxes[:, 1] >= confs_threshold)[0]
+        bboxes = bboxes[keep_index,:]
         if bboxes.shape == (1, 1) or bboxes is None:
             continue
         bboxes = bboxes.tolist()
 
         gt_boxes = t['gt_box'][0].tolist()
         gt_box_lengths = t['gt_box'][1][0]
-        faces_num_gt += np.sum(gt_box_lengths)
+        faces_num_gt += len(gt_boxes) #np.sum(gt_box_lengths)
 
         pred_is_true = eval_single_image(gt_boxes, bboxes)
         for i in range(0, len(pred_is_true)):
-            nowScore = bboxes[i][1]
-            if score_res_pair.has_key(nowScore):
-                score_res_pair[nowScore].append(int(pred_is_true[i]))
+            bbox_score = bboxes[i][1]
+            if score_res_pair.has_key(bbox_score):
+                score_res_pair[bbox_score].append(int(pred_is_true[i]))
             else:
-                score_res_pair[nowScore] = [int(pred_is_true[i])]
-    keys = score_res_pair.keys()
-    keys.sort(reverse=True)
-    res_file = output_fname
+                score_res_pair[bbox_score] = [int(pred_is_true[i])]
+    score_keys = score_res_pair.keys()
+    score_keys.sort(reverse=True)
+    pred_res_file = output_fname
     if output_eval_dir != None:
-        res_file = os.path.join(output_eval_dir, output_fname)
-    outfile = open(res_file, 'w')
-    tp_num = 0
+        pred_res_file = os.path.join(output_eval_dir, output_fname)
+    pred_outfile = open(pred_res_file, 'w')
+    true_positive_num = 0
     predict_num = 0
     precision_list = []
     recall_list = []
-    outfile.write("recall falsePositiveNum precision scoreThreshold\n")
-    for i in range(len(keys)):
-        k = keys[i]
+    pred_outfile.write("recall falsePositiveNum precision scoreThreshold\n")
+    for i in range(len(score_keys)):
+        k = score_keys[i]
         v = score_res_pair[k]
         predict_num += len(v)
-        tp_num += sum(v)
-        fp_num = predict_num - tp_num
-        recall = float(tp_num) / faces_num_gt
-        precision = float(tp_num) / predict_num
-        outfile.write('{} {} {} {}\n'.format(recall, fp_num, precision, k))
-        precision_list.append(float(tp_num) / predict_num)
+        true_positive_num += sum(v)
+        false_positive_num = predict_num - true_positive_num
+        recall = float(true_positive_num) / faces_num_gt
+        precision = float(true_positive_num) / predict_num
+        pred_outfile.write('{} {} {} {}\n'.format(recall, false_positive_num, precision, k))
+        precision_list.append(float(true_positive_num) / predict_num)
         recall_list.append(recall)
     ap = precision_list[0] * recall_list[0]
     for i in range(1, len(precision_list)):
         ap += precision_list[i] * (recall_list[i] - recall_list[i - 1])
-    outfile.write('AP={}\n'.format(ap))
+    pred_outfile.write('AP={}\n'.format(ap))
 
     logger.info(
         "AP = {}\nFor more details, please checkout the evaluation res at {}"
-        .format(ap, res_file))
-    outfile.close()
+        .format(ap, pred_res_file))
+    pred_outfile.close()
     return ap
 
 
