89169c809e5cad3083f364728870b135f7fc6b1c
Hongyu Li <lihongyu04@baidu.com>
2019-10-12 14:48:53 +0800

    Add ERNIE2.0 service

--- PaddleNLP/Research/MRQA2019-D-NET/server/start.sh
+++ PaddleNLP/Research/MRQA2019-D-NET/server/start.sh
@@ -1,10 +1,25 @@
-cd bert_server
-export CUDA_VISIBLE_DEVICES=1
-sh start.sh
-cd ../xlnet_server
-export CUDA_VISIBLE_DEVICES=2
-sh serve.sh
+#!/bin/bash
+
+gpu_id=0
+
+# start ernie service
+# usage: sh start.sh port gpu_id
+cd ernie_server
+nohup sh start.sh 5118 $gpu_id > ernie.log 2>&1 &
 cd ..
 
-sleep 60
-python main_server.py 
+# start xlnet service
+cd xlnet_server
+nohup sh start.sh 5119 $gpu_id > xlnet.log 2>&1 &
+cd ..
+
+# start bert service
+cd bert_server
+nohup sh start.sh 5120 $gpu_id > bert.log 2>&1 &
+cd ..
+
+sleep 3
+# start main server
+# usage: python main_server.py --model_name
+# the model_name specifies the model to be used in the ensemble.
+nohup python main_server.py --ernie --xlnet --bert > main_server.log 2>&1 &
