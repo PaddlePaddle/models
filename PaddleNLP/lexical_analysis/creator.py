6d4ad16a3c23972bfa7e9e156cd5ac6be52f63eb
kinghuin <kinghuin_chull@163.com>
2019-10-16 12:47:04 +0800

    padding LAC/ERNIE revert label shape

--- PaddleNLP/lexical_analysis/creator.py
+++ PaddleNLP/lexical_analysis/creator.py
@@ -144,7 +144,7 @@ def create_ernie_model(args, ernie_config):
     sent_ids = fluid.layers.data(name='sent_ids', shape=[args.max_seq_len, 1], dtype='int64',lod_level=0)
     pos_ids = fluid.layers.data(name='pos_ids', shape=[args.max_seq_len, 1], dtype='int64',lod_level=0)
     input_mask = fluid.layers.data(name='input_mask', shape=[args.max_seq_len, 1], dtype='float32',lod_level=0)
-    padded_labels =fluid.layers.data(name='padded_labels', shape=[args.max_seq_len], dtype='int64',lod_level=0)
+    padded_labels =fluid.layers.data(name='padded_labels', shape=[args.max_seq_len, 1], dtype='int64',lod_level=0)
     seq_lens = fluid.layers.data(name='seq_lens', shape=[-1], dtype='int64',lod_level=0)
     squeeze_labels = fluid.layers.squeeze(padded_labels, axes=[-1])
 
