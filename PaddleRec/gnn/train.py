9711ec60ec8c4d1b92ee74425b14d6de0477a695
hutuxian <hutuxian2011@sina.cn>
2019-10-17 17:04:37 +0800

    fix some path/download problems (#3643)

--- PaddleRec/gnn/train.py
+++ PaddleRec/gnn/train.py
@@ -140,7 +140,7 @@ def train():
         except fluid.core.EOFException:
             py_reader.reset()
         logger.info("epoch loss: %.4lf" % (np.mean(epoch_sum)))
-        save_dir = args.model_path + "/epoch_" + str(i)
+        save_dir = os.path.join(args.model_path, "epoch_" + str(i))
         fetch_vars = [loss, acc]
         fluid.io.save_inference_model(save_dir, feed_list, fetch_vars, exe)
         logger.info("model saved in " + save_dir)
