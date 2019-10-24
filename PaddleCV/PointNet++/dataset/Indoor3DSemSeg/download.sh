e1451a8804bf2af1156ffc0fa11f13daf3841ccf
dengkaipeng <dengkaipeng@baidu.com>
2019-10-24 11:26:15 +0000

    add download.sh

---
+++ PaddleCV/PointNet++/dataset/Indoor3DSemSeg/download.sh
@@ -0,0 +1,5 @@
+echo "Downloading https://shapenet.cs.stanford.edu/media/indoor3d_sem_seg_hdf5_data.zip"
+wget https://shapenet.cs.stanford.edu/media/indoor3d_sem_seg_hdf5_data.zip
+
+echo "Unzip indoor3d_sem_seg_hdf5_data.zip"
+unzip indoor3d_sem_seg_hdf5_data.zip
