5909ae09e01655a78f6a2624aef35705028abba5
huangjun12 <12272008@bjtu.edu.cn>
2019-10-30 11:01:10 +0000

    add log out for tall reader

--- PaddleCV/PaddleVideo/reader/tall_reader.py
+++ PaddleCV/PaddleVideo/reader/tall_reader.py
@@ -167,7 +167,12 @@ class TALLReader(DataReader):
             # TALL model doesn't take validation during training, it will test after all the training epochs finish.
             return
         if self.mode == 'train':
+            num_sliding_clips = len(sliding_clips_tmp)
+            count = 0
             for clip_name in sliding_clips_tmp:
+                count += 1
+                logger.info('processing data ............' + str(count) + '/' +
+                            str(num_sliding_clips))
                 if clip_name.split(".")[2] == "npy":
                     movie_name = clip_name.split("_")[0]
                     for clip_sentence in self.clip_sentence_pairs:
