301a03170574eb9dcca6a0d553c3be46cb43accb
whs <wanghaoshuang@baidu.com>
2019-11-01 09:42:54 +0800

    Fix data layer of ocr model. (#3852)

--- PaddleCV/ocr_recognition/crnn_ctc_model.py
+++ PaddleCV/ocr_recognition/crnn_ctc_model.py
@@ -232,9 +232,9 @@ def ctc_infer(images, num_classes, use_cudnn=True):
 
 
 def ctc_eval(data_shape, num_classes, use_cudnn=True):
-    images = fluid.layers.data(name='pixel', shape=data_shape, dtype='float32')
-    label = fluid.layers.data(
-        name='label', shape=[1], dtype='int32', lod_level=1)
+    images = fluid.data(name='pixel', shape=[None]+data_shape, dtype='float32')
+    label = fluid.data(
+        name='label', shape=[None, 1], dtype='int32', lod_level=1)
     fc_out = encoder_net(images, num_classes, is_test=True, use_cudnn=use_cudnn)
     decoded_out = fluid.layers.ctc_greedy_decoder(
         input=fc_out, blank=num_classes)
