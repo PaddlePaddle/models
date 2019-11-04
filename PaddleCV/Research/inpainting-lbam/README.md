022db23329b73afb141fd03178b5c9d9600cce92
lichao40 <lichao40@baidu.com>
2019-11-04 11:04:39 +0800

    add update readme, add 1 pic and 1 mask for demo; test=inpainting-lbam

--- PaddleCV/Research/inpainting-lbam/README.md
+++ PaddleCV/Research/inpainting-lbam/README.md
@@ -1,11 +1,17 @@
 # Image Inpainting with Learnable Bidirectional Attention Maps.
-The PaddlePaddle implementation of Image Inpainting with Learnable Bidirectional Attention Maps in ICCV 2019, by Chaohao Xie, Shaohui Liu, Chao Li, Ming-Ming Cheng, Wangmeng Zuo, Xiao Liu, Shilei Wen, Errui Ding.
+The PaddlePaddle implementation of Image Inpainting with Learnable Bidirectional Attention Maps in ICCV 2019, by Chaohao Xie, Shaohui Liu, Chao Li, Ming-Ming Cheng, Wangmeng Zuo, Xiao Liu, Shilei Wen, Errui Ding.\
+<https://arxiv.org/abs/1909.00968>
 
 ## 1. Requirements.
-PaddlePaddle version == 1.4
-Python version == 3.6
+
+PaddlePaddle version == 1.6.\
+Python version == 3.6.\
+NCCL for multiple GPUs.
 
 ## 2. Usage.
+
+Download the pretrained models by <https://pan.baidu.com/s/1Xpgj6pcBTYYYxsAlJrFXgg>, password: apfo.\
+Run the test script.
 ```
 sh test.sh
 ```
--- PaddleCV/Research/inpainting-lbam/test.sh
+++ PaddleCV/Research/inpainting-lbam/test.sh
@@ -1,26 +1,11 @@
-export LD_LIBRARY_PATH="/home/vis/chao/local/cudnn-7.1/cuda/lib64:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH=/home/vis/chao/codes/convert2paddle/fluid_1.1.0_for_slurm/nccl_2.3.5/lib:$LD_LIBRARY_PATH
-
 mkdir -p results/paris
-mkdir -p results/places
 
 FLAGS_fraction_of_gpu_memory_to_use=0.1 \
-CUDA_VISIBLE_DEVICES=3 \
+CUDA_VISIBLE_DEVICES=0 \
 FLAGS_eager_delete_tensor_gb=0.0 \
 FLAGS_fast_eager_deletion_mode=1 \
 python -u test.py \
 --pretrained_model 'pretrained_models/LBAM_ParisStreetView' \
---imgfn 'imgs/paris/*GT*.png' \
---maskfn 'imgs/paris/*mask*.png' \
+--imgfn 'imgs/paris/pic.png' \
+--maskfn 'imgs/paris/mask.png' \
 --resultfn 'results/paris'
-
-FLAGS_fraction_of_gpu_memory_to_use=0.1 \
-CUDA_VISIBLE_DEVICES=3 \
-FLAGS_eager_delete_tensor_gb=0.0 \
-FLAGS_fast_eager_deletion_mode=1 \
-python -u test.py \
---pretrained_model 'pretrained_models/LBAM_Places10classes' \
---imgfn 'imgs/places/*GT*.png' \
---maskfn 'imgs/places/*mask*.png' \
---resultfn 'results/places'
-
