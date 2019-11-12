c9b8a01eb030a6db5e38b659c4354c7acc41309d
Exception-star <53971568+Exception-star@users.noreply.github.com>
2019-11-12 12:52:40 +0800

    Update eval.py

--- PaddleCV/Research/danet/eval.py
+++ PaddleCV/Research/danet/eval.py
@@ -18,7 +18,7 @@ import os
 os.environ['FLAGS_eager_delete_tensor_gb'] = "0.0"
 os.environ['FLAGS_fraction_of_gpu_memory_to_use'] = "0.99"
 
-# 多尺度测试非常耗时（大概15小时）， DANet101, batch_size=1, 显存需要>8G,
+# 多尺度测试比较耗时， DANet101, batch_size=1, 显存需要>8G,
 
 import paddle.fluid as fluid
 import paddle
