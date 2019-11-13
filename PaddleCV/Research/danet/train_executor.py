e688bfd48c31c8bae60da48e166ce0db4276cba9
Exception-star <53971568+Exception-star@users.noreply.github.com>
2019-11-13 20:22:48 +0800

    Update train_executor.py

--- PaddleCV/Research/danet/train_executor.py
+++ PaddleCV/Research/danet/train_executor.py
@@ -223,9 +223,7 @@ def main(args):
             # miou不是真实的
             miou, wrong, correct = mean_iou(pred, label, num_classes=num_classes)
     
-    if args.cuda:
-        gpu_id = int(os.environ('FLAGS_selected_gpus', 0))
-    place = fluid.CUDAPlace(gpu_id) if args.cuda else fluid.CPUPlace()
+    place = fluid.CUDAPlace(0) if args.cuda else fluid.CPUPlace()
     exe = fluid.Executor(place)
     exe.run(start_prog)
 
