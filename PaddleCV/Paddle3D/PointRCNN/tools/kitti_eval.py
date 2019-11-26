8c8a3bffcd429a27685f4b595d707aabc836ba1e
dengkaipeng <dengkaipeng@baidu.com>
2019-11-26 03:53:02 +0000

    refine kitti_eval

--- PaddleCV/Paddle3D/PointRCNN/tools/kitti_eval.py
+++ PaddleCV/Paddle3D/PointRCNN/tools/kitti_eval.py
@@ -13,16 +13,55 @@
 # limitations under the License.
 
 import os 
-from tools.kitti_object_eval_python.evaluate import evaluate as kitti_evaluate 
+import sys
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        "KITTI mAP evaluation script")
+    parser.add_argument(
+        '--result_dir',
+        type=str,
+        default='./result_dir',
+        help='detection result directory to evaluate')
+    parser.add_argument(
+        '--data_dir',
+        type=str,
+        default='./data',
+        help='KITTI dataset root directory')
+    parser.add_argument(
+        '--split',
+        type=str,
+        default='val',
+        help='evaluation split, default val')
+    parser.add_argument(
+        '--class_name',
+        type=str,
+        default='Car',
+        help='evaluation class name, default Car')
+    args = parser.parse_args()
+    return args
+
 
 def kitti_eval():
-    label_dir = os.path.join('data/KITTI/object/training', 'label_2')
-    split_file = os.path.join('data/KITTI', 'ImageSets', 'val.txt')
-    final_output_dir = os.path.join("./result_dir", 'final_result', 'data')
+    if float(sys.version[:3]) < 3.6:
+        print("KITTI mAP evaluation can only run with python3.6+")
+        sys.exit(1)
+
+    args = parse_args()
+
+    label_dir = os.path.join(args.data_dir, 'KITTI/object/training', 'label_2')
+    split_file = os.path.join(args.data_dir, 'KITTI/ImageSets',
+                              '{}.txt'.format(args.split))
+    final_output_dir = os.path.join(args.result_dir, 'final_result', 'data')
     name_to_class = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2}
+
+    from tools.kitti_object_eval_python.evaluate import evaluate as kitti_evaluate 
     ap_result_str, ap_dict = kitti_evaluate(
         label_dir, final_output_dir, label_split_file=split_file,
-         current_class=name_to_class["Car"])
+         current_class=name_to_class[args.class_name])
+
     print("KITTI evaluate: ", ap_result_str, ap_dict)
 
 
