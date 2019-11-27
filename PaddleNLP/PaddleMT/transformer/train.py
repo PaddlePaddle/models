d69b25ef7629573f7cecaee9116435548812b895
guosheng <guosheng@baidu.com>
2019-11-27 17:06:19 +0800

    Fix the shape desc of attn_bias in Transformer.

--- PaddleNLP/PaddleMT/transformer/train.py
+++ PaddleNLP/PaddleMT/transformer/train.py
@@ -174,10 +174,11 @@ def do_train(args):
 
             input_field_names = desc.encoder_data_input_fields + \
                     desc.decoder_data_input_fields[:-1] + desc.label_data_input_fields
+            input_descs = desc.get_input_descs(args.args)
             input_slots = [{
                 "name": name,
-                "shape": desc.input_descs[name][0],
-                "dtype": desc.input_descs[name][1]
+                "shape": input_descs[name][0],
+                "dtype": input_descs[name][1]
             } for name in input_field_names]
 
             input_field = InputField(input_slots)
