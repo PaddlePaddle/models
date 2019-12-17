fbb59695508c4b7730cff79c23d5a9e93c4e581f
shippingwang <shipeng1108@163.com>
2019-12-17 10:13:35 +0000

    remove comment

--- PaddleCV/image_classification/reader.py
+++ PaddleCV/image_classification/reader.py
@@ -289,12 +289,6 @@ class ImageNetReader:
                         data_dir=None):
         num_trainers = int(os.environ.get('PADDLE_TRAINERS_NUM', 1))
 
-        #if settings.use_gpu:
-        #    batch_size = settings.batch_size // paddle.fluid.core.get_cuda_device_count(
-        #    )
-        #else:
-        #    batch_size = settings.batch_size // int(
-        #        os.environ.get('CPU_NUM', 1))
         batch_size = self._get_single_card_bs(settings, mode)
 
         def reader():
@@ -323,7 +317,7 @@ class ImageNetReader:
                     for i in range(settings.same_feed):
                         full_lines.append(temp_file)
 
-                for line in full_lines:
+                for line in full_lines[0:64]:
 
                     img_path, label = line.split()
                     img_path = os.path.join(data_dir, img_path)
