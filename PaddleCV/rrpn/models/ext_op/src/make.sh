9376c62854ac6af0acbb29de51a15f844ef161c5
cjt222 <18222160892@163.com>
2020-01-02 04:12:14 +0000

    update make.sh

--- PaddleCV/rrpn/models/ext_op/src/make.sh
+++ PaddleCV/rrpn/models/ext_op/src/make.sh
@@ -4,46 +4,77 @@ lib_dir=$( python -c 'import paddle; print(paddle.sysconfig.get_lib())' )
 echo $include_dir
 echo $lib_dir
 
+CUDA=$1
+CUDNN=$2
+NCCL=$3
+
+if [ ! -d "$CUDA" ]; then
+echo "path of cuda not exist"
+exit
+fi
+
+if [ ! -d "$CUDNN" ]; then
+echo "path of cudnn not exist"
+exit
+fi
+
+if [ ! -d "$NCCL" ]; then
+echo "path of nccl not exist"
+exit
+fi
+
 git clone https://github.com/NVlabs/cub.git
+wget https://paddleseg.bj.bcebos.com/deploy/temp/mkldnn.zip
+unzip mkldnn.zip
 
 nvcc rrpn_generate_proposals_op.cu -c -o rrpn_generate_proposals_op.cu.o -ccbin cc -DPADDLE_WITH_MKLDNN -DPADDLE_WITH_CUDA -DEIGEN_USE_GPU -DPADDLE_USE_DSO -Xcompiler -fPIC -std=c++11 -Xcompiler -fPIC -w --expt-relaxed-constexpr -O3 -DNVCC \
     -I ${include_dir} \
-    -I ${include_dir}/third_party \
-    -I /usr/local/cuda/include \
+   -I ${include_dir}/third_party \
+    -I ${CUDA}/include \
     -I ${PWD}/mkldnn/include \
+    -I ${CUDNN}/include \
+    -I ${NCCL}/include \
     -L ${lib_dir} -lpaddle_framework \
-    -L /usr/local/cuda/targets/x86_64-linux/lib/ -lcudart
+    -L ${CUDA}/lib64 -lcudart
 
 
 nvcc rotated_anchor_generator_op.cu -c -o rotated_anchor_generator_op.cu.o -ccbin cc -DPADDLE_WITH_MKLDNN -DPADDLE_WITH_CUDA -DEIGEN_USE_GPU -DPADDLE_USE_DSO -Xcompiler -fPIC -std=c++11 -Xcompiler -fPIC -w --expt-relaxed-constexpr -O3 -DNVCC \
     -I ${include_dir} \
     -I ${include_dir}/third_party \
-    -I /usr/local/cuda/include \
+    -I ${CUDA}/include \
     -I ${PWD}/mkldnn/include \
+    -I ${CUDNN}/include \
+    -I ${NCCL}/include \
     -L ${lib_dir} -lpaddle_framework \
-    -L /usr/local/cuda/targets/x86_64-linux/lib/ -lcudart
+    -L ${CUDA}/lib64 -lcudart
 
 nvcc rrpn_box_coder_op.cu -c -o rrpn_box_coder_op.cu.o -ccbin cc -DPADDLE_WITH_MKLDNN -DPADDLE_WITH_CUDA -DEIGEN_USE_GPU -DPADDLE_USE_DSO -Xcompiler -fPIC -std=c++11 -Xcompiler -fPIC -w --expt-relaxed-constexpr -O3 -DNVCC \
     -I ${include_dir} \
     -I ${include_dir}/third_party \
-    -I /usr/local/cuda/include \
+    -I ${CUDA}/include \
     -I ${PWD}/mkldnn/include \
+    -I ${CUDNN}/include \
+    -I ${NCCL}/include \
     -L ${lib_dir} -lpaddle_framework \
-    -L /usr/local/cuda/targets/x86_64-linux/lib/ -lcudart
+    -L ${CUDA}/lib64 -lcudart
 
 nvcc rrpn_rotated_roi_align_op.cu -c -o rrpn_rotated_roi_align_op.cu.o -ccbin cc -DPADDLE_WITH_MKLDNN -DPADDLE_WITH_CUDA -DEIGEN_USE_GPU -DPADDLE_USE_DSO -Xcompiler -fPIC -std=c++11 -Xcompiler -fPIC -w --expt-relaxed-constexpr -O3 -DNVCC \
     -I ${include_dir} \
     -I ${include_dir}/third_party \
-    -I /usr/local/cuda/include \
+    -I ${CUDA}/include \
+    -I ${CUDNN}/include \
+    -I ${NCCL}/include \
     -I ${PWD}/mkldnn/include \
     -L ${lib_dir} -lpaddle_framework \
-    -L /usr/local/cuda/targets/x86_64-linux/lib/ -lcudart
+    -L ${CUDA}/lib64 -lcudart
 
 
 g++ rotated_anchor_generator_op.cc concat_and_split.cc rrpn_generate_proposal_labels_op.cc rrpn_generate_proposals_op.cc rrpn_target_assign_op.cc rrpn_box_coder_op.cc rrpn_rotated_roi_align_op.cc rrpn_rotated_roi_align_op.cu.o rrpn_box_coder_op.cu.o rotated_anchor_generator_op.cu.o rrpn_generate_proposals_op.cu.o -o rrpn_lib.so -shared -fPIC -std=c++11 -O3 -DPADDLE_WITH_MKLDNN -DPADDLE_WITH_CUDA -DEIGEN_USE_GPU -DPADDLE_USE_DSO \
   -I ${include_dir} \
   -I ${include_dir}/third_party \
-  -I /usr/local/cuda/include \
+  -I ${CUDA}/include \
   -I ${PWD}/mkldnn/include \
+  -I ${CUDNN}/include \
+  -I ${NCCL}/include \
   -L ${lib_dir} -lpaddle_framework \
-  -L /usr/local/cuda/targets/x86_64-linux/lib/ -lcudart 
+  -L ${CUDA}/lib64 -lcudart 
