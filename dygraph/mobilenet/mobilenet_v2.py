80cbdf27ee4706660924f942b33b9b7ea8320055
chajchaj <57249073+chajchaj@users.noreply.github.com>
2020-01-19 17:29:15 +0800

    up to paddle 1.7 (#4216)

--- dygraph/mobilenet/mobilenet_v2.py
+++ dygraph/mobilenet/mobilenet_v2.py
@@ -25,7 +25,7 @@ import paddle.fluid as fluid
 from paddle.fluid.initializer import MSRA
 from paddle.fluid.param_attr import ParamAttr
 from paddle.fluid.layer_helper import LayerHelper
-from paddle.fluid.dygraph.nn import Conv2D, Pool2D, BatchNorm, FC
+from paddle.fluid.dygraph.nn import Conv2D, Pool2D, BatchNorm, Linear
 from paddle.fluid.dygraph.base import to_variable
 
 from paddle.fluid import framework
@@ -36,19 +36,19 @@ import sys
 
 class ConvBNLayer(fluid.dygraph.Layer):
     def __init__(self,
+                 num_channels,
                  filter_size,
                  num_filters,
                  stride,
                  padding,
                  channels=None,
                  num_groups=1,
-                 name=None,
                  use_cudnn=True):
-        super(ConvBNLayer, self).__init__(name)
+        super(ConvBNLayer, self).__init__()
 
-        tmp_param = ParamAttr(name=name + "_weights")
+        tmp_param = ParamAttr(name=self.full_name() + "_weights")
         self._conv = Conv2D(
-            self.full_name(),
+            num_channels=num_channels,
             num_filters=num_filters,
             filter_size=filter_size,
             stride=stride,
@@ -60,12 +60,11 @@ class ConvBNLayer(fluid.dygraph.Layer):
             bias_attr=False)
 
         self._batch_norm = BatchNorm(
-            self.full_name(),
             num_filters,
-            param_attr=ParamAttr(name=name + "_bn" + "_scale"),
-            bias_attr=ParamAttr(name=name + "_bn" + "_offset"),
-            moving_mean_name=name + "_bn" + '_mean',
-            moving_variance_name=name + "_bn" + '_variance')
+            param_attr=ParamAttr(name=self.full_name() + "_bn" + "_scale"),
+            bias_attr=ParamAttr(name=self.full_name() + "_bn" + "_offset"),
+            moving_mean_name=self.full_name() + "_bn" + '_mean',
+            moving_variance_name=self.full_name() + "_bn" + '_variance')
 
     def forward(self, inputs, if_act=True):
         y = self._conv(inputs)
@@ -76,18 +75,19 @@ class ConvBNLayer(fluid.dygraph.Layer):
 
 
 class InvertedResidualUnit(fluid.dygraph.Layer):
-    def __init__(self,
-                 num_in_filter,
-                 num_filters,
-                 stride,
-                 filter_size,
-                 padding,
-                 expansion_factor,
-                 name=None):
-        super(InvertedResidualUnit, self).__init__(name)
+    def __init__(
+            self,
+            num_channels,
+            num_in_filter,
+            num_filters,
+            stride,
+            filter_size,
+            padding,
+            expansion_factor, ):
+        super(InvertedResidualUnit, self).__init__()
         num_expfilter = int(round(num_in_filter * expansion_factor))
         self._expand_conv = ConvBNLayer(
-            name=name + "_expand",
+            num_channels=num_channels,
             num_filters=num_expfilter,
             filter_size=1,
             stride=1,
@@ -95,7 +95,7 @@ class InvertedResidualUnit(fluid.dygraph.Layer):
             num_groups=1)
 
         self._bottleneck_conv = ConvBNLayer(
-            name=name + "_dwise",
+            num_channels=num_expfilter,
             num_filters=num_expfilter,
             filter_size=filter_size,
             stride=stride,
@@ -104,7 +104,7 @@ class InvertedResidualUnit(fluid.dygraph.Layer):
             use_cudnn=False)
 
         self._linear_conv = ConvBNLayer(
-            name=name + "_linear",
+            num_channels=num_expfilter,
             num_filters=num_filters,
             filter_size=1,
             stride=1,
@@ -121,11 +121,11 @@ class InvertedResidualUnit(fluid.dygraph.Layer):
 
 
 class InvresiBlocks(fluid.dygraph.Layer):
-    def __init__(self, in_c, t, c, n, s, name=None):
-        super(InvresiBlocks, self).__init__(name)
+    def __init__(self, in_c, t, c, n, s):
+        super(InvresiBlocks, self).__init__()
 
         self._first_block = InvertedResidualUnit(
-            name=name + "_1",
+            num_channels=in_c,
             num_in_filter=in_c,
             num_filters=c,
             stride=s,
@@ -137,14 +137,14 @@ class InvresiBlocks(fluid.dygraph.Layer):
         for i in range(1, n):
             tmp = self.add_sublayer(
                 sublayer=InvertedResidualUnit(
-                    name=name + "_" + str(i + 1),
+                    num_channels=c,
                     num_in_filter=c,
                     num_filters=c,
                     stride=1,
                     filter_size=3,
                     padding=1,
                     expansion_factor=t),
-                name=name + "_" + str(i + 1))
+                name=self.full_name() + "_" + str(i + 1))
             self._inv_blocks.append(tmp)
 
     def forward(self, inputs):
@@ -155,8 +155,8 @@ class InvresiBlocks(fluid.dygraph.Layer):
 
 
 class MobileNetV2(fluid.dygraph.Layer):
-    def __init__(self, name, class_dim=1000, scale=1.0):
-        super(MobileNetV2, self).__init__(name)
+    def __init__(self, class_dim=1000, scale=1.0):
+        super(MobileNetV2, self).__init__()
         self.scale = scale
         self.class_dim = class_dim
 
@@ -172,7 +172,7 @@ class MobileNetV2(fluid.dygraph.Layer):
 
         #1. conv1 
         self._conv1 = ConvBNLayer(
-            name="conv1_1",
+            num_channels=3,
             num_filters=int(32 * scale),
             filter_size=3,
             stride=2,
@@ -187,34 +187,30 @@ class MobileNetV2(fluid.dygraph.Layer):
             i += 1
             tmp = self.add_sublayer(
                 sublayer=InvresiBlocks(
-                    name='conv' + str(i),
-                    in_c=in_c,
-                    t=t,
-                    c=int(c * scale),
-                    n=n,
-                    s=s),
+                    in_c=in_c, t=t, c=int(c * scale), n=n, s=s),
                 name='conv' + str(i))
             self._invl.append(tmp)
             in_c = int(c * scale)
 
         #3. last_conv
+        self._out_c = int(1280 * scale) if scale > 1.0 else 1280
         self._conv9 = ConvBNLayer(
-            name="conv9",
-            num_filters=int(1280 * scale) if scale > 1.0 else 1280,
+            num_channels=in_c,
+            num_filters=self._out_c,
             filter_size=1,
             stride=1,
             padding=0)
 
         #4. pool
-        self._pool2d_avg = Pool2D(
-            name_scope="pool", pool_type='avg', global_pooling=True)
+        self._pool2d_avg = Pool2D(pool_type='avg', global_pooling=True)
 
         #5. fc
-        tmp_param = ParamAttr(name="fc10_weights")
-        self._fc = FC(name_scope="fc",
-                      size=class_dim,
-                      param_attr=tmp_param,
-                      bias_attr=ParamAttr(name="fc10_offset"))
+        tmp_param = ParamAttr(name=self.full_name() + "fc10_weights")
+        self._fc = Linear(
+            self._out_c,
+            class_dim,
+            param_attr=tmp_param,
+            bias_attr=ParamAttr(name="fc10_offset"))
 
     def forward(self, inputs):
         y = self._conv1(inputs, if_act=True)
@@ -222,5 +218,6 @@ class MobileNetV2(fluid.dygraph.Layer):
             y = inv(y)
         y = self._conv9(y, if_act=True)
         y = self._pool2d_avg(y)
+        y = fluid.layers.reshape(y, shape=[-1, self._out_c])
         y = self._fc(y)
         return y
