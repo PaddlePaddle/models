930d1fe4723c1ea52c203184be7fcba33a3bc6e6
root <root@bjyz-sys-gpu-kongming4.bjyz.baidu.com>
2020-02-07 14:03:16 +0000

    fix multi cpu bug

--- PaddleCV/image_classification/infer.py
+++ PaddleCV/image_classification/infer.py
@@ -36,7 +36,7 @@ from utils import *
 parser = argparse.ArgumentParser(description=__doc__)
 # yapf: disable
 add_arg = functools.partial(add_arguments, argparser=parser)
-add_arg('data_dir',         str,  "./data/ILSVRC2012/val/", "The ImageNet data")
+add_arg('data_dir',         str,  "./data/ILSVRC2012/test/", "The ImageNet data")
 add_arg('use_gpu',          bool, True,                 "Whether to use GPU or not.")
 add_arg('class_dim',        int,  1000,                 "Class number.")
 parser.add_argument("--pretrained_model", default=None, required=True, type=str, help="The path to load pretrained model")
@@ -54,7 +54,7 @@ add_arg('interpolation',    int,  None,                 "The interpolation mode"
 add_arg('padding_type',     str,  "SAME",               "Padding type of convolution")
 add_arg('use_se',           bool, True,                 "Whether to use Squeeze-and-Excitation module for EfficientNet.")
 add_arg('image_path',       str,  None,                 "single image path")
-add_arg('batch_size',       int,  8,                    "batch_size on all the devices")
+add_arg('batch_size',       int,  4,                    "batch_size on all the devices")
 add_arg('save_json_path',        str,  "test_res.json",            "save output to a json file")
 # yapf: enable
 
@@ -106,9 +106,10 @@ def infer(args):
     place = fluid.CUDAPlace(gpu_id) if args.use_gpu else fluid.CPUPlace()
     exe = fluid.Executor(place)
     exe.run(fluid.default_startup_program())
-    places = place
     if args.use_gpu:
         places = fluid.framework.cuda_places()
+    else:
+        places = fluid.framework.cpu_places()
     compiled_program = fluid.compiler.CompiledProgram(
         test_program).with_data_parallel(places=places)
 
@@ -147,7 +148,8 @@ def infer(args):
     info = {}
     parallel_data = []
     parallel_id = []
-    place_num = paddle.fluid.core.get_cuda_device_count() if args.use_gpu else 1
+    place_num = paddle.fluid.core.get_cuda_device_count(
+    ) if args.use_gpu else int(os.environ.get('CPU_NUM', 1))
     if os.path.exists(args.save_json_path):
         logger.warning("path: {} Already exists! will recover it\n".format(
             args.save_json_path))
@@ -160,6 +162,7 @@ def infer(args):
             parallel_data.append(image_data)
 
             if place_num == len(parallel_data):
+                print("&&&&&", place_num)
                 result = exe.run(
                     compiled_program,
                     fetch_list=fetch_list,
