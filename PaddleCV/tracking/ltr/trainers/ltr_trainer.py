6967b7abb146af9b0e8cd01f0e0eeb97dbe77b06
Double_V <liuvv0203@163.com>
2020-02-27 13:20:12 +0800

    do not print stack frame when train process killed (#4346)
    * do not print stack frame when train process killed
    * add note about VOT does not support windows platform!

--- PaddleCV/tracking/ltr/trainers/ltr_trainer.py
+++ PaddleCV/tracking/ltr/trainers/ltr_trainer.py
@@ -10,6 +10,25 @@ import paddle.fluid.dygraph as dygraph
 import time
 import numpy as np
 
+import sys
+import signal
+
+
+# handle terminate reader process, do not print stack frame
+def _reader_quit(signum, frame):
+    print("Reader process exit.")
+    sys.exit()
+
+
+def _term_group(sig_num, frame):
+    print('pid {} terminated, terminate group '
+          '{}...'.format(os.getpid(), os.getpgrp()))
+    os.killpg(os.getpgid(os.getpid()), signal.SIGKILL)
+
+
+signal.signal(signal.SIGTERM, _reader_quit)
+signal.signal(signal.SIGINT, _term_group)
+
 
 class LTRTrainer(BaseTrainer):
     def __init__(self, actor, loaders, optimizer, settings, lr_scheduler=None):
