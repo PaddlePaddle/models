ff6d280c37171e801be313dc1a71695b7ac37a25
Chengmo <chengmo@baidu.com>
2020-03-02 19:57:17 +0800

    Fix distribute (#4368)
    * fix distribute local_cluster.sh

--- PaddleRec/ctr/dnn/local_cluster.sh
+++ PaddleRec/ctr/dnn/local_cluster.sh
@@ -25,9 +25,14 @@ export PADDLE_TRAINER_ID=0
 export PADDLE_TRAINERS_NUM=2
 export OUTPUT_PATH="output"
 
-export FLAGS_communicator_thread_pool_size=5
+export FLAGS_communicator_thread_pool_size=10
 export FLAGS_communicator_fake_rpc=0
 export FLAGS_communicator_is_sgd_optimizer=0
+
+# follow parameter = cpu_num
+export FLAGS_communicator_send_queue_size=2
+export FLAGS_communicator_max_merge_var_num=2
+
 export FLAGS_rpc_retry_times=3
 export PADDLE_PSERVERS_IP_PORT_LIST="127.0.0.1:36011,127.0.0.1:36012"
 export PADDLE_PSERVER_PORT_ARRAY=(36011 36012)
@@ -48,7 +53,7 @@ do
     echo "PADDLE WILL START PSERVER "$cur_port
     export PADDLE_PORT=${cur_port}
     export POD_IP=127.0.0.1
-    python -u train.py --save_model=1 --is_cloud=1 --cpu_num=10 &> ./log/pserver.$i.log &
+    python -u train.py --save_model=1 --is_cloud=1 --cpu_num=2 &> ./log/pserver.$i.log &
 done
 
 export TRAINING_ROLE=TRAINER
@@ -59,7 +64,7 @@ for((i=0;i<$PADDLE_TRAINERS;i++))
 do
     echo "PADDLE WILL START Trainer "$i
     PADDLE_TRAINER_ID=$i
-    python -u train.py --save_model=1 --is_cloud=1 --cpu_num=10 &> ./log/trainer.$i.log &
+    python -u train.py --save_model=1 --is_cloud=1 --cpu_num=2 &> ./log/trainer.$i.log &
 done
 
 echo "Training log stored in ./log/"
