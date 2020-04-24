3a7ed32e1ccc25884197f0fc1fc442fae547399f
slf12 <slf12thuss@163.com>
2020-04-24 07:45:13 +0000

    add mobilenetv3

--- PaddleCV/image_classification/models/mobilenet_v3.py
+++ PaddleCV/image_classification/models/mobilenet_v3.py
@@ -1,31 +1,15 @@
-#copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
-#
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import paddle.fluid as fluid
 from paddle.fluid.initializer import MSRA
 from paddle.fluid.param_attr import ParamAttr
+import math
 
 __all__ = [
     'MobileNetV3', 'MobileNetV3_small_x0_25', 'MobileNetV3_small_x0_5',
     'MobileNetV3_small_x0_75', 'MobileNetV3_small_x1_0',
     'MobileNetV3_small_x1_25', 'MobileNetV3_large_x0_25',
     'MobileNetV3_large_x0_5', 'MobileNetV3_large_x0_75',
-    'MobileNetV3_large_x1_0', 'MobileNetV3_large_x1_25'
+    'MobileNetV3_large_x1_0', 'MobileNetV3_large_x1_25',
+    'MobileNetV3_large_x2_0'
 ]
 
 
@@ -72,8 +56,7 @@ class MobileNetV3():
             self.cls_ch_squeeze = 576
             self.cls_ch_expand = 1280
         else:
-            raise NotImplementedError("mode[" + model_name +
-                                      "_model] is not implemented!")
+            raise NotImplementedError
 
     def net(self, input, class_dim=1000):
         scale = self.scale
@@ -86,13 +69,15 @@ class MobileNetV3():
         conv = self.conv_bn_layer(
             input,
             filter_size=3,
-            num_filters=int(scale * inplanes),
+            #num_filters=int(scale*inplanes),
+            num_filters=inplanes if scale <= 1.0 else int(inplanes * scale),
             stride=2,
             padding=1,
             num_groups=1,
             if_act=True,
             act='hard_swish',
             name='conv1')
+        print(conv.shape)
         i = 0
         for layer_cfg in cfg:
             conv = self.residual_unit(
@@ -105,6 +90,7 @@ class MobileNetV3():
                 filter_size=layer_cfg[0],
                 use_se=layer_cfg[3],
                 name='conv' + str(i + 2))
+
             inplanes = int(scale * layer_cfg[2])
             i += 1
 
@@ -129,12 +115,14 @@ class MobileNetV3():
             act=None,
             param_attr=ParamAttr(name='last_1x1_conv_weights'),
             bias_attr=False)
+        #conv = fluid.layers.hard_swish(conv)
         conv = self.hard_swish(conv)
-        drop = fluid.layers.dropout(x=conv, dropout_prob=0.2)
-        out = fluid.layers.fc(input=drop,
-                              size=class_dim,
-                              param_attr=ParamAttr(name='fc_weights'),
-                              bias_attr=ParamAttr(name='fc_offset'))
+        out = fluid.layers.fc(
+            input=conv,
+            size=class_dim,
+            #   act='softmax',
+            param_attr=ParamAttr(name='fc_weights'),
+            bias_attr=ParamAttr(name='fc_offset'))
         return out
 
     def conv_bn_layer(self,
@@ -162,14 +150,21 @@ class MobileNetV3():
         bn_name = name + '_bn'
         bn = fluid.layers.batch_norm(
             input=conv,
-            param_attr=ParamAttr(name=bn_name + "_scale"),
-            bias_attr=ParamAttr(name=bn_name + "_offset"),
+            param_attr=ParamAttr(
+                name=bn_name + "_scale",
+                regularizer=fluid.regularizer.L2DecayRegularizer(
+                    regularization_coeff=0.0)),
+            bias_attr=ParamAttr(
+                name=bn_name + "_offset",
+                regularizer=fluid.regularizer.L2DecayRegularizer(
+                    regularization_coeff=0.0)),
             moving_mean_name=bn_name + '_mean',
             moving_variance_name=bn_name + '_variance')
         if if_act:
             if act == 'relu':
                 bn = fluid.layers.relu(bn)
             elif act == 'hard_swish':
+                #bn = fluid.layers.hard_swish(bn)
                 bn = self.hard_swish(bn)
         return bn
 
@@ -194,6 +189,7 @@ class MobileNetV3():
             act='hard_sigmoid',
             param_attr=ParamAttr(name=name + '_2_weights'),
             bias_attr=ParamAttr(name=name + '_2_offset'))
+
         scale = fluid.layers.elementwise_mul(x=input, y=conv2, axis=0)
         return scale
 
@@ -208,21 +204,19 @@ class MobileNetV3():
                       use_se=False,
                       name=None):
 
-        first_conv = (num_out_filter != num_mid_filter)
         input_data = input
-        if first_conv:
-            input = self.conv_bn_layer(
-                input=input,
-                filter_size=1,
-                num_filters=num_mid_filter,
-                stride=1,
-                padding=0,
-                if_act=True,
-                act=act,
-                name=name + '_expand')
+        conv0 = self.conv_bn_layer(
+            input=input,
+            filter_size=1,
+            num_filters=num_mid_filter,
+            stride=1,
+            padding=0,
+            if_act=True,
+            act=act,
+            name=name + '_expand')
 
         conv1 = self.conv_bn_layer(
-            input=input,
+            input=conv0,
             filter_size=filter_size,
             num_filters=num_mid_filter,
             stride=stride,
@@ -230,11 +224,15 @@ class MobileNetV3():
             if_act=True,
             act=act,
             num_groups=num_mid_filter,
-            use_cudnn=True,
+            use_cudnn=False,
             name=name + '_depthwise')
+
         if use_se:
-            conv1 = self.se_block(
-                input=conv1, num_out_filter=num_mid_filter, name=name + '_se')
+            with fluid.name_scope('se_block_skip'):
+                conv1 = self.se_block(
+                    input=conv1,
+                    num_out_filter=num_mid_filter,
+                    name=name + '_se')
 
         conv2 = self.conv_bn_layer(
             input=conv1,
@@ -297,4 +295,9 @@ def MobileNetV3_large_x1_0():
 
 def MobileNetV3_large_x1_25():
     model = MobileNetV3(model_name='large', scale=1.25)
+    return model
+
+
+def MobileNetV3_large_x2_0():
+    model = MobileNetV3(model_name='large', scale=2.0)
     return model
