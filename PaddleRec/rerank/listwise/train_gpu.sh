63e1ac3352b2d5b6e3c12618b84664a54a768841
overlord <515704170@qq.com>
2020-05-18 21:43:40 +0800

    add listwise

---
+++ PaddleRec/rerank/listwise/train_gpu.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python train.py --use_gpu 1 --epochs 3 --batch_size 32 --model_dir './model_dir' --embd_dim 16 --hidden_size 128 --item_vocab 200 --user_vocab 200 --item_len 5 --sample_size 100 --base_lr 0.01
