60d045d307e8abf5a0d806e3bc189695df7038b4
Leo Chen <chenqiuliang@baidu.com>
2020-10-22 06:25:58 -0500

    support enable_addto (#4909)

--- PaddleCV/image_classification/scripts/train/ResNet50_fp16.sh
+++ PaddleCV/image_classification/scripts/train/ResNet50_fp16.sh
@@ -4,11 +4,17 @@ export FLAGS_conv_workspace_size_limit=4000 #MB
 export FLAGS_cudnn_exhaustive_search=1
 export FLAGS_cudnn_batchnorm_spatial_persistent=1
 
+
 DATA_DIR="Your image dataset path, e.g. /work/datasets/ILSVRC2012/"
 
 DATA_FORMAT="NHWC"
 USE_FP16=true #whether to use float16
 USE_DALI=true
+USE_ADDTO=true
+
+if ${USE_ADDTO} ;then
+    export FLAGS_max_inplace_grad_add=8
+fi
 
 if ${USE_DALI}; then
     export FLAGS_fraction_of_gpu_memory_to_use=0.8
@@ -31,6 +37,7 @@ python train.py \
        --fuse_elewise_add_act_ops=true \
        --fuse_bn_act_ops=true \
        --fuse_bn_add_act_ops=true \
+       --enable_addto=${USE_ADDTO} \
        --validate=true \
        --is_profiler=false \
        --profiler_path=profile/ \
