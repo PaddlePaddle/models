5cb194ae0f9fc3b57090c4d7de71292dc6adecf4
zhiqiu <chenqiuliang@baidu.com>
2020-10-30 11:19:16 +0800

    add training script

---
+++ PaddleCV/image_classification/scripts/train/MobileNetV3_fp16.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -ex
+
+export FLAGS_conv_workspace_size_limit=4000 #MB
+export FLAGS_cudnn_exhaustive_search=1
+export FLAGS_cudnn_batchnorm_spatial_persistent=1
+
+
+DATA_DIR="./data/ILSVRC2012/"
+
+DATA_FORMAT="NCHW"
+USE_FP16=true #whether to use float16
+USE_DALI=true
+USE_ADDTO=true
+
+if ${USE_FP16} ;then
+    DATA_FORMAT="NHWC"
+fi
+
+if ${USE_ADDTO} ;then
+    export FLAGS_max_inplace_grad_add=8
+fi
+
+if ${USE_DALI}; then
+    export FLAGS_fraction_of_gpu_memory_to_use=0.8
+fi
+
+#nvprof -o mobilenet_fp32 -f --cpu-profiling off  --profile-from-start off python train.py \
+python train.py \
+       --model=MobileNetV3 \
+       --data_dir=${DATA_DIR} \
+       --batch_size=128 \
+	   --num_epochs=240 \
+       --total_images=1281167 \
+       --image_shape 3 224 224 \
+       --class_dim=1000 \
+       --print_step=10 \
+       --model_save_dir=output/ \
+       --lr_strategy=cosine_decay \
+       --use_fp16=${USE_FP16} \
+       --scale_loss=128.0 \
+       --use_dynamic_loss_scaling=true \
+       --data_format=${DATA_FORMAT} \
+       --fuse_elewise_add_act_ops=true \
+       --fuse_bn_act_ops=true \
+       --fuse_bn_add_act_ops=true \
+       --enable_addto=${USE_ADDTO} \
+       --validate=true \
+       --is_profiler=false \
+       --profiler_path=profile/ \
+       --reader_thread=10 \
+       --reader_buf_size=4000 \
+       --use_dali=${USE_DALI} \
+       --lr=0.1 \
+	   --l2_decay=4e-5
+
