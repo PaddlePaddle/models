3238bdbaae801f7d28883779cbc7f3bb1c482225
cuicheng01 <45199522+cuicheng01@users.noreply.github.com>
2020-11-06 16:01:40 +0800

    Add InceptionV3 architecture (#4937)
    * Create inception_v3.py
    
    * Update __init__.py
    
    * Create InceptionV3.sh

--- PaddleCV/image_classification/models/__init__.py
+++ PaddleCV/image_classification/models/__init__.py
@@ -23,6 +23,7 @@ from .resnet_vc import ResNet50_vc, ResNet101_vc, ResNet152_vc
 from .resnet_vd import ResNet18_vd, ResNet34_vd, ResNet50_vd, ResNet101_vd, ResNet152_vd, ResNet200_vd
 from .resnext import ResNeXt50_64x4d, ResNeXt101_64x4d, ResNeXt152_64x4d, ResNeXt50_32x4d, ResNeXt101_32x4d, ResNeXt152_32x4d
 from .resnext_vd import ResNeXt50_vd_64x4d, ResNeXt101_vd_64x4d, ResNeXt152_vd_64x4d, ResNeXt50_vd_32x4d, ResNeXt101_vd_32x4d, ResNeXt152_vd_32x4d
+from .inception_v3 import InceptionV3
 from .inception_v4 import InceptionV4
 from .se_resnet_vd import SE_ResNet18_vd, SE_ResNet34_vd, SE_ResNet50_vd, SE_ResNet101_vd, SE_ResNet152_vd, SE_ResNet200_vd
 from .se_resnext import SE_ResNeXt50_32x4d, SE_ResNeXt101_32x4d, SE_ResNeXt152_32x4d
