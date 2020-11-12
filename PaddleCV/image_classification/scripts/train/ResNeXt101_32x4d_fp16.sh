b3509b81e6d8e482bc6e78222d155ec620b36fdd
huangxu96 <huangxu_app@163.com>
2020-11-12 03:57:15 +0000

    Added training script

---
+++ PaddleCV/image_classification/scripts/train/ResNeXt101_32x4d_fp16.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -ex
+
+export CUDA_VISIBLE_DEVICES=5
+export FLAGS_conv_workspace_size_limit=4000 #MB
+export FLAGS_cudnn_exhaustive_search=0
+export FLAGS_cudnn_batchnorm_spatial_persistent=1
+
+DATA_DIR="/ssd3/datasets/ILSVRC2012"
+
+DATA_FORMAT="NHWC"
+USE_FP16=false #whether to use float16
+USE_DALI=true
+USE_ADDTO=true
+
+if ${USE_ADDTO} ;then
+    export FLAGS_max_inplace_grad_add=8
+fi
+
+if ${USE_DALI}; then
+    export FLAGS_fraction_of_gpu_memory_to_use=0.8
+fi
+
+nvprof -o timeline_output -f --cpu-profiling off --profile-from-start off python train.py \
+       --model=ResNeXt101_32x4d \
+       --data_dir=${DATA_DIR} \
+       --batch_size=32 \
+       --total_images=1281167 \
+       --image_shape 4 224 224 \
+       --class_dim=1000 \
+       --print_step=10 \
+       --model_save_dir=output/ \
+       --lr_strategy=piecewise_decay \
+       --use_fp16=${USE_FP16} \
+       --scale_loss=128.0 \
+       --use_dynamic_loss_scaling=true \
+       --data_format=${DATA_FORMAT} \
+       --fuse_elewise_add_act_ops=true \
+       --fuse_bn_act_ops=true \
+       --fuse_bn_add_act_ops=true \
+       --enable_addto=${USE_ADDTO} \
+       --validate=true \
+       --is_profiler=false \
+       --profiler_path=profile/ \
+       --reader_thread=10 \
+       --reader_buf_size=4000 \
+       --use_dali=${USE_DALI} \
+       --lr=0.1
+
+
