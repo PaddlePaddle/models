3c3704d1967146d172d169f337b8720d1992f06a
smallv0221 <397551318@qq.com>
2020-12-07 04:02:41 +0000

    update lrscheduler

--- PaddleNLP/examples/language_model/rnnlm/train.py
+++ PaddleNLP/examples/language_model/rnnlm/train.py
@@ -40,20 +40,17 @@ def train(args):
     gloabl_norm_clip = paddle.nn.ClipGradByGlobalNorm(args.max_grad_norm)
     cross_entropy = CrossEntropyLossForLm()
     ppl_metric = Perplexity()
-    callback = UpdateModel(
-        base_lr=args.base_lr,
-        lr_decay=args.lr_decay,
-        epoch_start_decay=args.epoch_start_decay)
-
+    callback = UpdateModel()
+    scheduler = paddle.callbacks.LRScheduler(by_step=False, by_epoch=True)
     model = paddle.Model(network)
-    # FIXME(yuanxiaopeng): Use scheduler instead of callback
-    #scheduler = paddle.optimizer.lr.LambdaDecay(learning_rate=args.base_lr, lr_lambda=lambda x: args.lr_decay**max(x + 1 - args.epoch_start_decay, 0.0), verbose=True)
-    optimizer = paddle.optimizer.SGD(learning_rate=args.base_lr,
+
+    learning_rate = paddle.optimizer.lr.LambdaDecay(learning_rate=args.base_lr, lr_lambda=lambda x: args.lr_decay**max(x + 1 - args.epoch_start_decay, 0.0), verbose=True)
+    optimizer = paddle.optimizer.SGD(learning_rate=learning_rate,
                                      parameters=model.parameters(),
                                      grad_clip=gloabl_norm_clip)
 
     model.prepare(optimizer=optimizer, loss=cross_entropy, metrics=ppl_metric)
-
+    
     if args.init_from_ckpt:
         model.load(args.init_from_ckpt)
         print("Loaded checkpoint from %s" % args.init_from_ckpt)
@@ -62,7 +59,7 @@ def train(args):
               eval_data=valid_loader,
               epochs=args.max_epoch,
               shuffle=False,
-              callbacks=[callback],
+              callbacks=[callback,scheduler],
               log_freq=max(1, len(train_loader) // 10))
 
     model.save(path='checkpoint/test')  # save for training
