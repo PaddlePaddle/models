e4a88bec14a767740c98ecf66085b3a493ddbe64
smallv0221 <397551318@qq.com>
2020-12-07 05:25:02 +0000

    add pre-commit

--- PaddleNLP/examples/language_model/rnnlm/train.py
+++ PaddleNLP/examples/language_model/rnnlm/train.py
@@ -13,9 +13,9 @@ paddle.seed(102)
 
 
 def create_data_loader(batch_size, num_steps, data_path):
-    train_ds = PTBDataset(batch_size, num_steps,'train')
-    valid_ds = PTBDataset(batch_size, num_steps,'eval')
-    test_ds = PTBDataset(batch_size, num_steps,'test')
+    train_ds = PTBDataset(batch_size, num_steps, 'train')
+    valid_ds = PTBDataset(batch_size, num_steps, 'eval')
+    test_ds = PTBDataset(batch_size, num_steps, 'test')
 
     train_loader = DataLoader(train_ds, return_list=True, batch_size=None)
     valid_loader = DataLoader(valid_ds, return_list=True, batch_size=None)
@@ -45,13 +45,16 @@ def train(args):
     scheduler = paddle.callbacks.LRScheduler(by_step=False, by_epoch=True)
     model = paddle.Model(network)
 
-    learning_rate = paddle.optimizer.lr.LambdaDecay(learning_rate=args.base_lr, lr_lambda=lambda x: args.lr_decay**max(x + 1 - args.epoch_start_decay, 0.0), verbose=True)
+    learning_rate = paddle.optimizer.lr.LambdaDecay(
+        learning_rate=args.base_lr,
+        lr_lambda=lambda x: args.lr_decay**max(x + 1 - args.epoch_start_decay, 0.0),
+        verbose=True)
     optimizer = paddle.optimizer.SGD(learning_rate=learning_rate,
                                      parameters=model.parameters(),
                                      grad_clip=gloabl_norm_clip)
 
     model.prepare(optimizer=optimizer, loss=cross_entropy, metrics=ppl_metric)
-    
+
     if args.init_from_ckpt:
         model.load(args.init_from_ckpt)
         print("Loaded checkpoint from %s" % args.init_from_ckpt)
@@ -60,7 +63,7 @@ def train(args):
               eval_data=valid_loader,
               epochs=args.max_epoch,
               shuffle=False,
-              callbacks=[callback,scheduler],
+              callbacks=[callback, scheduler],
               log_freq=max(1, len(train_loader) // 10))
 
     model.save(path='checkpoint/test')  # save for training
