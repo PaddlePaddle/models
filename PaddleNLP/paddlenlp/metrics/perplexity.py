4cae84235bf9b87006cd6f80bf90fa7cd92722dd
LiuChiaChi <709153940@qq.com>
2020-12-08 07:19:25 +0000

    update ppl, inherited from Metric, suitable for common mt and lm task.

--- PaddleNLP/paddlenlp/metrics/perplexity.py
+++ PaddleNLP/paddlenlp/metrics/perplexity.py
@@ -15,33 +15,59 @@
 import paddle
 import numpy as np
 
+import paddle.nn.functional as F
+
 
 class Perplexity(paddle.metric.Metric):
-    def __init__(self, name='Perplexity', *args, **kwargs):
+    """
+    Perplexity is calculated using cross entropy. It supports both padding data
+    and no padding data.
+
+    If data is not padded, users should provide `seq_len` for `Metric`
+    initialization. If data is padded, your label should contain `seq_mask`,
+    which indicates the actual length of samples.
+
+    This Perplexity requires that the output of your network is prediction,
+    label and sequence length (opitonal). If the Perplexity here doesn't meet
+    your needs, you could override the `compute` or `update` method for
+    caculating Perplexity.
+
+    Args:
+        seq_len(int): Sequence length of each sample, it must be provided while
+            data is not padded. Default: 20.
+        name(str): Name of `Metric` instance. Default: 'Perplexity'.
+
+    """
+
+    def __init__(self, seq_len=20, name='Perplexity', *args, **kwargs):
         super(Perplexity, self).__init__(*args, **kwargs)
         self._name = name
-        self.total_ce = 0
-        self.num_batch = 0
+        self.total_cost = 0
+        self.word_count = 0
+        self.word_num = seq_len
 
-    def update(self, y, label, *args):
-        # Perplexity is calculated using cross entropy
-        label = paddle.to_tensor(label)
-        y = paddle.to_tensor(y)
-        label = paddle.unsqueeze(label, axis=2)
+    def compute(self, pred, label, seq_mask=None):
+        cost = F.softmax_with_cross_entropy(
+            logits=pred, label=label, soft_label=False)
+        cost = paddle.squeeze(cost, axis=[2])
+        word_num = self.word_num
 
-        ce = paddle.nn.functional.softmax_with_cross_entropy(
-            logits=y, label=label, soft_label=False)
-        ce = paddle.mean(ce)
+        if seq_mask is not None:
+            cost = cost * seq_mask
+            word_num = paddle.sum(seq_mask)
+        batch_cost = paddle.sum(cost)
+        return batch_cost, word_num
 
-        self.total_ce += ce.numpy()[0]
-        self.num_batch += 1
+    def update(self, batch_cost, word_num):
+        self.total_cost += batch_cost
+        self.word_count += word_num
 
     def reset(self):
-        self.total_ce = 0
-        self.num_batch = 0
+        self.total_cost = 0
+        self.word_count = 0
 
     def accumulate(self):
-        return np.exp(self.total_ce / self.num_batch)
+        return np.exp(self.total_cost / self.word_count)
 
     def name(self):
         return self._name
