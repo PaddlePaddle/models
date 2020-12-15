a01ed4399a02377386ab16a7602465d3d2687713
LiuChiaChi <709153940@qq.com>
2020-12-15 11:28:00 +0000

    update __init__

--- PaddleNLP/paddlenlp/__init__.py
+++ PaddleNLP/paddlenlp/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '2.0.0a6'
+__version__ = '2.0.0a8'
 
 from . import data
 from . import datasets
