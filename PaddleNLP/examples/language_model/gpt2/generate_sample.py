8c9eb3b3ef8d5219ec2b0ed669e7bf0518e284b4
Zhong Hui <zhonghui.net@gmail.com>
2021-01-24 22:46:11 +0800

    add readme

--- PaddleNLP/examples/language_model/gpt2/generate_sample.py
+++ PaddleNLP/examples/language_model/gpt2/generate_sample.py
@@ -1,61 +1,53 @@
 import paddle
 import argparse
 import numpy as np
+from paddlenlp.utils.tools import loadz
 from paddlenlp.transformers import GPT2Model, GPT2ForPretraining
 from paddlenlp.transformers import GPT2ChineseTokenizer
-
-tokenizer = GPT2ChineseTokenizer.from_pretrained("gpt2-base-cn")
-token_ids = tokenizer.convert_tokens_to_ids("中国")
-print(token_ids)
-
-print('正在加载模型，耗时需要几分钟，请稍后...')
-model = GPT2ForPretraining.from_pretrained("gpt2-base-cn")
-model.eval()
-print('模型加载完成.')
+from paddlenlp.utils.log import logger
 
 
-# 基础预测函数
-def predict(text, max_len=10):
-    ids = tokenizer.convert_tokens_to_ids(text)
-    input_id = paddle.to_tensor(np.array(ids).reshape(1, -1).astype('int64'))
-    output, cached_kvs = model(input_id, use_cache=True, cache=None)
-    nid = int(np.argmax(output[0, -1].numpy()))
-    ids += [nid]
-    out = [nid]
-    print(np.mean(output.numpy()))
-    for i in range(max_len):
+class Demo:
+    def __init__(self):
+        self.tokenizer = GPT2ChineseTokenizer.from_pretrained("gpt2-base-cn")
+        logger.info('Loading the model parameters, please wait...')
+        self.model = GPT2ForPretraining.from_pretrained("gpt2-base-cn")
+        self.model.eval()
+        logger.info('Model loaded.')
+
+# prediction function
+
+    def predict(self, text, max_len=10):
+        ids = self.tokenizer.convert_tokens_to_ids(text)
         input_id = paddle.to_tensor(
-            np.array([nid]).reshape(1, -1).astype('int64'))
-        output, cached_kvs = model(input_id, use_cache=True, cache=cached_kvs)
-        print(np.mean(output.numpy()))
+            np.array(ids).reshape(1, -1).astype('int64'))
+        output, cached_kvs = self.model(input_id, use_cache=True, cache=None)
         nid = int(np.argmax(output[0, -1].numpy()))
         ids += [nid]
-        # 若遇到'\n'则结束预测
-        if nid == 3:
-            break
-        out.append(nid)
-    print(tokenizer.convert_ids_to_tokens(out))
+        out = [nid]
+        for i in range(max_len):
+            input_id = paddle.to_tensor(
+                np.array([nid]).reshape(1, -1).astype('int64'))
+            output, cached_kvs = self.model(
+                input_id, use_cache=True, cache=cached_kvs)
+            nid = int(np.argmax(output[0, -1].numpy()))
+            ids += [nid]
+            # if nid is '\n', the predicion is over.
+            if nid == 3:
+                break
+            out.append(nid)
+        logger.info(text)
+        logger.info(self.tokenizer.convert_ids_to_tokens(out))
 
+    # One shot example
+    def ask_question(self, question, max_len=10):
+        self.predict("问题：中国的首都是哪里？答案：北京。\n问题：%s 答案：" % question, max_len)
 
-# 问答
-def ask_question(question, max_len=10):
-    predict('''问题：中国的首都是哪里？
-    答案：北京。
-    问题：李白在哪个朝代？
-    答案：唐朝。
-    问题：%s
-    答案：''' % question, max_len)
+    # dictation poetry
+    def dictation_poetry(self, front, max_len=10):
+        self.predict('''默写古诗: 大漠孤烟直，长河落日圆。\n%s''' % front, max_len)
 
-
-def ask_question(question, max_len=10):
-    predict("问题：中国的首都是哪里？答案：北京。\n问题：%s 答案：" % question, max_len)
-
-
-# 古诗默写
-def dictation_poetry(front, max_len=10):
-    predict('''默写古诗:
-    白日依山尽，黄河入海流。
-    %s，''' % front, max_len)
-
-
-dictation_poetry("黄河之水天上来")
+if __name__ == "__main__":
+    demo = Demo()
+    demo.ask_question("百度的厂长是谁?")
+    demo.dictation_poetry("举杯邀明月，")
--- PaddleNLP/examples/language_model/gpt2/process_data.py
+++ PaddleNLP/examples/language_model/gpt2/process_data.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import argparse
 import json
 import multiprocessing
@@ -7,43 +7,50 @@ import mmap
 import numpy as np
 from paddlenlp.transformers import GPT2Tokenizer
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_path', type=str, required=True,
-                       help='Path to input JSON')
-    parser.add_argument('--model_name', type=str, required=True,
-                       help='What model to use.')
-    parser.add_argument('--append_eod', action='store_true',
-                       help='Append an <eod> token to the end of a document.')
-    parser.add_argument('--workers', type=int, default=1,
-                       help='Number of worker processes to launch')
+    parser.add_argument(
+        '--input_path', type=str, required=True, help='Path to input JSON')
+    parser.add_argument(
+        '--model_name', type=str, required=True, help='What model to use.')
+    parser.add_argument(
+        '--append_eod',
+        action='store_true',
+        help='Append an <eod> token to the end of a document.')
+    parser.add_argument(
+        '--workers',
+        type=int,
+        default=1,
+        help='Number of worker processes to launch')
     args = parser.parse_args()
     return args
 
 
 class Converter(object):
-   def __init__(self, model_name, append_eod):
-       self.append_eod = append_eod  
-       tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-       Converter.tokenizer = tokenizer
-       self.eod_id = tokenizer.get_command("eod").Id 
-       self.vocab_size = len(tokenizer)
-       
-   def encode(self, json_line):
+    def __init__(self, model_name, append_eod):
+        self.append_eod = append_eod
+        tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+        Converter.tokenizer = tokenizer
+        self.eod_id = tokenizer.get_command("eod").Id
+        self.vocab_size = len(tokenizer)
+
+    def encode(self, json_line):
         data = json.loads(json_line)
         ids = {}
         text = data["text"]
         doc_ids = []
         tokenization = self.tokenizer.encode(text)
         if self.append_eod:
-           tokenization.append(self.eod_id)
+            tokenization.append(self.eod_id)
         tokens = tokenization.tokenization
-        return tokens, len(tokens) 
+        return tokens, len(tokens)
+
 
 def main():
     args = get_args()
     fin = open(args.input_path, 'r', encoding='utf-8')
-    convert  = Converter(args.model_name, args.append_eod)
+    convert = Converter(args.model_name, args.append_eod)
     pool = multiprocessing.Pool(args.workers)
     encoded_docs = pool.imap(convert.encode, fin, 25)
     all_doc_ids = []
@@ -57,8 +64,10 @@ def main():
         save_dtype = np.uint16
     else:
         save_dtype = np.int32
-    all_doc_ids  = np.array(all_doc_ids, dtype=save_dtype)
+    all_doc_ids = np.array(all_doc_ids, dtype=save_dtype)
     lens = np.array(lens, dtype=save_dtype)
     np.savez(args.input_path + "_ids.npz", ids=all_doc_ids, lens=lens)
+
+
 if __name__ == "__main__":
     main()
