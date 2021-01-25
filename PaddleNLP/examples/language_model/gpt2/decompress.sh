0b8e3acd8779ad537b88a0765777a2516ba13d84
wawltor <fanzeyang@hotmail0904.com>
2021-01-25 19:47:05 +0800

    fix preprocess bug

--- PaddleNLP/examples/language_model/gpt2/decompress.sh
+++ PaddleNLP/examples/language_model/gpt2/decompress.sh
@@ -8,10 +8,10 @@ for i in $(ls openwebtext); do
     echo $i; 
     if  ((n % $maxfiles == 0)); then
         ((m=n))
-        mkdir data/data_$m
+        mkdir raw_data/data_$m
     fi
     if  ((++n % $maxjobs == 0)) ; then
         wait 
     fi
-    tar xJf openwebtext/$i --warning=no-timestamp -C data/data_$m/ &
+    tar xJf openwebtext/$i --warning=no-timestamp -C raw_data/data_$m/ &
 done
