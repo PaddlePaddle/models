0f0a8966e62a7f6afafce00a754c44f2f9647298
Steffy-zxf <zhangxuefei@baidu.com>
2021-02-03 11:21:18 +0800

    update codes

--- PaddleNLP/examples/text_matching/simnet/train.py
+++ PaddleNLP/examples/text_matching/simnet/train.py
@@ -28,7 +28,7 @@ from utils import convert_example
 # yapf: disable
 parser = argparse.ArgumentParser(__doc__)
 parser.add_argument("--epochs", type=int, default=10, help="Number of epoches for training.")
-parser.add_argument('--use_gpu', type=eval, default=True, help="Whether use GPU for training, input should be True or False")
+parser.add_argument('--use_gpu', type=eval, default=False, help="Whether use GPU for training, input should be True or False")
 parser.add_argument("--lr", type=float, default=5e-4, help="Learning rate used to train.")
 parser.add_argument("--save_dir", type=str, default='checkpoints/', help="Directory to save model checkpoint")
 parser.add_argument("--batch_size", type=int, default=64, help="Total examples' number of a batch for training.")
