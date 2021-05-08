b162d26e333782b0ea52ae7e6bec771e3efddb1e
ranchlai <=ranchlai@163.com>
2021-05-08 11:09:59 +0800

    remove SoundClassification from paddlespeech, since it's in PaddleAudio now

--- PaddleSpeech/SoundClassification/train_multifold.sh
+++
@@ -1,6 +0,0 @@
-d=$1
-python train.py --test_fold=1 --device=$d
-python train.py --test_fold=2 --device=$d
-python train.py --test_fold=3 --device=$d
-python train.py --test_fold=4 --device=$d
-python train.py --test_fold=5 --device=$d
