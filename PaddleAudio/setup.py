b409f1366762f46a47e78a4c7a9e7c4ffb80a426
ranchlai <=ranchlai@163.com>
2021-05-14 18:27:48 +0800

    refactor

--- PaddleAudio/setup.py
+++ PaddleAudio/setup.py
@@ -1,27 +1,21 @@
-
 import setuptools
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="PaddleAudio", 
-    version="0.0.0",
-    author="",
-    author_email="",
-    description="PaddleAudio, in development",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_packages(exclude=["build*", "test*", "examples*"]),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=[ 
-        'numpy >= 1.15.0',
-        'scipy >= 1.0.0',
-        'resampy >= 0.2.2',
-        'soundfile >= 0.9.0']
-)
+setuptools.setup(name="PaddleAudio",
+                 version="0.0.0",
+                 author="",
+                 author_email="",
+                 description="PaddleAudio, in development",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="",
+                 packages=setuptools.find_packages(exclude=["build*", "test*", "examples*"]),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 python_requires='>=3.6',
+                 install_requires=['numpy >= 1.15.0', 'scipy >= 1.0.0', 'resampy >= 0.2.2', 'soundfile >= 0.9.0'])
