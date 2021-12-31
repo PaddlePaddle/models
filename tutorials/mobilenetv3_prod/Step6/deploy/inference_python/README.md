446146a050a6d55a6441b66a97eafc7f8e137ea9
fengshuai <fengshuai03@baidu.com>
2021-12-31 04:28:00 +0000

    do some change

--- tutorials/mobilenetv3_prod/Step6/deploy/inference_python/README.md
+++ tutorials/mobilenetv3_prod/Step6/deploy/inference_python/README.md
@@ -1,30 +1,56 @@
-## 使用Paddle Inference部署
-### 背景
+# Inference 推理
+
+
+# 目录
+
+- [1. 简介]()
+- [2. 推理过程]()
+    - [2.1 准备推理环境]()
+    - [2.2 模型动转静导出]()
+    - [2.3 模型推理]()
+- [3. FAQ]()
+
+
+## 1. 简介
 
 Paddle Inference 是飞桨的原生推理库， 作用于服务器端和云端，提供高性能的推理能力。相比于直接基于预训练模型进行预测，Paddle Inference可使用MKLDNN、CUDNN、TensorRT进行预测加速，从而实现更优的推理性能。
 
-更多关于Paddle Inference推理引擎的介绍，可以参考Paddle Inference[官网教程](https://www.paddlepaddle.org.cn/documentation/docs/zh/guides/05_inference_deployment/inference/inference_cn.html)。
-### 模型和环境
-#### 准备模型
-训练的模型包括反向传播，在推理时候我们需要将模型进行精简，只保留前向传播的流程即可，将动态模型转化为用于推理的静态图模型。这里我们可以利用`export_model.py`脚本来获取该模型，其他模型可以基于该代码进行修改，本教程模型获取的脚本为[export_model.py](https://github.com/PaddlePaddle/models/blob/79e14a5935372af1848921c4e12122f0b94c5a50/tutorials/mobilenetv3_prod/Step6/tools/export_model.py)
-```python
-python export_model.py --save-inference-dir=model
-```
-**[验收]**
-保存路径下面会生成3个文件，如下所示，其中在Inference推理中用到的为inference.pdiparams与inference.pdmodel。
-* inference.pdiparams     : 模型参数文件
-* inference.pdmodel       : 模型结构文件
-* inference.pdiparams.info: 模型参数信息文件
-#### 准备环境
-1. 如果使用GPU的话，需要自己安转Cuda, Cudnn, TensorRT(不使用tensorRT的话可以不安装,不过建议使用加速)，同时要将每个库的动态库配置好，比如Cuda动态库设置：
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
-2. 去[官网](https://paddleinference.paddlepaddle.org.cn/user_guides/download_lib.html)下载对应版本的推理框架库,假如我们本地安装的环境是:`Cuda=11.1,Cudnn=8.1,TensorRT=7.2.3.4，Python=3.6`,那么就可以下载whl包到本地，然后pip install **.whl就完成了环境的配置
+本文档主要基于Paddle Inference的AlexNet模型推理。
 
-### 运行代码
-#### 整体运行代码
-```python
-python ./infer.py --model-dir=./model --img-path=../images/demo.jpg
+更多关于Paddle Inference推理引擎的介绍，可以参考[Paddle Inference官网教程](https://www.paddlepaddle.org.cn/documentation/docs/zh/guides/05_inference_deployment/inference/inference_cn.html)。
+
+
+## 2. 推理过程
+
+### 2.1 准备推理环境
+
+安装好PaddlePaddle即可体验Paddle Inference部署能力。
+
+
+### 2.2 模型动转静导出
+
+使用下面的命令完成`mobilenet_v3_net`模型的动转静导出。
+
+```bash
+python tools/export_model.py --pretrained=./mobilenet_v3_small_pretrained.pdparams --save-inference-dir="./mobilenet_v3_small_infer" --model=mobilenet_v3_small
 ```
+
+最终在`mobilenet_v3_small_infer/`文件夹下会生成下面的3个文件。
+
+```
+alexnet_infer
+     |----inference.pdiparams     : 模型参数文件
+     |----inference.pdmodel       : 模型结构文件
+     |----inference.pdiparams.info: 模型参数信息文件
+```
+
+### 2.3 模型推理
+
+
+```bash
+python deploy/inference_python/infer.py --model-dir=./mobilenet_v3_small_infer/ --img-path=./images/demo.jpg
+```
+
 对于下面的图像进行预测
 
 <div align="center">
@@ -34,60 +60,10 @@ python ./infer.py --model-dir=./model --img-path=../images/demo.jpg
 在终端中输出结果如下。
 
 ```
-image_name: ../../images/demo.jpg, class_id: 812, prob: 0.001000000862404704
+image_name: ./images/demo.jpg, class_id: 8, prob: 0.9091264605522156
 ```
 
-表示预测的类别ID是`812`，置信度为`0.001`，该模型未经过训练，所以1000类每个概率都是0.001。
-
-#### 运行代码分解
-使用 Paddle Inference 开发 Python 预测程序仅需以下五个步骤：
+表示预测的类别ID是`8`，置信度为`0.909`，该结果与基于训练引擎的结果完全一致。
 
 
-(1) 引用 paddle inference 预测库
-
-```python
-import paddle.inference 
-```
-
-(2) 创建配置对象，并根据需求配置，详细可参考 [Python API 文档 - Config](https://paddleinference.paddlepaddle.org.cn/api_reference/python_api_doc/Config_index.html)
-
-```python
-# 创建 config，并设置预测模型路径
-config = Config(args.model_file, args.params_file)
-```
-此外如果使用TensorRT加速,配置详细说明可以参考[Python API 文档 - TensorRT 设置](https://paddleinference.paddlepaddle.org.cn/api_reference/python_api_doc/Config/GPUConfig.html#tensorrt)
-
-(3) 根据Config创建预测对象predictor，详细可参考 [Python API 文档 - Predictor](https://paddleinference.paddlepaddle.org.cn/api_reference/python_api_doc/Predictor.html)
-
-```python
-predictor = create_predictor(config)
-```
-
-(4) 设置模型输入 Tensor，详细可参考 [Python API 文档 - Tensor](https://paddleinference.paddlepaddle.org.cn/api_reference/python_api_doc/Tensor.html)
-
-```python
-# 获取输入的名称
-input_names = predictor.get_input_names()
-input_handle = predictor.get_input_handle(input_names[0])
-
-# 设置一个假输入
-fake_input = np.random.randn(args.batch_size, 3, 224, 224).astype("float32")
-input_handle.reshape([args.batch_size, 3, 224, 224])
-input_handle.copy_from_cpu(fake_input)
-```
-
-(5) 执行预测
-
-```python
-predictor.run()
-```
-
-(5) 获得预测结果，详细可参考 [Python API 文档 - Tensor](https://paddleinference.paddlepaddle.org.cn/api_reference/python_api_doc/Tensor.html)
-
-```python
-output_names = predictor.get_output_names()
-output_handle = predictor.get_output_handle(output_names[0])
-output_data = output_handle.copy_to_cpu() # numpy.ndarray类型
-```
-### 常见问题
-常见问题参考如下[链接](https://paddleinference.paddlepaddle.org.cn/introduction/faq.html)
+## 3. FAQ
--- tutorials/mobilenetv3_prod/Step6/deploy/inference_python/infer.py
+++ tutorials/mobilenetv3_prod/Step6/deploy/inference_python/infer.py
@@ -1,24 +1,39 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
-import argparse
+import paddle
+from paddle import inference
 import numpy as np
 from PIL import Image
 
+from reprod_log import ReprodLogger
 from preprocess_ops import ResizeImage, CenterCropImage, NormalizeImage, ToCHW, Compose
 
-from paddle.inference import Config
-from paddle.inference import PrecisionType
-from paddle.inference import create_predictor
 
 class InferenceEngine(object):
     """InferenceEngine
     
     Inference engina class which contains preprocess, run, postprocess
+
     """
 
     def __init__(self, args):
         """
         Args:
             args: Parameters generated using argparser.
+
         Returns: None
         """
         super().__init__()
@@ -39,7 +54,8 @@ class InferenceEngine(object):
         if self.args.warmup > 0:
             for idx in range(args.warmup):
                 print(idx)
-                x = np.random.rand(1, 3, 224, 224).astype("float32")
+                x = np.random.rand(1, 3, self.args.crop_size,
+                                   self.args.crop_size).astype("float32")
                 self.input_tensor.copy_from_cpu(x)
                 self.predictor.run()
                 self.output_tensor.copy_to_cpu()
@@ -47,7 +63,9 @@ class InferenceEngine(object):
 
     def load_predictor(self, model_file_path, params_file_path):
         """load_predictor
+
         initialize the inference engine
+
         Args:
             model_file_path: inference model path (*.pdmodel)
             model_file_path: inference parmaeter path (*.pdiparams)
@@ -58,27 +76,22 @@ class InferenceEngine(object):
             output_tensor: Output tensor of the predictor.
         """
         args = self.args
-        config = Config(model_file_path, params_file_path)
-        config.enable_memory_optim()
+        config = inference.Config(model_file_path, params_file_path)
         if args.use_gpu:
-            config.enable_use_gpu(100, 0)
-            config.enable_tensorrt_engine(workspace_size=1 << 30,
-                                      max_batch_size=10,
-                                      min_subgraph_size=5,
-                                      precision_mode=PrecisionType.Float32,
-                                      use_static=False,
-                                      use_calib_mode=False)
-            config.set_trt_dynamic_shape_info(
-                                      min_input_shape={"input": [1, 3, 1, 1]},
-                                      max_input_shape={"input": [10, 3, 1200, 1200]},
-                                      optim_input_shape={"input": [1, 3, 224, 224]})
+            config.enable_use_gpu(1000, 0)
         else:
-            # If not specific mkldnn, you can set the blas thread.
-            # The thread num should not be greater than the number of cores in the CPU.
-            config.set_cpu_math_library_num_threads(4)
-            config.enable_mkldnn()
-        # creat predictor
-        predictor = create_predictor(config)
+            config.disable_gpu()
+
+        # enable memory optim
+        config.enable_memory_optim()
+        config.disable_glog_info()
+
+        config.switch_use_feed_fetch_ops(False)
+        config.switch_ir_optim(True)
+
+        # create predictor
+        predictor = inference.create_predictor(config)
+
         # get input and output tensor property
         input_names = predictor.get_input_names()
         input_tensor = predictor.get_input_handle(input_names[0])
@@ -90,9 +103,12 @@ class InferenceEngine(object):
 
     def preprocess(self, img_path):
         """preprocess
+
         Preprocess to the input.
+
         Args:
             img_path: Image path.
+
         Returns: Input data after preprocess.
         """
         with open(img_path, "rb") as f:
@@ -100,14 +116,16 @@ class InferenceEngine(object):
             img = img.convert("RGB")
         img = self.transforms(img)
         img = np.expand_dims(img, axis=0)
-        print(np.shape(img))
         return img
 
     def postprocess(self, x):
         """postprocess
+
         Postprocess to the inference engine output.
+
         Args:
             x: Inference engine output.
+
         Returns: Output data after argmax.
         """
         x = x.flatten()
@@ -117,9 +135,12 @@ class InferenceEngine(object):
 
     def run(self, x):
         """run
+
         Inference process using inference engine.
+
         Args:
             x: Input data after preprocess.
+
         Returns: Inference engine output
         """
         self.input_tensor.copy_from_cpu(x)
@@ -128,45 +149,96 @@ class InferenceEngine(object):
         return output
 
 
+def get_args(add_help=True):
+    """
+    parse args
+    """
+    import argparse
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--use_gpu", type=int, default=1, help="Whether use gpu.")
-    parser.add_argument("--warmup", default=1, type=int, help="warmup iter")
-    parser.add_argument("--img-path", default="../../images/demo.jpg")
-    parser.add_argument("--model-dir", default=None, help="inference model dir")
-    parser.add_argument("--resize-size", default=256, type=int, help="resize_size")
+    def str2bool(v):
+        return v.lower() in ("true", "t", "1")
+
+    parser = argparse.ArgumentParser(
+        description="PaddlePaddle Classification Training", add_help=add_help)
+
+    parser.add_argument(
+        "--model-dir", default=None, help="inference model dir")
+    parser.add_argument(
+        "--use-gpu", default=False, type=str2bool, help="use_gpu")
+    parser.add_argument(
+        "--max-batch-size", default=16, type=int, help="max_batch_size")
+    parser.add_argument("--batch-size", default=1, type=int, help="batch size")
+
+    parser.add_argument(
+        "--resize-size", default=256, type=int, help="resize_size")
     parser.add_argument("--crop-size", default=224, type=int, help="crop_szie")
-    return parser.parse_args()
+    parser.add_argument("--img-path", default="./images/demo.jpg")
+
+    parser.add_argument(
+        "--benchmark", default=False, type=str2bool, help="benchmark")
+    parser.add_argument("--warmup", default=0, type=int, help="warmup iter")
+
+    args = parser.parse_args()
+    return args
+
 
 def infer_main(args):
     """infer_main
+
     Main inference function.
+
     Args:
         args: Parameters generated using argparser.
+
     Returns:
         class_id: Class index of the input.
         prob: : Probability of the input.
     """
     inference_engine = InferenceEngine(args)
 
+    # init benchmark
+    if args.benchmark:
+        import auto_log
+        autolog = auto_log.AutoLogger(
+            model_name="classification",
+            batch_size=args.batch_size,
+            inference_config=inference_engine.config,
+            gpu_ids="auto" if args.use_gpu else None)
+
+    assert args.batch_size == 1, "batch size just supports 1 now."
+
+    # enable benchmark
+    if args.benchmark:
+        autolog.times.start()
+
     # preprocess
     img = inference_engine.preprocess(args.img_path)
 
-    # run
+    if args.benchmark:
+        autolog.times.stamp()
+
     output = inference_engine.run(img)
 
+    if args.benchmark:
+        autolog.times.stamp()
 
     # postprocess
     class_id, prob = inference_engine.postprocess(output)
 
-    # result
+    if args.benchmark:
+        autolog.times.stamp()
+        autolog.times.end(stamp=True)
+        autolog.report()
+
     print(f"image_name: {args.img_path}, class_id: {class_id}, prob: {prob}")
     return class_id, prob
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
     class_id, prob = infer_main(args)
 
+    reprod_logger = ReprodLogger()
+    reprod_logger.add("class_id", np.array([class_id]))
+    reprod_logger.add("prob", np.array([prob]))
+    reprod_logger.save("output_inference_engine.npy")
