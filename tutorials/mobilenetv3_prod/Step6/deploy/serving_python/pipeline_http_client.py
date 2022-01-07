49c58c7cc244988271c82c807d2015f6db7ed9f1
felixhjh <852142024@qq.com>
2022-01-07 07:29:51 +0000

    Add Paddle Serving code for mobilenetv3

---
+++ tutorials/mobilenetv3_prod/Step6/deploy/serving_python/pipeline_http_client.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import requests
+import json
+import cv2
+import base64
+import os
+
+
+def cv2_to_base64(image):
+    """cv2_to_base64
+
+    Convert an numpy array to a base64 object.
+
+    Args:
+        image: Input array.
+
+    Returns: Base64 output of the input.
+    """
+    return base64.b64encode(image).decode('utf8')
+
+
+if __name__ == "__main__":
+    url = "http://127.0.0.1:18087/imagenet/prediction"
+    logid = 10000
+    img_path = "../../images/demo.jpg"
+    with open(img_path, 'rb') as file:
+        image_data = file.read()
+    # data should be transformed to the base64 format
+    image = cv2_to_base64(image_data)
+    data = {"key": ["image"], "value": [image], "logid": logid}
+    # send requests
+    r = requests.post(url=url, data=json.dumps(data))
+    print(r.json())
---
+++ tutorials/mobilenetv3_prod/Step6/deploy/serving_python/web_service.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from paddle_serving_server.web_service import WebService, Op
+
+import sys
+import logging
+import numpy as np
+import base64, cv2
+from paddle_serving_app.reader import Sequential, URL2Image, Resize, CenterCrop, RGB2BGR, Transpose, Div, Normalize, Base64ToImage
+
+
+class MobileNetV3Op(Op):
+    def init_op(self):
+        self.seq = Sequential([
+            Resize(256), CenterCrop(224), RGB2BGR(), Transpose((2, 0, 1)),
+            Div(255), Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225],
+                                True)
+        ])
+
+    def preprocess(self, input_dicts, data_id, log_id):
+        (_, input_dict), = input_dicts.items()
+        batch_size = len(input_dict.keys())
+        imgs = []
+        for key in input_dict.keys():
+            data = base64.b64decode(input_dict[key].encode('utf8'))
+            data = np.fromstring(data, np.uint8)
+            im = cv2.imdecode(data, cv2.IMREAD_COLOR)
+            img = self.seq(im)
+            imgs.append(img[np.newaxis, :].copy())
+        input_imgs = np.concatenate(imgs, axis=0)
+        return {"input": input_imgs}, False, None, ""
+
+    def postprocess(self, input_dicts, fetch_dict, data_id, log_id):
+        score_list = fetch_dict["softmax_1.tmp_0"]
+        result = {"class_id": [], "prob": []}
+        for score in score_list:
+            score = score.tolist()
+            max_score = max(score)
+            result["class_id"].append(score.index(max_score))
+            result["prob"].append(max_score)
+        result["class_id"] = str(result["class_id"])
+        result["prob"] = str(result["prob"])
+        return result, None, ""
+
+class MobileNetV3Service(WebService):
+    def get_pipeline_response(self, read_op):
+        mobilenetv3_op = MobileNetV3Op(
+            name="imagenet", input_ops=[read_op])
+        return mobilenetv3_op
+
+
+# define the service class
+uci_service = MobileNetV3Service(name="imagenet")
+# load config and prepare the service
+uci_service.prepare_pipeline_config("config.yml")
+# start the service
+uci_service.run_service()
