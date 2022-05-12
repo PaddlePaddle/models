f3ff5987c28888b57cf399d500992aed796e3abc
LDOUBLEV <liuvv0203@outlook.com>
2022-05-12 11:36:18 +0800

    fix cpp

--- tutorials/mobilenetv3_prod/Step6/test_tipc/test_inference_cpp.sh
+++ tutorials/mobilenetv3_prod/Step6/test_tipc/test_inference_cpp.sh
@@ -30,12 +30,6 @@ lines=(${dataline})
 # parser load config
 use_gpu_key=$(func_parser_key_cpp "${lines[1]}")
 use_gpu_value=$(func_parser_value_cpp "${lines[1]}")
-use_mkldnn_key=$(func_parser_key_cpp "${lines[5]}")
-use_mkldnn_value=$(func_parser_value_cpp "${lines[5]}")
-use_tensorrt_key=$(func_parser_key_cpp "${lines[6]}")
-use_tensorrt_value=$(func_parser_value_cpp "${lines[6]}")
-use_fp16_key=$(func_parser_key_cpp "${lines[7]}")
-use_fp16_value=$(func_parser_value_cpp "${lines[7]}")
 
 LOG_PATH="./log/infer_cpp"
 mkdir -p ${LOG_PATH}
@@ -43,22 +37,10 @@ status_log="${LOG_PATH}/results_infer_cpp.log"
 
 function func_infer_cpp(){
     # inference cpp
-    if test $use_gpu_value -gt 0; then
-        if test $use_tensorrt_value -gt 0; then
-            if test $use_fp16_value -gt 0; then
-                _save_log_path="${LOG_PATH}/infer_cpp_${use_gpu_key}_${use_tensorrt_key}_${use_fp16_key}.log"
-            else
-                _save_log_path="${LOG_PATH}/infer_cpp_${use_gpu_key}_${use_tensorrt_key}.log"
-            fi
-        else
-            _save_log_path="${LOG_PATH}/infer_cpp_${use_gpu_key}.log"
-        fi
+    if test $use_gpu_value -gt 0; then     
+        _save_log_path="${LOG_PATH}/infer_cpp_use_cpu.log" 
     else
-        if test $use_mkldnn_value -gt 0; then
-            _save_log_path="${LOG_PATH}/infer_cpp_use_cpu_${use_mkldnn_key}.log"
-        else
-            _save_log_path="${LOG_PATH}/infer_cpp_use_cpu.log"
-        fi    
+        _save_log_path="${LOG_PATH}/infer_cpp_${use_gpu_key}.log"
     fi
     # run infer cpp
     inference_cpp_cmd="./deploy/inference_cpp/build/clas_system"
