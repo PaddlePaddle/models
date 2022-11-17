11c8a8b8f9d29c6d1f3d809eb873f59eb9243f45
Tingquan Gao <35441050@qq.com>
2022-11-17 16:41:54 +0800

    add pplcnet pplcnetv2 pphgnet (#5560)
    * add pplcnet
    
    * add pplcnetv2
    
    * add pphgnet
    
    * fix
    
    * update
    
    * support gradio for PPLCNet PPLCNetV2 PPHGNet
    
    * fix
    
    * enrich contents
    
    * Update benchmark_cn.md
    
    * Update download_cn.md
    
    * Update info.yaml
    
    * Update info.yaml
    
    * add example
    
    * Add modelcenter en
    
    * Add PP-HGNet en
    
    * Add PP-LCNet and PP-LCNetV2 en
    
    * Add modelcenter English
    
    * rename v2 to V2
    
    * fix
    
    * Update info.yaml
    
    Co-authored-by: liuTINA0907 <65896652+liuTINA0907@users.noreply.github.com>
    Co-authored-by: zhangyubo0722 <zhangyubo0722@163.com>

---
+++ modelcenter/PP-LCNetV2/APP/app.py
@@ -0,0 +1,40 @@
+import gradio as gr
+import numpy as np
+
+from paddleclas import PaddleClas
+import cv2 as cv
+
+
+clas = PaddleClas(model_name='PPLCNetV2_base')
+
+# UGC: Define the inference fn() for your models
+def model_inference(image):
+    result = clas.predict(image)
+    return next(result)
+
+
+def clear_all():
+    return None, None
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("Image Classification")
+
+    with gr.Column(scale=1, min_width=100):
+
+        img_in = gr.Image(
+            value="https://gitee.com/paddlepaddle/PaddleClas/raw/release/2.5/docs/images/inference_deployment/whl_demo.jpg",
+            shape=(200, 200),
+            label="Input").style(height=200)
+
+        with gr.Row():
+            btn1 = gr.Button("Clear")
+            btn2 = gr.Button("Submit")
+
+        prediction_out = gr.Textbox(label="Prediction")
+
+    btn1.click(fn=clear_all, inputs=None, outputs=[img_in, prediction_out])
+    btn2.click(fn=model_inference, inputs=img_in, outputs=[prediction_out])
+    gr.Button.style(1)
+
+demo.launch()
