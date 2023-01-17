f61af3e04114a21aa7edc594f002796c839b6ef0
GuoxiaWang <mingzilaochongtu@gmail.com>
2023-01-17 11:51:11 +0800

    fix cache directory and app style

--- modelcenter/PLSC-ViT/APP/app.py
+++ modelcenter/PLSC-ViT/APP/app.py
@@ -1,13 +1,20 @@
 import gradio as gr
 from predictor import Predictor
 
-
 model_path = "paddlecv://models/vit/v2.4/imagenet2012-ViT-B_16-224_infer.pdmodel"
 params_path = "paddlecv://models/vit/v2.4/imagenet2012-ViT-B_16-224_infer.pdiparams"
 label_path = "paddlecv://dataset/imagenet2012_labels.txt"
 
+predictor = None
+
+
 def model_inference(image):
-    predictor = Predictor(model_path=model_path, params_path=params_path, label_path=label_path)
+    global predictor
+    if predictor is None:
+        predictor = Predictor(
+            model_path=model_path,
+            params_path=params_path,
+            label_path=label_path)
     scores, labels = predictor.predict(image)
     json_out = {"scores": scores.tolist(), "labels": labels.tolist()}
     return image, json_out
@@ -22,13 +29,15 @@ with gr.Blocks() as demo:
 
     with gr.Column(scale=1, min_width=100):
 
-        img_in = gr.Image(value="https://plsc.bj.bcebos.com/dataset/test_images/cat.jpg",label="Input").style(height=200)
+        img_in = gr.Image(
+            value="https://plsc.bj.bcebos.com/dataset/test_images/cat.jpg",
+            label="Input")
 
         with gr.Row():
             btn1 = gr.Button("Clear")
             btn2 = gr.Button("Submit")
 
-        img_out = gr.Image(label="Output").style(height=200)
+        img_out = gr.Image(label="Output")
         json_out = gr.JSON(label="jsonOutput")
 
     btn2.click(fn=model_inference, inputs=img_in, outputs=[img_out, json_out])
